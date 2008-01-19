2003fe2974d27b0c2fc7ab63df4b9891f0bf2fc6
Giovanni Di Sirio <gdisirio@gmail.com>
2008-01-19 09:29:36 +0000

    made a copy
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/tags/ver_0.5.2@181 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

---
+++ src/chschd.c
@@ -0,0 +1,171 @@
+/*
+    ChibiOS/RT - Copyright (C) 2006-2007 Giovanni Di Sirio.
+
+    This file is part of ChibiOS/RT.
+
+    ChibiOS/RT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    ChibiOS/RT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * @addtogroup Scheduler
+ * @{
+ */
+
+#include <ch.h>
+
+/** @cond never*/
+ReadyList rlist;
+/** @endcond */
+
+/**
+ * Scheduler initialization.
+ * @note Internally invoked by the \p chSysInit().
+ */
+void chSchInit(void) {
+
+  fifo_init(&rlist.r_queue);
+  rlist.r_prio = NOPRIO;
+  rlist.r_preempt = CH_TIME_QUANTUM;
+#ifdef CH_USE_SYSTEMTIME
+  rlist.r_stime = 0;
+#endif
+}
+
+/**
+ * Inserts a thread in the Ready List.
+ * @param tp the Thread to be made ready
+ * @param msg message to the awakened thread
+ * @return the Thread pointer
+ * @note The function must be called in the system mutex zone.
+ * @note The function does not reschedule, the \p chSchRescheduleS() should
+ *       be called soon after.
+ * @note The function is not meant to be used in the user code directly.
+ */
+#ifdef CH_OPTIMIZE_SPEED
+/* NOTE: it is inlined in this module only.*/
+INLINE void chSchReadyI(Thread *tp, t_msg msg) {
+#else
+void chSchReadyI(Thread *tp, t_msg msg) {
+#endif
+  Thread *cp = rlist.r_queue.p_next;
+
+  tp->p_state = PRREADY;
+  tp->p_rdymsg = msg;
+  while (cp->p_prio >= tp->p_prio)
+    cp = cp->p_next;
+  /* Insertion on p_prev.*/
+  tp->p_prev = (tp->p_next = cp)->p_prev;
+  tp->p_prev->p_next = cp->p_prev = tp;
+}
+
+/**
+ * Puts the current thread to sleep into the specified state, the next highest
+ * priority thread becomes running. The threads states are described into
+ * \p threads.h
+ * @param newstate the new thread state
+ * @return the wakeup message
+ * @note The function must be called in the system mutex zone.
+ * @note The function is not meant to be used in the user code directly.
+ */
+void chSchGoSleepS(t_tstate newstate) {
+  Thread *otp;
+
+  (otp = currp)->p_state = newstate;
+  (currp = fifo_remove(&rlist.r_queue))->p_state = PRCURR;
+  rlist.r_preempt = CH_TIME_QUANTUM;
+#ifdef CH_USE_TRACE
+  chDbgTrace(otp, currp);
+#endif
+  chSysSwitchI(otp, currp);
+}
+
+/**
+ * Wakeups a thread, the thread is inserted into the ready list or made
+ * running directly depending on its relative priority compared to the current
+ * thread.
+ * @param ntp the Thread to be made ready
+ * @param msg message to the awakened thread
+ * @note The function must be called in the system mutex zone.
+ * @note The function is not meant to be used in the user code directly.
+ * @note It is equivalent to a \p chSchReadyI() followed by a
+ *       \p chSchRescheduleS() but much more efficient.
+ */
+void chSchWakeupS(Thread *ntp, t_msg msg) {
+
+  if (ntp->p_prio <= currp->p_prio)
+    chSchReadyI(ntp, msg);
+  else {
+    Thread *otp = currp;
+    chSchReadyI(otp, RDY_OK);
+    (currp = ntp)->p_state = PRCURR;
+    ntp->p_rdymsg = msg;
+    rlist.r_preempt = CH_TIME_QUANTUM;
+#ifdef CH_USE_TRACE
+    chDbgTrace(otp, ntp);
+#endif
+    chSysSwitchI(otp, ntp);
+  }
+}
+
+/**
+ * Performs a reschedulation. It is meant to be called if
+ * \p chSchRescRequired() evaluates to \p TRUE.
+ */
+void chSchDoRescheduleI(void) {
+  Thread *otp = currp;
+
+  chSchReadyI(otp, RDY_OK);
+  (currp = fifo_remove(&rlist.r_queue))->p_state = PRCURR;
+  rlist.r_preempt = CH_TIME_QUANTUM;
+#ifdef CH_USE_TRACE
+  chDbgTrace(otp, currp);
+#endif
+  chSysSwitchI(otp, currp);
+}
+
+/**
+ * If a thread with an higher priority than the current thread is in the
+ * ready list then it becomes running.
+ * @note The function must be called in the system mutex zone.
+ */
+void chSchRescheduleS(void) {
+
+  if (isempty(&rlist.r_queue) || firstprio(&rlist.r_queue) <= currp->p_prio)
+    return;
+
+  chSchDoRescheduleI();
+}
+
+/**
+ * Evaluates if a reschedulation is required.
+ * @return \p TRUE if there is a thread that should go in running state
+ *         immediatly else \p FALSE.
+ */
+BOOL chSchRescRequiredI(void) {
+
+  if (isempty(&rlist.r_queue))
+    return FALSE;
+
+  if (rlist.r_preempt) {
+    if (firstprio(&rlist.r_queue) <= currp->p_prio)
+      return FALSE;
+  }
+  else { /* Time quantum elapsed. */
+    if (firstprio(&rlist.r_queue) < currp->p_prio)
+      return FALSE;
+  }
+  return TRUE;
+}
+
+/** @} */
---
+++ src/include/debug.h
@@ -0,0 +1,99 @@
+/*
+    ChibiOS/RT - Copyright (C) 2006-2007 Giovanni Di Sirio.
+
+    This file is part of ChibiOS/RT.
+
+    ChibiOS/RT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    ChibiOS/RT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * @addtogroup Debug
+ * @{
+ */
+
+#ifndef _DEBUG_H_
+#define _DEBUG_H_
+
+#ifdef CH_USE_DEBUG
+
+#ifndef TRACE_BUFFER_SIZE
+#define TRACE_BUFFER_SIZE 64
+#endif
+
+/**
+ * Fill value for threads working area in debug mode.
+ */
+#define MEM_FILL_PATTERN 0x55
+
+typedef struct {
+  void          *cse_wtobjp;
+  t_time        cse_time;
+  UWORD16       cse_state: 4;
+  UWORD16       cse_tid: 12;
+} CtxSwcEvent;
+
+typedef struct {
+  t_size        tb_size;
+  CtxSwcEvent   *tb_ptr;
+  CtxSwcEvent   tb_buffer[TRACE_BUFFER_SIZE];
+} TraceBuffer;
+
+extern CtxSwcEvent *dbgnext;
+extern TraceBuffer dbgtb;
+extern char *dbglastmsg;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+  void chDbgInit(void);
+  void chDbgPuts(char *msg);
+  void chDbgPanic(char *msg);
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * Condition assertion, if the condition check fails then the kernel panics
+ * with the specified message.
+ * @param c the condition to be verified to be true
+ * @param m the text message
+ * @note The condition is tested only if the \p CH_USE_DEBUG switch is
+ *       specified in \p chconf.h else the macro does nothing.
+ */
+#define chDbgAssert(c, m) {                                             \
+  if (!(c))                                                             \
+    chDbgPanic(m);                                                      \
+}
+
+#else /* CH_USE_DEBUG */
+
+#define chDbgInit()
+#define chDbgPanic(msg) {}
+#define chDbgAssert(c, m) {}
+
+#endif /* CH_USE_DEBUG */
+
+#ifdef CH_USE_TRACE
+#ifdef __cplusplus
+extern "C" {
+#endif
+  void chDbgTrace(Thread *otp, Thread *ntp);
+#ifdef __cplusplus
+}
+#endif
+#endif /* CH_USE_TRACE */
+
+#endif /* _DEBUG_H_ */
+
+/** @} */
