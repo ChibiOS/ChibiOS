3c361f9a80cd78ea88ded189eb2622d3d3416432
Giovanni Di Sirio <gdisirio@gmail.com>
2008-02-21 18:31:45 +0000

    made a copy
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/tags/ver_0.5.4@200 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

---
+++ src/chevents.c
@@ -0,0 +1,220 @@
+/*
+    ChibiOS/RT - Copyright (C) 2006-2007 Giovanni Di Sirio.
+
+    This file is part of ChibiOS/RT.
+
+    ChibiOS/RT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    ChibiOS/RT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * @addtogroup Events
+ * @{
+ */
+#include <ch.h>
+
+#ifdef CH_USE_EVENTS
+/**
+ * Registers an Event Listener on an Event Source.
+ * @param esp pointer to the  \p EventSource structure
+ * @param elp pointer to the \p EventListener structure
+ * @param eid numeric identifier assigned to the Event Listener. The identifier
+ *            is used as index for the event callback function.
+ *            The value must range between zero and the size, in bit, of the
+ *            \p t_eventid type minus one.
+ * @note Multiple Event Listeners can use the same event identifier, the
+ *       listener will share the callback function.
+ */
+void chEvtRegister(EventSource *esp, EventListener *elp, t_eventid eid) {
+
+  chSysLock();
+
+  elp->el_next = esp->es_next;
+  esp->es_next = elp;
+  elp->el_listener = currp;
+  elp->el_id = eid;
+
+  chSysUnlock();
+}
+
+/**
+ * Unregisters an Event Listener from its Event Source.
+ * @param esp pointer to the  \p EventSource structure
+ * @param elp pointer to the \p EventListener structure
+ * @note If the event listener is not registered on the specified event source
+ *       then the function does nothing.
+ * @note For optimal performance it is better to perform the unregister
+ *       operations in inverse order of the register operations (elements are
+ *       found on top of the list).
+ */
+void chEvtUnregister(EventSource *esp, EventListener *elp) {
+  EventListener *p = (EventListener *)esp;
+
+  chSysLock();
+
+  while (p->el_next != (EventListener *)esp) {
+    if (p->el_next == elp) {
+      p->el_next = elp->el_next;
+      break;
+    }
+    p = p->el_next;
+  }
+
+  chSysUnlock();
+}
+
+/**
+ * Clears the pending events specified in the mask.
+ * @param mask the events to be cleared
+ */
+void chEvtClear(t_eventmask mask) {
+
+  chSysLock();
+
+  currp->p_epending &= ~mask;
+
+  chSysUnlock();
+}
+
+/**
+ * Signals all the Event Listeners registered on the specified Event Source.
+ * @param esp pointer to the \p EventSource structure
+ */
+void chEvtSend(EventSource *esp) {
+
+  chSysLock();
+
+  chEvtSendI(esp);
+  chSchRescheduleS();
+
+  chSysUnlock();
+}
+
+/**
+ * Signals all the Event Listeners registered on the specified Event Source.
+ * @param esp pointer to the \p EventSource structure
+ * @note This function does not reschedule.
+ */
+void chEvtSendI(EventSource *esp) {
+  EventListener *elp;
+
+  elp = esp->es_next;
+  while (elp != (EventListener *)esp) {
+    Thread *tp = elp->el_listener;
+
+    tp->p_epending |= EventMask(elp->el_id);
+    if ((tp->p_state == PRWTEVENT) && (tp->p_epending & tp->p_ewmask))
+      chSchReadyI(tp, RDY_OK);
+    elp = elp->el_next;
+  }
+}
+
+#ifdef CH_USE_EVENTS_TIMEOUT
+/**
+ * The function waits for an event and returns the event identifier, if an
+ * event handler is specified then the handler is executed before returning.
+ * @param ewmask mask of the events that should be served by the function,
+ *               \p ALL_EVENTS enables all the sources
+ * @param handlers an array of \p t_evhandler. The array must be
+ *                 have indexes from zero up the higher registered event
+ *                 identifier. The array can be \p NULL or contain \p NULL
+ *                 elements (no callbacks specified).
+ * @return the event identifier
+ * @note Only a single event is served in the function, the one with the
+ *       lowest event id. The function is meant to be invoked into a loop so
+ *       that all events are received and served.<br>
+ *       This means that Event Listeners with a lower event identifier have
+ *       an higher priority.
+ */
+t_eventid chEvtWait(t_eventmask ewmask,
+                    const t_evhandler handlers[]) {
+
+  return chEvtWaitTimeout(ewmask, handlers, TIME_INFINITE);
+}
+
+/**
+ * The function waits for an event or the specified timeout then returns the
+ * event identifier, if an event handler is specified then the handler is
+ * executed before returning.
+ * @param ewmask mask of the events that should be served by the function,
+ *               \p ALL_EVENTS enables all the sources
+ * @param handlers an array of \p t_evhandler. The array must be
+ *                 have indexes from zero up the higher registered event
+ *                 identifier. The array can be NULL or contain NULL elements
+ *                 (no callback specified).
+ * @param time the number of ticks before the operation timouts
+ * @return the event identifier or \p RDY_TIMEOUT if the specified time expired
+ * @note Only a single event is served in the function, the one with the
+ *       lowest event id. The function is meant to be invoked into a loop so
+ *       that all events are received and served.<br>
+ *       This means that Event Listeners with a lower event identifier have
+ *       an higher priority.
+ * @note The function is available only if the \p CH_USE_EVENTS_TIMEOUT
+ *       option is enabled in \p chconf.h.
+ */
+t_eventid chEvtWaitTimeout(t_eventmask ewmask,
+                           const t_evhandler handlers[],
+                           t_time time) {
+  t_eventid i;
+  t_eventmask m;
+
+  chSysLock();
+
+  if ((currp->p_epending & ewmask) == 0) {
+    currp->p_ewmask = ewmask;
+    if (chSchGoSleepTimeoutS(PRWTEVENT, time) < RDY_OK)
+      return RDY_TIMEOUT;
+  }
+  i = 0, m = 1;
+  while ((currp->p_epending & ewmask & m) == 0)
+    i += 1, m <<= 1;
+  currp->p_epending &= ~m;
+
+  chSysUnlock();
+
+  if (handlers && handlers[i])
+    handlers[i](i);
+
+  return i;
+}
+
+#else /* !CH_USE_EVENTS_TIMEOUT */
+t_eventid chEvtWait(t_eventmask ewmask,
+                    const t_evhandler handlers[]) {
+  t_eventid i;
+  t_eventmask m;
+
+  chSysLock();
+
+  if ((currp->p_epending & ewmask) == 0) {
+    currp->p_ewmask = ewmask;
+    chSchGoSleepS(PRWTEVENT);
+  }
+  i = 0, m = 1;
+  while ((currp->p_epending & ewmask & m) == 0)
+    i += 1, m <<= 1;
+  currp->p_epending &= ~m;
+
+  chSysUnlock();
+
+  if (handlers && handlers[i])
+    handlers[i](i);
+
+  return i;
+}
+
+#endif /* CH_USE_EVENTS_TIMEOUT */
+
+#endif /* CH_USE_EVENTS */
+
+/** @} */
---
+++ src/chmtx.c
@@ -0,0 +1,243 @@
+/*
+    ChibiOS/RT - Copyright (C) 2006-2007 Giovanni Di Sirio.
+
+    This file is part of ChibiOS/RT.
+
+    ChibiOS/RT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    ChibiOS/RT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * @addtogroup Mutexes
+ * @{
+ */
+
+#include <ch.h>
+
+#ifdef CH_USE_MUTEXES
+
+/**
+ * Initializes s \p Mutex structure.
+ * @param mp pointer to a \p Mutex structure
+ */
+void chMtxInit(Mutex *mp) {
+
+  fifo_init(&mp->m_queue);
+  mp->m_owner = NULL;
+}
+
+/**
+ * Locks the specified mutex.
+ * @param mp pointer to the \p Mutex structure
+ */
+void chMtxLock(Mutex *mp) {
+
+  chSysLock();
+
+  chMtxLockS(mp);
+
+  chSysUnlock();
+}
+
+/**
+ * Locks the specified mutex.
+ * @param mp pointer to the \p Mutex structure
+ * @note This function must be called within a \p chSysLock() / \p chSysUnlock()
+ *       block.
+ */
+void chMtxLockS(Mutex *mp) {
+
+  if (mp->m_owner != NULL) {
+    /*
+     * Inheritance, explores the thread-mutex dependances adjusting
+     * the priority of all the affected threads.
+     */
+    Thread *tp = mp->m_owner;
+    while (tp->p_prio < currp->p_prio) {
+      tp->p_prio = currp->p_prio;
+      /*
+       * The following states need priority queues reordering.
+       */
+      switch (tp->p_state) {
+      case PRWTMTX:
+        prio_insert(dequeue(tp), &tp->p_wtmtxp->m_queue);
+        tp = tp->p_wtmtxp->m_owner;
+        continue;
+#ifdef CH_USE_MESSAGES_PRIORITY
+      case PRSNDMSG:
+        if (tp->p_flags & P_MSGBYPRIO)
+          prio_insert(dequeue(tp), &tp->p_wtthdp->p_msgqueue);
+#endif
+      case PRREADY:
+        chSchReadyI(dequeue(tp), RDY_OK);
+      }
+      break;
+    }
+    /*
+     * Goes to sleep on the mutex.
+     */
+    prio_insert(currp, &mp->m_queue);
+    currp->p_wtmtxp = mp;
+    chSchGoSleepS(PRWTMTX);
+    chDbgAssert(mp->m_owner == NULL, "chmtx.c, chMtxLockS()");
+  }
+  /*
+   * The mutex is now inserted in the owned mutexes list.
+   */
+  mp->m_owner = currp;
+  mp->m_next = currp->p_mtxlist;
+  currp->p_mtxlist = mp;
+}
+
+/**
+ * Tries to lock a mutex. This function does not have any overhead related to
+ * the priority inheritance mechanism because it does not try to enter a sleep
+ * state on the mutex.
+ * @param mp pointer to the \p Mutex structure
+ * @return \p TRUE if the mutex was successfully acquired else \p FALSE
+ */
+BOOL chMtxTryLock(Mutex *mp) {
+  BOOL b;
+
+  chSysLock();
+
+  b = chMtxTryLockS(mp);
+
+  chSysUnlock();
+  return b;
+}
+
+/**
+ * Tries to lock a mutex. This function does not have any overhead related to
+ * the priority inheritance mechanism because it does not try to enter a sleep
+ * state on the mutex.
+ * @param mp pointer to the \p Mutex structure
+ * @return \p TRUE if the mutex was successfully acquired else \p FALSE
+ * @note This function must be called within a \p chSysLock() / \p chSysUnlock()
+ *       block.
+ */
+BOOL chMtxTryLockS(Mutex *mp) {
+
+  if (mp->m_owner != NULL)
+    return FALSE;
+  mp->m_owner = currp;
+  mp->m_next = currp->p_mtxlist;
+  currp->p_mtxlist = mp;
+  return TRUE;
+}
+
+/**
+ * Unlocks the next owned mutex in reverse lock order.
+ */
+void chMtxUnlock(void) {
+  Mutex *mp;
+
+  chSysLock();
+
+  chDbgAssert((currp->p_mtxlist != NULL) && (currp->p_mtxlist->m_owner == currp),
+              "chmtx.c, chMtxUnlock()");
+
+  /*
+   * Removes the top Mutex from the owned mutexes list and marks it as not owned.
+   */
+  mp = currp->p_mtxlist;
+  currp->p_mtxlist = mp->m_next;
+  mp->m_owner = NULL;
+  /*
+   * If a thread is waiting on the mutex then the hard part begins.
+   */
+  if (chMtxQueueNotEmptyS(mp)) {
+    Thread *tp = fifo_remove(&mp->m_queue);
+    /*
+     * Recalculates the optimal thread priority by scanning the owned mutexes list.
+     */
+    t_prio newprio = currp->p_realprio;
+    mp = currp->p_mtxlist;
+    while (mp != NULL) {
+      if (chMtxQueueNotEmptyS(mp) && (mp->m_queue.p_next->p_prio > newprio))
+        newprio = mp->m_queue.p_next->p_prio;
+      mp = mp->m_next;
+    }
+    currp->p_prio = newprio;
+    chSchWakeupS(tp, RDY_OK);
+  }
+
+  chSysUnlock();
+}
+
+/**
+ * Unlocks the next owned mutex in reverse lock order.
+ * @note This function must be called within a \p chSysLock() / \p chSysUnlock()
+ *       block.
+ * @note This function does not reschedule internally.
+ */
+void chMtxUnlockS(void) {
+  Mutex *mp;
+
+  chDbgAssert((currp->p_mtxlist != NULL) && (currp->p_mtxlist->m_owner == currp),
+              "chmtx.c, chMtxUnlockS()");
+
+  /*
+   * Removes the top Mutex from the owned mutexes list and marks it as not owned.
+   */
+  mp = currp->p_mtxlist;
+  currp->p_mtxlist = mp->m_next;
+  mp->m_owner = NULL;
+  /*
+   * If a thread is waiting on the mutex then the hard part begins.
+   */
+  if (chMtxQueueNotEmptyS(mp)) {
+    Thread *tp = fifo_remove(&mp->m_queue);
+    /*
+     * Recalculates the optimal thread priority by scanning the owned mutexes list.
+     */
+    t_prio newprio = currp->p_realprio;
+    mp = currp->p_mtxlist;
+    while (mp != NULL) {
+      if (chMtxQueueNotEmptyS(mp) && (mp->m_queue.p_next->p_prio > newprio))
+        newprio = mp->m_queue.p_next->p_prio;
+      mp = mp->m_next;
+    }
+    currp->p_prio = newprio;
+    chSchReadyI(tp, RDY_OK);
+  }
+}
+
+/**
+ * Unlocks all the mutexes owned by the invoking thread, this is <b>MUCH MORE</b>
+ * efficient than releasing the mutexes one by one and not just because the
+ * call overhead, this function does not have any overhead related to the
+ * priority inheritance mechanism.
+ */
+void chMtxUnlockAll(void) {
+
+  chSysLock();
+
+  if (currp->p_mtxlist != NULL) {
+    do {
+      Mutex *mp = currp->p_mtxlist;
+      currp->p_mtxlist = mp->m_next;
+      mp->m_owner = NULL;
+      if (chMtxQueueNotEmptyS(mp))
+        chSchReadyI(fifo_remove(&mp->m_queue), RDY_OK);
+    } while (currp->p_mtxlist != NULL);
+    currp->p_prio = currp->p_realprio;
+    chSchRescheduleS();
+  }
+
+  chSysUnlock();
+}
+
+#endif /* CH_USE_MUTEXES */
+
+/** @} */
