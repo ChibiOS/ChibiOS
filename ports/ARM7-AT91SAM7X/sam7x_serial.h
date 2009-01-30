a97f7fe244d0f00c3fb1a17e64fd067c9ddcb247
Giovanni Di Sirio <gdisirio@gmail.com>
2009-01-30 17:18:09 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_1.0.x@691 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- ports/ARM7-AT91SAM7X/sam7x_serial.h
+++ ports/ARM7-AT91SAM7X/sam7x_serial.h
@@ -1,5 +1,5 @@
 /*
-    ChibiOS/RT - Copyright (C) 2006-2007 Giovanni Di Sirio.
+    ChibiOS/RT - Copyright (C) 2009 Giovanni Di Sirio.
 
     This file is part of ChibiOS/RT.
 
@@ -15,6 +15,13 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+                                      ---
+
+    A special exception to the GPL can be applied should you wish to distribute
+    a combined work that includes ChibiOS/RT, without being obliged to provide
+    the source code for any proprietary components. See the file exception.txt
+    for full details of how and when the exception can be applied.
 */
 
 #ifndef _SAM7X_SERIAL_H_
