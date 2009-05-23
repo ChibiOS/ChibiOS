957da89e719258a6cbb4b7c655baeef976d28cb9
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2009-05-23 09:03:49 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@983 35acf78f-673a-0410-8e92-d51de3d6d3f4

---
+++ demos/GNU-Linux-GCC/chcore.c
@@ -0,0 +1,107 @@
+/*
+    ChibiOS/RT - Copyright (C) 2006-2007 Giovanni Di Sirio.
+
+    This file is part of ChibiOS/RT.
+
+    ChibiOS/RT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    ChibiOS/RT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <time.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+/**
+ * @addtogroup LINUXSIM_CORE
+ * @{
+ */
+
+#include <ch.h>
+
+static struct itimerval tempo;
+static bool_t pending = FALSE;
+
+void timer(int numSignal) {
+
+  pending = TRUE;
+}
+
+/*
+ * Simulated HW initialization.
+ */
+void _init_core(void) {
+
+  signal(SIGALRM, timer);
+  tempo.it_value.tv_sec = 0;
+  tempo.it_value.tv_usec = 10000;
+  tempo.it_interval.tv_sec = 0;
+  tempo.it_interval.tv_usec = 10000;
+  setitimer(ITIMER_REAL, &tempo, NULL);
+}
+
+/*
+ * Interrupt simulation.
+ */
+void ChkIntSources(void) {
+
+  if (pending) {
+    chSysTimerHandlerI();
+    pending = FALSE;
+  }
+
+  if (chSchRescRequiredI())
+    chSchDoRescheduleI();
+}
+
+/**
+ * Performs a context switch between two threads.
+ * @param otp the thread to be switched out
+ * @param ntp the thread to be switched in
+ */
+__attribute__((used))
+static void __dummy(Thread *otp, Thread *ntp) {
+  asm volatile (".globl @port_switch@8                          \n\t" \
+                "@port_switch@8:                                \n\t" \
+                "push    %ebp                                   \n\t" \
+                "push    %esi                                   \n\t" \
+                "push    %edi                                   \n\t" \
+                "push    %ebx                                   \n\t" \
+                "movl    %esp, 16(%ecx)                         \n\t" \
+                "movl    16(%edx), %esp                         \n\t" \
+                "pop     %ebx                                   \n\t" \
+                "pop     %edi                                   \n\t" \
+                "pop     %esi                                   \n\t" \
+                "pop     %ebp                                   \n\t" \
+                "ret");
+}
+
+/**
+ * Halts the system. In this implementation it just exits the simulation.
+ */
+__attribute__((fastcall))
+void port_halt(void) {
+
+  exit(2);
+}
+
+/**
+ * Threads return point, it just invokes @p chThdExit().
+ */
+void threadexit(void) {
+
+  asm volatile ("push    %eax                                   \n\t" \
+                "call    _chThdExit");
+}
+
+/** @} */
---
+++ demos/GNU-Linux-GCC/main.c
@@ -0,0 +1,53 @@
+/*
+    ChibiOS/RT - Copyright (C) 2006-2007 Giovanni Di Sirio.
+
+    This file is part of ChibiOS/RT.
+
+    ChibiOS/RT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    ChibiOS/RT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+
+#include <ch.h>
+
+static WORKING_AREA(waThread1, 2048);
+static msg_t Thread1(void *arg) {
+
+  while (TRUE) {
+    chThdSleepMilliseconds(1000);
+    printf("Hello World!");
+  }
+  return 0;
+}
+
+/*------------------------------------------------------------------------*
+ * Simulator main, start here your threads, examples inside.              *
+ *------------------------------------------------------------------------*/
+int main(void) {
+
+  /*
+   * ChibiOS/RT initialization.
+   */
+  chSysInit();
+
+  /*
+   * Starting threads.
+   */
+  chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
+
+  while (TRUE)
+    chThdSleepSeconds(1);
+
+  return 0;
+}
