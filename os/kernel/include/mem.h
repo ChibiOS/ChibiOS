85786fabbfd94e480ff43a8979f074c1fd6292e1
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2009-10-11 19:43:13 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@1218 35acf78f-673a-0410-8e92-d51de3d6d3f4

---
+++ os/kernel/include/mem.h
@@ -0,0 +1,61 @@
+/*
+    ChibiOS/RT - Copyright (C) 2006-2007 Giovanni Di Sirio.
+
+    This file is part of ChibiOS/RT.
+
+    ChibiOS/RT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    ChibiOS/RT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * @file mem.h
+ * @brief Low level memory manager macros and structures.
+ * @addtogroup coremem
+ * @{
+ */
+
+#ifndef _MEM_H_
+#define _MEM_H_
+
+#if CH_USE_COREMEM
+
+/**
+ * @brief Memory alignment type.
+ */
+typedef void *align_t;
+
+/**
+ * @brief Alignment mask constant.
+ */
+#define MEM_ALIGN_MASK      (sizeof(align_t) - 1)
+
+/**
+ * @brief Alignment helper macro.
+ */
+#define MEM_ALIGN_SIZE(p)   (((size_t)(p) + ALIGN_MASK) & ~ALIGN_MASK)
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+  void mem_init(void);
+  void *chMemAlloc(size_t size);
+  void *chMemAllocI(size_t size);
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CH_USE_COREMEM */
+
+#endif /* _MEM_H_ */
+
+/** @} */
---
+++ os/kernel/src/chmem.c
@@ -0,0 +1,94 @@
+/*
+    ChibiOS/RT - Copyright (C) 2006-2007 Giovanni Di Sirio.
+
+    This file is part of ChibiOS/RT.
+
+    ChibiOS/RT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    ChibiOS/RT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * @file chmem.c
+ * @brief Low level memory manager code.
+ * @addtogroup coremem
+ * @{
+ */
+
+#include <ch.h>
+
+#if CH_USE_COREMEM
+
+#if CH_COREMEM_SIZE == 0
+  extern align_t __heap_base__;
+  extern align_t __heap_end__;
+#else
+align_t buffer[ALIGN_SIZE(CH_MEM_SIZE) / sizeof(align_t)];
+#endif
+
+static align_t *nextmem;
+static align_t *endmem;
+
+void mem_init(void) {
+#if CH_COREMEM_SIZE == 0
+  nextmem = &__heap_base__;
+  endmem = &__heap_end__;
+#else
+  nextmem = &buffer[0];
+  endmem = &buffer[ALIGN_SIZE(CH_MEM_SIZE) / sizeof(align_t)];
+#endif
+}
+
+/**
+ * @brief Allocates a block of memory.
+ * @details The size of the returned block is aligned to the alignment
+ *          type @p align_t so it is not possible to allocate less than
+ *          <code>sizeof(align_t)</code>.
+ *
+ *
+ * @param[in] the size of the block to be allocated
+ * @return A pointer to the allocated memory block.
+ * @retval NULL allocation failed, core memory exhausted.
+ */
+void *chCoreAlloc(size_t size) {
+  void *p;
+
+  chSysLock();
+  p = chCoreAllocI(size);
+  chSysUnlock();
+  return p;
+}
+
+/**
+ * @brief Allocates a block of memory.
+ * @details The size of the returned block is aligned to the alignment
+ *          type @p align_t so it is not possible to allocate less than
+ *          <code>sizeof(align_t)</code>.
+ *
+ * @param[in] the size of the block to be allocated.
+ * @return A pointer to the allocated memory block.
+ * @retval NULL allocation failed, core memory exhausted.
+ */
+void *chCoreAllocI(size_t size) {
+  void *p;
+
+  size = ALIGN_SIZE(size);
+  if (nextmem + size > endmem)
+    return NULL;
+  p = nextmem;
+  nextmem += size;
+  return p;
+}
+
+#endif /* CH_USE_COREMEM */
+
+/** @} */
