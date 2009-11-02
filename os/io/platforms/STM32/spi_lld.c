c692c27eabe9e77be1c22dce2e1cb0685d7df429
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2009-11-02 21:18:17 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@1265 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/io/platforms/STM32/spi_lld.c
+++ os/io/platforms/STM32/spi_lld.c
@@ -185,7 +185,7 @@ void spi_lld_init(void) {
   SPID1.spd_dmarx   = DMA1_Channel2;
   SPID1.spd_dmatx   = DMA1_Channel3;
   SPID1.spd_dmaprio = STM32_SPI1_DMA_PRIORITY << 12;
-  GPIOA->CRH = (GPIOA->CRH & 0x000FFFFF) | 0xB4B00000;
+  GPIOA->CRL = (GPIOA->CRL & 0x000FFFFF) | 0xB4B00000;
 #endif
 
 #if USE_STM32_SPI2
@@ -195,7 +195,7 @@ void spi_lld_init(void) {
   SPID2.spd_dmarx   = DMA1_Channel4;
   SPID2.spd_dmatx   = DMA1_Channel5;
   SPID2.spd_dmaprio = STM32_SPI2_DMA_PRIORITY << 12;
-  GPIOB->CRL = (GPIOB->CRL & 0x000FFFFF) | 0xB4B00000;
+  GPIOB->CRH = (GPIOB->CRH & 0x000FFFFF) | 0xB4B00000;
 #endif
 }
 
