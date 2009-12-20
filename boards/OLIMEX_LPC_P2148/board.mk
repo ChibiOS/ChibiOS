af494b355e7354e2cc3034a0465100876f9c136f
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2009-12-20 11:49:53 +0000

    Added the buzzer driver to the Olimex LPC-P2148 board generic support.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@1446 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- boards/OLIMEX_LPC_P2148/board.mk
+++ boards/OLIMEX_LPC_P2148/board.mk
@@ -1,5 +1,6 @@
 # List of all the board related files.
-BOARDSRC = ${CHIBIOS}/boards/OLIMEX_LPC_P2148/board.c
+BOARDSRC = ${CHIBIOS}/boards/OLIMEX_LPC_P2148/board.c \
+           ${CHIBIOS}/boards/OLIMEX_LPC_P2148/buzzer.c
 
 # Required include directories
 BOARDINC = ${CHIBIOS}/boards/OLIMEX_LPC_P2148
