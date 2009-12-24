327bcfad6bfbcb79063a3af9839185180d658222
Giovanni Di Sirio <gdisirio@gmail.com>
2009-12-24 11:00:45 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/tags/ver_1.3.6unstable@1468 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

---
+++ os/hal/platforms/Linux/console.c
@@ -0,0 +1,73 @@
+/*
+    ChibiOS/RT - Copyright (C) 2006-2007 Giovanni Di Sirio.
+
+    This file is part of ChibiOS/RT.
+
+    ChibiOS/RT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    ChibiOS/RT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * @file console.c
+ * @brief Simulator console driver code.
+ * @{
+ */
+
+#include <stdio.h>
+
+#include "ch.h"
+#include "console.h"
+
+/**
+ * @brief Console driver 1.
+ */
+BaseChannel CD1;
+
+static bool_t putwouldblock(void *ip) {
+
+  (void)ip;
+  return FALSE;
+}
+
+static bool_t getwouldblock(void *ip) {
+
+  (void)ip;
+  return TRUE;
+}
+
+static msg_t put(void *ip, uint8_t b, systime_t timeout) {
+
+  (void)ip;
+  (void)timeout;
+  fputc(b, stdout);
+  fflush(stdout);
+  return RDY_OK;
+}
+
+static msg_t get(void *ip, systime_t timeout) {
+
+  (void)ip;
+  (void)timeout;
+  return fgetc(stdin);
+}
+
+static const struct BaseChannelVMT vmt = {
+  {putwouldblock, getwouldblock, put, get}
+};
+
+void conInit(void) {
+
+  CD1.vmt = &vmt;
+}
+
+/** @} */
