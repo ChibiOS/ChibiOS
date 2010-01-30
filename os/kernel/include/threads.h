68f7f90f1a71d4f083439826483066ee474d417a
Giovanni Di Sirio <gdisirio@gmail.com>
2010-01-30 10:38:52 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/tags/ver_1.5.0unstable@1554 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

---
+++ os/kernel/include/threads.h
@@ -0,0 +1,256 @@
+/*
+    ChibiOS/RT - Copyright (C) 2006-2007 Giovanni Di Sirio.
+
+    This file is part of ChibiOS/RT.
+
+    ChibiOS/RT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    ChibiOS/RT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * @file threads.h
+ * @brief Threads macros and structures.
+ * @addtogroup threads
+ * @{
+ */
+
+#ifndef _THREADS_H_
+#define _THREADS_H_
+
+/*
+ * Module dependencies check.
+ */
+#if CH_USE_DYNAMIC && !CH_USE_WAITEXIT
+#error "CH_USE_DYNAMIC requires CH_USE_WAITEXIT"
+#endif
+#if CH_USE_DYNAMIC && !CH_USE_HEAP && !CH_USE_MEMPOOLS
+#error "CH_USE_DYNAMIC requires CH_USE_HEAP and/or CH_USE_MEMPOOLS"
+#endif
+
+/**
+ * @brief Structure representing a thread.
+ *
+ * @extends ThreadsQueue
+ * @note Not all the listed fields are always needed, by switching off some
+ *       not needed ChibiOS/RT subsystems it is possible to save RAM space by
+ *       shrinking the @p Thread structure.
+ */
+struct Thread {
+  Thread                *p_next;        /**< Next @p Thread in the threads
+                                             list/queue.                    */
+  /* End of the fields shared with the ThreadsList structure. */
+  Thread                *p_prev;        /**< Previous @p Thread in the threads
+                                             queue.                         */
+  /* End of the fields shared with the ThreadsQueue structure. */
+  tprio_t               p_prio;         /**< Thread priority.               */
+  /* End of the fields shared with the ReadyList structure. */
+  tstate_t              p_state;        /**< Current thread state.          */
+  tmode_t               p_flags;        /**< Various thread flags.          */
+  struct context        p_ctx;          /**< Processor context.             */
+#if CH_USE_NESTED_LOCKS
+  cnt_t                 p_locks;        /**< Number of nested locks.        */
+#endif
+#if CH_DBG_THREADS_PROFILING
+  volatile systime_t    p_time;         /**< Thread  consumed time in ticks.
+                                             @note This field can overflow. */
+#endif
+  union {
+    msg_t               rdymsg;         /**< Thread wakeup code.            */
+    msg_t               exitcode;       /**< The thread exit code
+                                             (@p THD_STATE_FINAL state).    */
+    void                *wtobjp;        /**< Generic kernel object pointer. */
+#if CH_USE_EVENTS
+    eventmask_t         ewmask;         /**< Enabled events mask
+                                             (@p THD_STATE_WTOREVT or
+                                             @p THD_STATE_WTANDEVT states). */
+#endif
+  }                     p_u;            /**< State-specific fields.         */
+#if CH_USE_WAITEXIT
+  Thread                *p_waiting;     /**< Thread waiting for termination.*/
+#endif
+#if CH_USE_MESSAGES
+  ThreadsQueue          p_msgqueue;     /**< Messages queue.                */
+  msg_t                 p_msg;          /**< Thread message.                */
+#endif
+#if CH_USE_EVENTS
+  eventmask_t           p_epending;     /**< Pending events mask.           */
+#endif
+#if CH_USE_MUTEXES
+  Mutex                 *p_mtxlist;     /**< List of the mutexes owned by this
+                                             thread, @p NULL terminated.    */
+  tprio_t               p_realprio;     /**< Thread's own, non-inherited,
+                                             priority.                      */
+#endif
+#if CH_USE_DYNAMIC && CH_USE_MEMPOOLS
+  void                  *p_mpool;       /**< Memory Pool where the thread
+                                             workspace is returned.         */
+#endif
+  /* Extra fields defined in chconf.h */
+  THREAD_EXT_FIELDS
+};
+
+/** Thread state: Ready to run, waiting on the ready list.*/
+#define THD_STATE_READY             0
+/** Thread state: Currently running.*/
+#define THD_STATE_CURRENT           1
+/** Thread state: Thread created in suspended state.*/
+#define THD_STATE_SUSPENDED         2
+/** Thread state: Waiting on a semaphore.*/
+#define THD_STATE_WTSEM             3
+/** Thread state: Waiting on a mutex.*/
+#define THD_STATE_WTMTX             4
+/** Thread state: Waiting in @p chCondWait().*/
+#define THD_STATE_WTCOND            5
+/** Thread state: Waiting in @p chThdSleep() or @p chThdSleepUntil().*/
+#define THD_STATE_SLEEPING          6
+/** Thread state: Waiting in @p chThdWait().*/
+#define THD_STATE_WTEXIT            7
+/** Thread state: Waiting in @p chEvtWaitOneTimeout() or
+    @p chEvtWaitAnyTimeout().*/
+#define THD_STATE_WTOREVT           8
+/** Thread state: Waiting in @p chEvtWaitAllTimeout().*/
+#define THD_STATE_WTANDEVT          9
+/** Thread state: Waiting in @p chMsgSend().*/
+#define THD_STATE_SNDMSG            10
+/** Thread state: Waiting in @p chMsgWait().*/
+#define THD_STATE_WTMSG             11
+/** Thread state: After termination.*/
+#define THD_STATE_FINAL             12
+
+/*
+ * Various flags into the thread p_flags field.
+ */
+#define THD_MEM_MODE_MASK           3       /**< Thread memory mode mask.   */
+#define THD_MEM_MODE_STATIC         0       /**< Thread memory mode: static.*/
+#define THD_MEM_MODE_HEAP           1       /**< Thread memory mode: heap.  */
+#define THD_MEM_MODE_MEMPOOL        2       /**< Thread memory mode: pool.  */
+#define THD_TERMINATE               4       /**< Termination requested.     */
+
+/* Not an API, don't use into the application code.*/
+Thread *init_thread(Thread *tp, tprio_t prio);
+
+/** Thread function.*/
+typedef msg_t (*tfunc_t)(void *);
+
+/*
+ * Threads APIs.
+ */
+#ifdef __cplusplus
+extern "C" {
+#endif
+  Thread *chThdInit(void *wsp, size_t size,
+                    tprio_t prio, tfunc_t pf, void *arg);
+  Thread *chThdCreateStatic(void *wsp, size_t size,
+                            tprio_t prio, tfunc_t pf, void *arg);
+#if CH_USE_DYNAMIC && CH_USE_WAITEXIT && CH_USE_HEAP
+  Thread *chThdCreateFromHeap(MemoryHeap *heapp, size_t size,
+                              tprio_t prio, tfunc_t pf, void *arg);
+#endif
+#if CH_USE_DYNAMIC && CH_USE_WAITEXIT && CH_USE_MEMPOOLS
+  Thread *chThdCreateFromMemoryPool(MemoryPool *mp, tprio_t prio,
+                                    tfunc_t pf, void *arg);
+#endif
+  tprio_t chThdSetPriority(tprio_t newprio);
+  Thread *chThdResume(Thread *tp);
+  void chThdTerminate(Thread *tp);
+  void chThdSleep(systime_t time);
+  void chThdSleepUntil(systime_t time);
+  void chThdYield(void);
+  void chThdExit(msg_t msg);
+#if CH_USE_WAITEXIT
+  msg_t chThdWait(Thread *tp);
+#endif
+#ifdef __cplusplus
+}
+#endif
+
+/** Returns the pointer to the @p Thread currently in execution.*/
+#define chThdSelf() currp
+
+/** Returns the current thread priority.*/
+#define chThdGetPriority() (currp->p_prio)
+
+/** Returns the pointer to the @p Thread local storage area, if any.*/
+#define chThdLS() (void *)(currp + 1)
+
+/**
+ * Verifies if the specified thread is in the @p THD_STATE_FINAL state.
+ *
+ * @param[in] tp the pointer to the thread
+ * @retval TRUE thread terminated.
+ * @retval FALSE thread not terminated.
+ */
+#define chThdTerminated(tp) ((tp)->p_state == THD_STATE_FINAL)
+
+/**
+ * Verifies if the current thread has a termination request pending.
+ *
+ * @retval TRUE termination request pended.
+ * @retval FALSE termination request not pended.
+ */
+#define chThdShouldTerminate() (currp->p_flags & THD_TERMINATE)
+
+/**
+ * Resumes a thread created with @p chThdInit().
+ *
+ * @param[in] tp the pointer to the thread
+ */
+#define chThdResumeI(tp) chSchReadyI(tp)
+
+/**
+ * Suspends the invoking thread for the specified time.
+ *
+ * @param[in] time the delay in system ticks, the special values are handled as
+ *                 follow:
+ *                 - @a TIME_INFINITE the thread enters an infinite sleep
+ *                   state.
+ *                 - @a TIME_IMMEDIATE this value is accepted but interpreted
+ *                   as a normal time specification not as an immediate timeout
+ *                   specification.
+ *                 .
+ */
+#define chThdSleepS(time) chSchGoSleepTimeoutS(THD_STATE_SLEEPING, time)
+
+/**
+ * Delays the invoking thread for the specified number of seconds.
+ *
+ * @param[in] sec the time in seconds
+ * @note The specified time is rounded up to a value allowed by the real
+ *       system clock.
+ * @note The maximum specified value is implementation dependent.
+ */
+#define chThdSleepSeconds(sec) chThdSleep(S2ST(sec))
+
+/**
+ * Delays the invoking thread for the specified number of milliseconds.
+ *
+ * @param[in] msec the time in milliseconds
+ * @note The specified time is rounded up to a value allowed by the real
+ *       system clock.
+ * @note The maximum specified value is implementation dependent.
+ */
+#define chThdSleepMilliseconds(msec) chThdSleep(MS2ST(msec))
+
+/**
+ * Delays the invoking thread for the specified number of microseconds.
+ *
+ * @param[in] usec the time in microseconds
+ * @note The specified time is rounded up to a value allowed by the real
+ *       system clock.
+ * @note The maximum specified value is implementation dependent.
+ */
+#define chThdSleepMicroseconds(usec) chThdSleep(US2ST(usec))
+
+#endif /* _THREADS_H_ */
+
+/** @} */
---
+++ os/kernel/src/chheap.c
@@ -0,0 +1,308 @@
+/*
+    ChibiOS/RT - Copyright (C) 2006-2007 Giovanni Di Sirio.
+
+    This file is part of ChibiOS/RT.
+
+    ChibiOS/RT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    ChibiOS/RT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * @file chheap.c
+ * @brief Heaps code.
+ * @addtogroup heaps
+ * @{
+ */
+
+#include "ch.h"
+
+#if CH_USE_HEAP
+
+/*
+ * Defaults on the best synchronization mechanism available.
+ */
+#if CH_USE_MUTEXES
+#define H_LOCK(h)       chMtxLock(&(h)->h_mtx)
+#define H_UNLOCK(h)     chMtxUnlock()
+#else
+#define H_LOCK(h)       chSemWait(&(h)->h_sem)
+#define H_UNLOCK(h)     chSemSignal(&(h)->h_sem)
+#endif
+
+#if !CH_USE_MALLOC_HEAP
+
+/**
+ * @brief Default heap descriptor.
+ */
+static MemoryHeap default_heap;
+
+/**
+ * @brief Initializes the default heap.
+ *
+ * @note Internal use only.
+ */
+void heap_init(void) {
+  default_heap.h_provider = chCoreAlloc;
+  default_heap.h_free.h_u.next = (struct heap_header  *)NULL;
+  default_heap.h_free.h_size = 0;
+#if CH_USE_MUTEXES
+  chMtxInit(&default_heap.h_mtx);
+#else
+  chSemInit(&default_heap.h_sem, 1);
+#endif
+}
+
+/**
+ * @brief Initializes a memory heap from a static memory area.
+ *
+ * @param[out] heapp pointer to a memory heap descriptor to be initialized
+ * @param[in] buf heap buffer base
+ * @param[in] size heap size
+ *
+ * @note Both the heap buffer base and the heap size must be aligned to
+ *       the @p align_t type size.
+ */
+void chHeapInit(MemoryHeap *heapp, void *buf, size_t size) {
+  struct heap_header *hp;
+
+  chDbgCheck(MEM_IS_ALIGNED(buf) && MEM_IS_ALIGNED(size), "chHeapInit");
+
+  heapp->h_provider = (memgetfunc_t)NULL;
+  heapp->h_free.h_u.next = hp = buf;
+  heapp->h_free.h_size = 0;
+  hp->h_u.next = NULL;
+  hp->h_size = size - sizeof(struct heap_header);
+#if CH_USE_MUTEXES
+  chMtxInit(&heapp->h_mtx);
+#else
+  chSemInit(&heapp->h_sem, 1);
+#endif
+}
+
+/**
+ * @brief Allocates a block of memory from the heap by using the first-fit
+ *        algorithm.
+ * @details The allocated block is guaranteed to be properly aligned for a
+ *          pointer data type (@p align_t).
+ *
+ * @param[in] heapp pointer to a heap descriptor or @p NULL in order to access
+ *                  the default heap.
+ * @param[in] size the size of the block to be allocated. Note that the
+ *                 allocated block may be a bit bigger than the requested
+ *                 size for alignment and fragmentation reasons.
+ * @return A pointer to the allocated block.
+ * @retval NULL if the block cannot be allocated.
+ */
+void *chHeapAlloc(MemoryHeap *heapp, size_t size) {
+  struct heap_header *qp, *hp, *fp;
+
+  if (heapp == NULL)
+    heapp = &default_heap;
+
+  size = MEM_ALIGN_SIZE(size);
+  qp = &heapp->h_free;
+  H_LOCK(heapp);
+
+  while (qp->h_u.next != NULL) {
+    hp = qp->h_u.next;
+    if (hp->h_size >= size) {
+      if (hp->h_size < size + sizeof(struct heap_header)) {
+        /*
+         * Gets the whole block even if it is slightly bigger than the
+         * requested size because the fragment would be too small to be
+         * useful.
+         */
+        qp->h_u.next = hp->h_u.next;
+      }
+      else {
+        /*
+         * Block bigger enough, must split it.
+         */
+        fp = (void *)((uint8_t *)(hp) + sizeof(struct heap_header) + size);
+        fp->h_u.next = hp->h_u.next;
+        fp->h_size = hp->h_size - sizeof(struct heap_header) - size;
+        qp->h_u.next = fp;
+        hp->h_size = size;
+      }
+      hp->h_u.heap = heapp;
+
+      H_UNLOCK(heapp);
+      return (void *)(hp + 1);
+    }
+    qp = hp;
+  }
+
+  H_UNLOCK(heapp);
+
+  /*
+   * More memory is required, tries to get it from the associated provider.
+   */
+  if (heapp->h_provider) {
+    hp = heapp->h_provider(size + sizeof(struct heap_header));
+    if (hp != NULL) {
+      hp->h_u.heap = heapp;
+      hp->h_size = size;
+      hp++;
+      return (void *)hp;
+    }
+  }
+  return NULL;
+}
+
+#define LIMIT(p) (struct heap_header *)((uint8_t *)(p) + \
+                                        sizeof(struct heap_header) + \
+                                        (p)->h_size)
+
+/**
+ * @brief Frees a previously allocated memory block.
+ *
+ * @param[in] p the memory block pointer
+ */
+void chHeapFree(void *p) {
+  struct heap_header *qp, *hp;
+  MemoryHeap *heapp;
+
+  chDbgCheck(p != NULL, "chHeapFree");
+
+  hp = (struct heap_header *)p - 1;
+  heapp = hp->h_u.heap;
+  qp = &heapp->h_free;
+  H_LOCK(heapp);
+
+  while (TRUE) {
+    chDbgAssert((hp < qp) || (hp >= LIMIT(qp)),
+                "chHeapFree(), #1",
+                "within free block");
+
+    if (((qp == &heapp->h_free) || (hp > qp)) &&
+        ((qp->h_u.next == NULL) || (hp < qp->h_u.next))) {
+      /*
+       * Insertion after qp.
+       */
+      hp->h_u.next = qp->h_u.next;
+      qp->h_u.next = hp;
+      /*
+       * Verifies if the newly inserted block should be merged.
+       */
+      if (LIMIT(hp) == hp->h_u.next) {
+        /*
+         * Merge with the next block.
+         */
+        hp->h_size += hp->h_u.next->h_size + sizeof(struct heap_header);
+        hp->h_u.next = hp->h_u.next->h_u.next;
+      }
+      if ((LIMIT(qp) == hp)) {
+        /*
+         * Merge with the previous block.
+         */
+        qp->h_size += hp->h_size + sizeof(struct heap_header);
+        qp->h_u.next = hp->h_u.next;
+      }
+      break;
+    }
+    qp = qp->h_u.next;
+  }
+
+  H_UNLOCK(heapp);
+  return;
+}
+
+/**
+ * @brief Reports the heap status.
+ *
+ * @param[in] heapp pointer to a heap descriptor or @p NULL in order to access
+ *                  the default heap.
+ * @param[in] sizep pointer to a variable that will receive the total
+ *                  fragmented free space
+ * @return The number of fragments in the heap.
+ * @note This function is meant to be used in the test suite, it should not be
+ *       really useful for the application code.
+ * @note This function is not implemented when the @p CH_USE_MALLOC_HEAP
+ *       configuration option is used (it always returns zero).
+ */
+size_t chHeapStatus(MemoryHeap *heapp, size_t *sizep) {
+  struct heap_header *qp;
+  size_t n, sz;
+
+  if (heapp == NULL)
+    heapp = &default_heap;
+
+  H_LOCK(heapp);
+
+  sz = 0;
+  for (n = 0, qp = &heapp->h_free; qp->h_u.next; n++, qp = qp->h_u.next)
+    sz += qp->h_u.next->h_size;
+  if (sizep)
+    *sizep = sz;
+
+  H_UNLOCK(heapp);
+  return n;
+}
+
+#else /* CH_USE_MALLOC_HEAP */
+
+#include <stdlib.h>
+
+#if CH_USE_MUTEXES
+#define H_LOCK()        chMtxLock(&hmtx)
+#define H_UNLOCK()      chMtxUnock()
+static Mutex            hmtx;
+#elif CH_USE_SEMAPHORES
+#define H_LOCK()        chSemWait(&hsem)
+#define H_UNLOCK()      chSemSignal(&hsem)
+static Semaphore        hsem;
+#endif
+
+void heap_init(void) {
+
+#if CH_USE_MUTEXES
+  chMtxInit(&hmtx);
+#else
+  chSemInit(&hsem, 1);
+#endif
+}
+
+void *chHeapAlloc(MemoryHeap *heapp, size_t size) {
+  void *p;
+
+  chDbgCheck(heapp == NULL, "chHeapAlloc");
+
+  H_LOCK();
+  p = malloc(size);
+  H_UNLOCK();
+  return p;
+}
+
+void chHeapFree(void *p) {
+
+  chDbgCheck(p != NULL, "chHeapFree");
+
+  H_LOCK();
+  free(p);
+  H_UNLOCK();
+}
+
+size_t chHeapStatus(MemoryHeap *heapp, size_t *sizep) {
+
+  chDbgCheck(heapp == NULL, "chHeapStatus");
+
+  if (sizep)
+    *sizep = 0;
+  return 0;
+}
+
+#endif /* CH_USE_MALLOC_HEAP */
+
+#endif /* CH_USE_HEAP */
+
+/** @} */
