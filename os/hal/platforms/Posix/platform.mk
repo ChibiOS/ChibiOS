8cd9954ec674d2fe29620c292355562d554c5fd4
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2010-02-21 11:35:10 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@1652 35acf78f-673a-0410-8e92-d51de3d6d3f4

---
+++ os/hal/platforms/Posix/platform.mk
@@ -0,0 +1,7 @@
+# List of all the Posix platform files.
+PLATFORMSRC = ${CHIBIOS}/os/hal/platforms/Linux/hal_lld.c \
+               ${CHIBIOS}/os/hal/platforms/Linux/pal_lld.c \
+              ${CHIBIOS}/os/hal/platforms/Linux/serial_lld.c
+
+# Required include directories
+PLATFORMINC = ${CHIBIOS}/os/hal/platforms/Linux
