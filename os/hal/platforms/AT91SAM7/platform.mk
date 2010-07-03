7f17ad48b28a21af45f5b10ea3558392647ef179
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2010-07-03 07:17:29 +0000

    AT91SAM7 SPI driver contributed by Alexander.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@2049 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/platforms/AT91SAM7/platform.mk
+++ os/hal/platforms/AT91SAM7/platform.mk
@@ -2,6 +2,7 @@
 PLATFORMSRC = ${CHIBIOS}/os/hal/platforms/AT91SAM7/hal_lld.c \
               ${CHIBIOS}/os/hal/platforms/AT91SAM7/pal_lld.c \
               ${CHIBIOS}/os/hal/platforms/AT91SAM7/serial_lld.c \
+              ${CHIBIOS}/os/hal/platforms/AT91SAM7/spi_lld.c \
               ${CHIBIOS}/os/hal/platforms/AT91SAM7/mac_lld.c \
               ${CHIBIOS}/os/hal/platforms/AT91SAM7/at91sam7_mii.c \
               ${CHIBIOS}/os/hal/platforms/AT91SAM7/at91lib/aic.c
