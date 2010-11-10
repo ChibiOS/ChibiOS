c48b251d9100586a4a606ae5da65a43f7b6ee9d6
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2010-11-10 19:15:09 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@2343 35acf78f-673a-0410-8e92-d51de3d6d3f4

---
+++ demos/STM8L-STM8L152-DISCOVERY-STVD/demo/main.c
@@ -0,0 +1,75 @@
+/*
+    ChibiOS/RT - Copyright (C) 2006,2007,2008,2009,2010 Giovanni Di Sirio.
+
+    This file is part of ChibiOS/RT.
+
+    ChibiOS/RT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    ChibiOS/RT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "ch.h"
+#include "hal.h"
+#include "test.h"
+
+/*
+ * LEDs blinker thread, times are in milliseconds.
+ */
+static WORKING_AREA(waThread1, 64);
+static msg_t Thread1(void *arg) {
+
+  (void)arg;
+  while (TRUE) {
+    palClearPad(GPIOD, PD_LD10);
+    chThdSleepMilliseconds(500);
+    palSetPad(GPIOD, PD_LD10);
+    chThdSleepMilliseconds(500);
+  }
+  return 0;
+}
+
+/*
+ * Entry point.
+ */
+void main(void) {
+
+  /*
+   * Board/HAL initialization.
+   */
+  hwinit();
+
+  /*
+   * OS initialization.
+   */
+  chSysInit();
+
+  /*
+   * Activates the serial driver 1 using the driver default configuration.
+   */
+  sdStart(&SD2, NULL);
+
+  /*
+   * Creates the blinker thread.
+   */
+  chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
+
+  /*
+   * Normal main() thread activity.
+   */
+  while (TRUE) {
+    if (palReadPad(GPIOG, 0) == PAL_LOW)
+      TestThread(&SD2);
+    if (palReadPad(GPIOG, 1) == PAL_LOW)
+      sdWriteTimeout(&SD2, "Hello World!\r\n", 14, TIME_INFINITE);
+    chThdSleepMilliseconds(1000);
+  }
+}
