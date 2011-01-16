2c03b9f784b0689d32f2751d25e8af5df98cd206
Giovanni Di Sirio <gdisirio@gmail.com>
2011-01-16 10:45:21 +0000

    License exception added.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_2.2.x@2654 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/ports/IAR/ARMCMx/chcoreasm_v6m.s
+++ os/ports/IAR/ARMCMx/chcoreasm_v6m.s
@@ -1,5 +1,5 @@
 /*
-    ChibiOS/RT - Copyright (C) 2006,2007,2008,2009,2010 Giovanni Di Sirio.
+    ChibiOS/RT - Copyright (C) 2006,2007,2008,2009,2010,2011 Giovanni Di Sirio.
 
     This file is part of ChibiOS/RT.
 
@@ -10,11 +10,18 @@
 
     ChibiOS/RT is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+                                      ---
+
+    A special exception to the GPL can be applied should you wish to distribute
+    a combined work that includes ChibiOS/RT, without being obliged to provide
+    the source code for any proprietary components. See the file exception.txt
+    for full details of how and when the exception can be applied.
 */
 
         MODULE  ?chcoreasm_v6m
