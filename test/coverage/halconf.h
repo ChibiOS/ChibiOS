4eb94d8a4e1c8dec5220b8cddb96844f32ef3118
Giovanni Di Sirio <gdisirio@gmail.com>
2011-01-16 10:19:39 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_2.2.x@2653 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- test/coverage/halconf.h
+++ test/coverage/halconf.h
@@ -55,13 +55,6 @@
 #endif
 
 /**
- * @brief   Enables the I2C subsystem.
- */
-#if !defined(HAL_USE_I2C) || defined(__DOXYGEN__)
-#define HAL_USE_I2C                 FALSE
-#endif
-
-/**
  * @brief   Enables the MAC subsystem.
  */
 #if !defined(HAL_USE_MAC) || defined(__DOXYGEN__)
