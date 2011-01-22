23267b6b1aea7308d6ccf51780c66227325334e6
Giovanni Di Sirio <gdisirio@gmail.com>
2011-01-22 09:54:18 +0000

    Backported fix for bug 3163851.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_2.2.x@2665 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/hal/platforms/STM32/serial_lld.c
+++ os/hal/platforms/STM32/serial_lld.c
@@ -105,10 +105,10 @@ static void usart_init(SerialDriver *sdp, const SerialConfig *config) {
    * Note that some bits are enforced.
    */
   u->CR1 = config->sc_cr1 | USART_CR1_UE | USART_CR1_PEIE |
-                            USART_CR1_RXNEIE | USART_CR1_TE |
-                            USART_CR1_RE;
+                            USART_CR1_RXNEIE | USART_CR1_RE;
   u->CR2 = config->sc_cr2 | USART_CR2_LBDIE;
   u->CR3 = config->sc_cr3 | USART_CR3_EIE;
+  u->SR = 0;
   (void)u->SR;  /* SR reset step 1.*/
   (void)u->DR;  /* SR reset step 2.*/
 }
@@ -182,8 +182,10 @@ static void serve_interrupt(SerialDriver *sdp) {
       chIOAddFlagsI(sdp, IO_OUTPUT_EMPTY);
       u->CR1 = cr1 & ~USART_CR1_TXEIE;
     }
-    else
+    else {
       u->DR = b;
+      u->CR1 = cr1 | USART_CR1_TE;
+    }
     chSysUnlockFromIsr();
   }
 }
