e7e79a6ccb4f3e320b2b8b7bad1b14d65218641d
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2011-03-18 18:38:08 +0000

    License updated.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@2827 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/templates/usb_lld.c
+++ os/hal/templates/usb_lld.c
@@ -1,5 +1,6 @@
 /*
-    ChibiOS/RT - Copyright (C) 2006,2007,2008,2009,2010 Giovanni Di Sirio.
+    ChibiOS/RT - Copyright (C) 2006,2007,2008,2009,2010,
+                 2011 Giovanni Di Sirio.
 
     This file is part of ChibiOS/RT.
 
