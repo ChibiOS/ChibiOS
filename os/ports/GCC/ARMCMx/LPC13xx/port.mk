1fcdf9b0f78be59eb18b5d0f3d285229bb665fbe
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2011-04-17 18:29:20 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@2886 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/ports/GCC/ARMCMx/LPC13xx/port.mk
+++ os/ports/GCC/ARMCMx/LPC13xx/port.mk
@@ -1,10 +1,11 @@
 # List of the ChibiOS/RT Cortex-M0 LPC13xx port files.
-PORTSRC = $(CHIBIOS)/os/ports/GCC/ARMCMx/LPC13xx/vectors.c \
+PORTSRC = $(CHIBIOS)/os/ports/GCC/ARMCMx/crt0.c \
+          $(CHIBIOS)/os/ports/GCC/ARMCMx/LPC13xx/vectors.c \
           ${CHIBIOS}/os/ports/GCC/ARMCMx/chcore.c \
           ${CHIBIOS}/os/ports/GCC/ARMCMx/chcore_v7m.c \
           ${CHIBIOS}/os/ports/GCC/ARMCMx/nvic.c
 
-PORTASM = ${CHIBIOS}/os/ports/GCC/ARMCMx/crt0_v7m.s
+PORTASM =
 
 PORTINC = ${CHIBIOS}/os/ports/GCC/ARMCMx \
           ${CHIBIOS}/os/ports/GCC/ARMCMx/LPC13xx
