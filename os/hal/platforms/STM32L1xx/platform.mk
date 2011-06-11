3cdb6f6ed629b9fd98e6c490d77bdecf5cc34685
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2011-06-11 10:49:51 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@3041 35acf78f-673a-0410-8e92-d51de3d6d3f4

---
+++ os/hal/platforms/STM32L1xx/platform.mk
@@ -0,0 +1,5 @@
+# List of all the STM32 platform files.
+PLATFORMSRC = ${CHIBIOS}/os/hal/platforms/STM32L1xx/hal_lld.c
+
+# Required include directories
+PLATFORMINC = ${CHIBIOS}/os/hal/platforms/STM32L1xx
