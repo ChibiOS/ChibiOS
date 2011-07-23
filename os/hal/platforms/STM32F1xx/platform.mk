8f41aa3029615548dfd7c1e22c0a47445caada7b
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2011-07-23 07:54:31 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@3171 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/platforms/STM32F1xx/platform.mk
+++ os/hal/platforms/STM32F1xx/platform.mk
@@ -1,17 +1,21 @@
-# List of all the STM32 platform files.
-PLATFORMSRC = ${CHIBIOS}/os/hal/platforms/STM32/hal_lld.c \
-              ${CHIBIOS}/os/hal/platforms/STM32/adc_lld.c \
+# List of all the STM32F1xx platform files.
+PLATFORMSRC = ${CHIBIOS}/os/hal/platforms/STM32F1xx/hal_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/can_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/gpt_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/icu_lld.c \
-              ${CHIBIOS}/os/hal/platforms/STM32/pal_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/pwm_lld.c \
-              ${CHIBIOS}/os/hal/platforms/STM32/sdc_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/serial_lld.c \
-              ${CHIBIOS}/os/hal/platforms/STM32/spi_lld.c \
-              ${CHIBIOS}/os/hal/platforms/STM32/uart_lld.c \
-              ${CHIBIOS}/os/hal/platforms/STM32/usb_lld.c \
-              ${CHIBIOS}/os/hal/platforms/STM32/stm32_dma.c
+              ${CHIBIOS}/os/hal/platforms/STM32/GPIOv1/pal_lld.c \
+              ${CHIBIOS}/os/hal/platforms/STM32/DMAv1/adc_lld.c \
+              ${CHIBIOS}/os/hal/platforms/STM32/DMAv1/sdc_lld.c \
+              ${CHIBIOS}/os/hal/platforms/STM32/DMAv1/spi_lld.c \
+              ${CHIBIOS}/os/hal/platforms/STM32/DMAv1/uart_lld.c \
+              ${CHIBIOS}/os/hal/platforms/STM32/DMAv1/stm32_dma.c \
+              ${CHIBIOS}/os/hal/platforms/STM32/USBv1/usb_lld.c
 
 # Required include directories
-PLATFORMINC = ${CHIBIOS}/os/hal/platforms/STM32
+PLATFORMINC = ${CHIBIOS}/os/hal/platforms/STM32F1xx \
+              ${CHIBIOS}/os/hal/platforms/STM32 \
+              ${CHIBIOS}/os/hal/platforms/STM32/GPIOv1 \
+              ${CHIBIOS}/os/hal/platforms/STM32/DMAv1 \
+              ${CHIBIOS}/os/hal/platforms/STM32/USBv1
