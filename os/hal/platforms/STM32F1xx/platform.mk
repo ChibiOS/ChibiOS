3602b6e67dbae804da81736550b21a0ddb159c0a
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2011-07-23 17:21:36 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@3176 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/platforms/STM32F1xx/platform.mk
+++ os/hal/platforms/STM32F1xx/platform.mk
@@ -1,12 +1,12 @@
 # List of all the STM32F1xx platform files.
 PLATFORMSRC = ${CHIBIOS}/os/hal/platforms/STM32F1xx/hal_lld.c \
+              ${CHIBIOS}/os/hal/platforms/STM32F1xx/adc_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/can_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/gpt_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/icu_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/pwm_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/serial_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/GPIOv1/pal_lld.c \
-              ${CHIBIOS}/os/hal/platforms/STM32/DMAv1/adc_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/DMAv1/sdc_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/DMAv1/spi_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/DMAv1/uart_lld.c \
