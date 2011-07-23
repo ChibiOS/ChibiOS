b774805ce99c89b28972705ce86a8a9b9f7a2173
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2011-07-23 09:04:29 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@3174 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/platforms/STM32L1xx/platform.mk
+++ os/hal/platforms/STM32L1xx/platform.mk
@@ -1,7 +1,19 @@
-# List of all the STM32 platform files.
+# List of all the STM32L1xx platform files.
 PLATFORMSRC = ${CHIBIOS}/os/hal/platforms/STM32L1xx/hal_lld.c \
-              ${CHIBIOS}/os/hal/platforms/STM32L1xx/pal_lld.c \
-              ${CHIBIOS}/os/hal/platforms/STM32L1xx/stm32_dma.c
+              ${CHIBIOS}/os/hal/platforms/STM32/gpt_lld.c \
+              ${CHIBIOS}/os/hal/platforms/STM32/icu_lld.c \
+              ${CHIBIOS}/os/hal/platforms/STM32/pwm_lld.c \
+              ${CHIBIOS}/os/hal/platforms/STM32/serial_lld.c \
+              ${CHIBIOS}/os/hal/platforms/STM32/GPIOv2/pal_lld.c \
+              ${CHIBIOS}/os/hal/platforms/STM32/DMAv1/adc_lld.c \
+              ${CHIBIOS}/os/hal/platforms/STM32/DMAv1/spi_lld.c \
+              ${CHIBIOS}/os/hal/platforms/STM32/DMAv1/uart_lld.c \
+              ${CHIBIOS}/os/hal/platforms/STM32/DMAv1/stm32_dma.c \
+              ${CHIBIOS}/os/hal/platforms/STM32/USBv1/usb_lld.c
 
 # Required include directories
-PLATFORMINC = ${CHIBIOS}/os/hal/platforms/STM32L1xx
+PLATFORMINC = ${CHIBIOS}/os/hal/platforms/STM32L1xx \
+              ${CHIBIOS}/os/hal/platforms/STM32 \
+              ${CHIBIOS}/os/hal/platforms/STM32/GPIOv2 \
+              ${CHIBIOS}/os/hal/platforms/STM32/DMAv1 \
+              ${CHIBIOS}/os/hal/platforms/STM32/USBv1
