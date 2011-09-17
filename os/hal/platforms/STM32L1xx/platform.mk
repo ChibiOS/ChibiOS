17ab902203a60ac1c70aebf41facc0f278babfd5
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2011-09-17 09:16:10 +0000

    STM32L152 report added.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@3325 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/platforms/STM32L1xx/platform.mk
+++ os/hal/platforms/STM32L1xx/platform.mk
@@ -1,12 +1,12 @@
 # List of all the STM32L1xx platform files.
 PLATFORMSRC = ${CHIBIOS}/os/hal/platforms/STM32L1xx/hal_lld.c \
+              ${CHIBIOS}/os/hal/platforms/STM32/GPIOv2/pal_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/gpt_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/icu_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/pwm_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/serial_lld.c \
-              ${CHIBIOS}/os/hal/platforms/STM32/GPIOv2/pal_lld.c \
-              ${CHIBIOS}/os/hal/platforms/STM32/DMAv1/spi_lld.c \
-              ${CHIBIOS}/os/hal/platforms/STM32/DMAv1/uart_lld.c \
+              ${CHIBIOS}/os/hal/platforms/STM32/spi_lld.c \
+              ${CHIBIOS}/os/hal/platforms/STM32/uart_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/DMAv1/stm32_dma.c \
               ${CHIBIOS}/os/hal/platforms/STM32/USBv1/usb_lld.c
 
