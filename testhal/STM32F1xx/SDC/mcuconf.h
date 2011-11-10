97d1a377042e1847cb45adeafac98fedf97ccefe
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2011-11-10 20:27:29 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@3484 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- testhal/STM32F1xx/SDC/mcuconf.h
+++ testhal/STM32F1xx/SDC/mcuconf.h
@@ -138,11 +138,13 @@
 #define STM32_SERIAL_USE_USART3             FALSE
 #define STM32_SERIAL_USE_UART4              FALSE
 #define STM32_SERIAL_USE_UART5              FALSE
+#define STM32_SERIAL_USE_USART6             FALSE
 #define STM32_SERIAL_USART1_PRIORITY        12
 #define STM32_SERIAL_USART2_PRIORITY        12
 #define STM32_SERIAL_USART3_PRIORITY        12
 #define STM32_SERIAL_UART4_PRIORITY         12
 #define STM32_SERIAL_UART5_PRIORITY         12
+#define STM32_SERIAL_USART6_PRIORITY        12
 
 /*
  * SPI driver system settings.
