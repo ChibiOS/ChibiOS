e6b38cdd3741b822c3be51aba268f90fc1771032
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2011-11-24 21:00:24 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@3524 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/platforms/STM32L1xx/adc_lld.c
+++ os/hal/platforms/STM32L1xx/adc_lld.c
@@ -162,10 +162,10 @@ void adc_lld_start(ADCDriver *adcp) {
     }
 #endif /* STM32_ADC_USE_ADC1 */
 
-
     /* ADC initial setup, starting the analog part here in order to reduce
        the latency when starting a conversion.*/
     adcp->adc->CR1 = 0;
+    adcp->adc->CR2 = 0;
     adcp->adc->CR2 = ADC_CR2_ADON;
   }
 }
