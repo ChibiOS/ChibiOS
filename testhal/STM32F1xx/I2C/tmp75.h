a012c230787b1621f90dc43641bf83c5adcced5d
Giovanni Di Sirio <gdisirio@gmail.com>
2012-01-28 15:39:37 +0000

    License switch, release candidate 1.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_2.4.x@3891 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- testhal/STM32F1xx/I2C/tmp75.h
+++ testhal/STM32F1xx/I2C/tmp75.h
@@ -16,6 +16,13 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+                                      ---
+
+    A special exception to the GPL can be applied should you wish to distribute
+    a combined work that includes ChibiOS/RT, without being obliged to provide
+    the source code for any proprietary components. See the file exception.txt
+    for full details of how and when the exception can be applied.
 */
 
 #ifndef TMP75_H_
