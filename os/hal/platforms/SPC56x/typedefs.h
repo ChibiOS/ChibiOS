9d73ed2045a86fa52f115009aa2591181829c978
Giovanni Di Sirio <gdisirio@gmail.com>
2012-03-17 06:59:47 +0000

    License updated.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_2.2.x@4045 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/hal/platforms/SPC56x/typedefs.h
+++ os/hal/platforms/SPC56x/typedefs.h
@@ -1,5 +1,6 @@
 /*
-    ChibiOS/RT - Copyright (C) 2006,2007,2008,2009,2010,2011 Giovanni Di Sirio.
+    ChibiOS/RT - Copyright (C) 2006,2007,2008,2009,2010,
+                 2011,2012 Giovanni Di Sirio.
 
     This file is part of ChibiOS/RT.
 
@@ -10,11 +11,11 @@
 
     ChibiOS/RT is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program. If not, see <http://www.gnu.org/licenses/>.
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
                                       ---
 
