31fa09fa03a56d51ee4a9fdbf9eb70262beccb57
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2012-04-21 17:26:34 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@4118 35acf78f-673a-0410-8e92-d51de3d6d3f4

---
+++ os/various/fatfs_bindings/fatfs.mk
@@ -0,0 +1,7 @@
+# FATFS files.
+FATFSSRC = ${CHIBIOS}/various/fatfs_bindings/fatfs_diskio.c \
+           ${CHIBIOS}/various/fatfs_bindings/fatfs_syscall.c \
+           ${CHIBIOS}/ext/fatfs/src/ff.c \
+           ${CHIBIOS}/ext/fatfs/src/option/ccsbcs.c \
+
+FATFSINC = ${CHIBIOS}/ext/fatfs/src
