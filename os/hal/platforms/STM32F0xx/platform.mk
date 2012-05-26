512b78de728dfa1fe6bacadc525c8edc1dfdb041
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2012-05-26 11:07:27 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@4234 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/platforms/STM32F0xx/platform.mk
+++ os/hal/platforms/STM32F0xx/platform.mk
@@ -1,5 +1,6 @@
 # List of all the STM32F1xx platform files.
 PLATFORMSRC = ${CHIBIOS}/os/hal/platforms/STM32F0xx/stm32_dma.c \
+              ${CHIBIOS}/os/hal/platforms/STM32F0xx/adc_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32F0xx/hal_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/GPIOv2/pal_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/serial_lld.c	\
