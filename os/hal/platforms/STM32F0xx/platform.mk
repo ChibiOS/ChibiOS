ae3db0129bf8fe34a660388e5066fb1328ba9e8a
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2012-06-23 17:52:27 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@4334 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/platforms/STM32F0xx/platform.mk
+++ os/hal/platforms/STM32F0xx/platform.mk
@@ -10,7 +10,7 @@ PLATFORMSRC = ${CHIBIOS}/os/hal/platforms/STM32F0xx/stm32_dma.c \
               ${CHIBIOS}/os/hal/platforms/STM32/serial_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/spi_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/GPIOv2/pal_lld.c \
-              ${CHIBIOS}/os/hal/platforms/STM32/USARTv2\uart_lld.c
+              ${CHIBIOS}/os/hal/platforms/STM32/USARTv2/uart_lld.c
 
 # Required include directories
 PLATFORMINC = ${CHIBIOS}/os/hal/platforms/STM32F0xx \
