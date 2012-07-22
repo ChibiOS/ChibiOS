0c66e8a71b78e9ff5313809c36e9af2a64de9215
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2012-07-22 16:26:21 +0000

    Fixed bug 3547297.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@4477 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/platforms/STM32/USARTv1/uart_lld.c
+++ os/hal/platforms/STM32/USARTv1/uart_lld.c
@@ -175,7 +175,11 @@ static void usart_start(UARTDriver *uartp) {
   usart_stop(uartp);
 
   /* Baud rate setting.*/
+#if STM32_HAS_USART6
   if ((uartp->usart == USART1) || (uartp->usart == USART6))
+#else
+  if (uartp->usart == USART1)
+#endif
     u->BRR = STM32_PCLK2 / uartp->config->speed;
   else
     u->BRR = STM32_PCLK1 / uartp->config->speed;
