d672c60a80d0a00473a4c86fe5ab44f34730b36f
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2012-11-10 08:23:59 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@4810 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- demos/MSP430-MSP430F5437/chconf.h
+++ demos/MSP430-MSP430F5437/chconf.h
@@ -16,18 +16,11 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-                                      ---
-
-    A special exception to the GPL can be applied should you wish to distribute
-    a combined work that includes ChibiOS/RT, without being obliged to provide
-    the source code for any proprietary components. See the file exception.txt
-    for full details of how and when the exception can be applied.
 */
 
 /**
- * @file    chconf.h
- * @brief   Configuration file.
+ * @file    templates/chconf.h
+ * @brief   Configuration file template.
  * @details A copy of this file must be placed in each project directory, it
  *          contains the application specific kernel settings.
  *
