2002722c01b9c1ca55a7800cca8946dc6c8d7f98
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2012-11-11 09:37:58 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@4814 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- boards/NONSTANDARD_MSP430_F5437/board.mk
+++
@@ -1,5 +0,0 @@
-# List of all the board related files.
-BOARDSRC = ${CHIBIOS}/boards/NONSTANDARD_MSP430_F5437/board.c
-
-# Required include directories
-BOARDINC = ${CHIBIOS}/boards/NONSTANDARD_MSP430_F5437
--- os/ports/common/MSP430X/port.mk
+++
@@ -1,6 +0,0 @@
-# List of the ChibiOS/RT MSP430 port files.
-PORTSRC = ${CHIBIOS}/os/ports/common/MSP430X/chcore.c
-
-PORTASM = 
-
-PORTINC = ${CHIBIOS}/os/ports/common/MSP430X
