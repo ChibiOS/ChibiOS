3880e7e17858dee2d75eca54d623ad610329ef60
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2012-11-27 10:14:29 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@4838 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/platforms/SPC5xx/SIUL_v1/pal_lld.h
+++ os/hal/platforms/SPC5xx/SIUL_v1/pal_lld.h
@@ -1,22 +1,16 @@
 /*
-    ChibiOS/RT - Copyright (C) 2006,2007,2008,2009,2010,
-                 2011,2012 Giovanni Di Sirio.
-
-    This file is part of ChibiOS/RT.
-
-    ChibiOS/RT is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 3 of the License, or
-    (at your option) any later version.
-
-    ChibiOS/RT is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Licensed under ST Liberty SW License Agreement V2, (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *        http://www.st.com/software_license_agreement_liberty_v2
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 /**
  * @file    SPC5xx/SIU_v1//pal_lld.h
