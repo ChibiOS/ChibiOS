2e285c54de4f269970482d3338042aa62adb765a
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2012-11-28 15:05:43 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@4843 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- boards/GENERIC_SPC560BC/board.mk
+++ boards/GENERIC_SPC560BC/board.mk
@@ -1,5 +1,5 @@
 # List of all the board related files.
-BOARDSRC = ${CHIBIOS}/boards/GENERIC_SPC560B/board.c
+BOARDSRC = ${CHIBIOS}/boards/GENERIC_SPC560BC/board.c
 
 # Required include directories
-BOARDINC = ${CHIBIOS}/boards/GENERIC_SPC560B
+BOARDINC = ${CHIBIOS}/boards/GENERIC_SPC560BC
