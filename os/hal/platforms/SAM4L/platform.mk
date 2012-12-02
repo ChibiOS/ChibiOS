6f70b6257b72346ea8856053dcf3af1e46fc937b
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2012-12-02 08:30:37 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@4862 35acf78f-673a-0410-8e92-d51de3d6d3f4

---
+++ os/hal/platforms/SAM4L/platform.mk
@@ -0,0 +1,6 @@
+# List of all the STM32F3xx platform files.
+PLATFORMSRC = ${CHIBIOS}/os/hal/platforms/SAM4L/hal_lld.c
+
+# Required include directories
+PLATFORMINC = ${CHIBIOS}/os/hal/platforms/SAM4L
+
