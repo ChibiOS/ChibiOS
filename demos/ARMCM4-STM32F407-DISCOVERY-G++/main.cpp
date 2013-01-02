0d3c7da33152ace5422f79fae6ce757d7328c32c
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2013-01-02 10:51:52 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@5014 35acf78f-673a-0410-8e92-d51de3d6d3f4

---
+++ demos/ARMCM4-STM32F407-DISCOVERY-G++/main.cpp
@@ -0,0 +1,169 @@
+/*
+    ChibiOS/RT - Copyright (C) 2006,2007,2008,2009,2010,
+                 2011,2012 Giovanni Di Sirio.
+
+    This file is part of ChibiOS/RT.
+
+    ChibiOS/RT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    ChibiOS/RT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "ch.hpp"
+#include "hal.h"
+#include "test.h"
+#include "evtimer.h"
+
+using namespace chibios_rt;
+
+/*
+ * LED blink sequences.
+ * NOTE: Sequences must always be terminated by a GOTO instruction.
+ * NOTE: The sequencer language could be easily improved but this is outside
+ *       the scope of this demo.
+ */
+#define SLEEP           0
+#define GOTO            1
+#define STOP            2
+#define BITCLEAR        3
+#define BITSET          4
+
+typedef struct {
+  uint8_t       action;
+  uint32_t      value;
+} seqop_t;
+
+// Flashing sequence for LED4.
+static const seqop_t LED4_sequence[] =
+{
+  {BITCLEAR, PAL_PORT_BIT(GPIOD_LED4)},
+  {SLEEP,    200},
+  {BITSET,   PAL_PORT_BIT(GPIOD_LED4)},
+  {SLEEP,    1800},
+  {GOTO,     0}
+};
+
+// Flashing sequence for LED3.
+static const seqop_t LED3_sequence[] =
+{
+  {SLEEP,    1000},
+  {BITCLEAR, PAL_PORT_BIT(GPIOD_LED3)},
+  {SLEEP,    1800},
+  {BITSET,   PAL_PORT_BIT(GPIOD_LED3)},
+  {SLEEP,    200},
+  {GOTO,     1}
+};
+
+/*
+ * Sequencer thread class. It can drive LEDs or other output pins.
+ * Any sequencer is just an instance of this class, all the details are
+ * totally encapsulated and hidden to the application level.
+ */
+class SequencerThread : public BaseStaticThread<128> {
+private:
+  const seqop_t *base, *curr;                   // Thread local variables.
+
+protected:
+  virtual msg_t Main(void) {
+
+    setName("sequencer");
+
+    while (true) {
+      switch(curr->action) {
+      case SLEEP:
+        sleep(curr->value);
+        break;
+      case GOTO:
+        curr = &base[curr->value];
+        continue;
+      case STOP:
+        return 0;
+      case BITCLEAR:
+        palClearPort(GPIOD, curr->value);
+        break;
+      case BITSET:
+        palSetPort(GPIOD, curr->value);
+        break;
+      }
+      curr++;
+    }
+  }
+
+public:
+  SequencerThread(const seqop_t *sequence) : BaseStaticThread<128>() {
+
+    base = curr = sequence;
+  }
+};
+
+/*
+ * Tester thread class. This thread executes the test suite.
+ */
+class TesterThread : public BaseStaticThread<256> {
+
+protected:
+  virtual msg_t Main(void) {
+
+    setName("tester");
+
+    return TestThread(&SD2);
+  }
+
+public:
+  TesterThread(void) : BaseStaticThread<256>() {
+  }
+};
+
+/* Static threads instances.*/
+static TesterThread tester;
+static SequencerThread blinker1(LED3_sequence);
+static SequencerThread blinker2(LED4_sequence);
+
+/*
+ * Application entry point.
+ */
+int main(void) {
+
+  /*
+   * System initializations.
+   * - HAL initialization, this also initializes the configured device drivers
+   *   and performs the board-specific initializations.
+   * - Kernel initialization, the main() function becomes a thread and the
+   *   RTOS is active.
+   */
+  halInit();
+  System::init();
+
+  /*
+   * Activates the serial driver 2 using the driver default configuration.
+   */
+  sdStart(&SD2, NULL);
+
+  /*
+   * Starts several instances of the SequencerThread class, each one operating
+   * on a different LED.
+   */
+  blinker1.start(NORMALPRIO + 10);
+  blinker2.start(NORMALPRIO + 10);
+
+  /*
+   * Serves timer events.
+   */
+  while (true) {
+    if (palReadPad(GPIOA, GPIOA_BUTTON)) {
+      tester.start(NORMALPRIO);
+      tester.wait();
+    };
+  }
+
+  return 0;
+}
