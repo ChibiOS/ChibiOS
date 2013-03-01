8e8017b4c6c8d014131e8a170628b356e8ac4c0e
theShed <theShed@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2013-03-01 20:57:03 +0000

    Added LPC11Uxx ext_lld driver
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@5342 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/platforms/LPC11Uxx/platform.mk
+++ os/hal/platforms/LPC11Uxx/platform.mk
@@ -3,7 +3,9 @@ PLATFORMSRC = ${CHIBIOS}/os/hal/platforms/LPC11Uxx/hal_lld.c \
               ${CHIBIOS}/os/hal/platforms/LPC11Uxx/gpt_lld.c \
               ${CHIBIOS}/os/hal/platforms/LPC11Uxx/pal_lld.c \
               ${CHIBIOS}/os/hal/platforms/LPC11Uxx/spi_lld.c \
-              ${CHIBIOS}/os/hal/platforms/LPC11Uxx/serial_lld.c
+              ${CHIBIOS}/os/hal/platforms/LPC11Uxx/serial_lld.c \
+              ${CHIBIOS}/os/hal/platforms/LPC11Uxx/ext_lld.c \
+              ${CHIBIOS}/os/hal/platforms/LPC11Uxx/ext_lld_isr.c 
 
 # Required include directories
 PLATFORMINC = ${CHIBIOS}/os/hal/platforms/LPC11Uxx
