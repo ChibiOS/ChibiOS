0424e11412d67f90d39bc1a9a090dfc70a1093cd
theShed <theShed@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2013-04-02 14:01:52 +0000

    Add os/hal/platforms/LPC8xx/spi_lld
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@5534 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/platforms/LPC8xx/platform.mk
+++ os/hal/platforms/LPC8xx/platform.mk
@@ -3,6 +3,7 @@ PLATFORMSRC = ${CHIBIOS}/os/hal/platforms/LPC8xx/hal_lld.c \
               ${CHIBIOS}/os/hal/platforms/LPC8xx/gpt_lld.c \
               ${CHIBIOS}/os/hal/platforms/LPC8xx/pal_lld.c \
               ${CHIBIOS}/os/hal/platforms/LPC8xx/serial_lld.c \
+              ${CHIBIOS}/os/hal/platforms/LPC8xx/spi_lld.c \
               ${CHIBIOS}/os/hal/platforms/LPC8xx/ext_lld.c \
               ${CHIBIOS}/os/hal/platforms/LPC8xx/ext_lld_isr.c 
 
