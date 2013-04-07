098b67f2c5739fe3c8c5e22d9067f91dd35c01a9
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2013-04-07 07:43:23 +0000

    Updated mcuconf.h files for F0xx, F30x, F37x.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@5552 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- testhal/STM32F30x/USB_CDC/mcuconf.h
+++ testhal/STM32F30x/USB_CDC/mcuconf.h
@@ -122,6 +122,17 @@
 #define STM32_GPT_TIM8_IRQ_PRIORITY         7
 
 /*
+ * I2C driver system settings.
+ */
+#define STM32_I2C_USE_I2C1                  FALSE
+#define STM32_I2C_USE_I2C2                  FALSE
+#define STM32_I2C_I2C1_IRQ_PRIORITY         10
+#define STM32_I2C_I2C2_IRQ_PRIORITY         10
+#define STM32_I2C_I2C1_DMA_PRIORITY         1
+#define STM32_I2C_I2C2_DMA_PRIORITY         1
+#define STM32_I2C_DMA_ERROR_HOOK(i2cp)      chSysHalt()
+
+/*
  * ICU driver system settings.
  */
 #define STM32_ICU_USE_TIM1                  FALSE
