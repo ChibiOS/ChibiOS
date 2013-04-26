e396a6007e50d7a082597275fbdef20a025cde5f
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2013-04-26 10:49:48 +0000

    White space fix.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@5625 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/kernel/include/ch.h
+++ os/kernel/include/ch.h
@@ -69,14 +69,14 @@
  * @brief   Generic 'false' boolean constant.
  */
 #if !defined(FALSE) || defined(__DOXYGEN__)
-#define FALSE       false
+#define FALSE                   false
 #endif
 
 /**
  * @brief   Generic 'true' boolean constant.
  */
 #if !defined(TRUE) || defined(__DOXYGEN__)
-#define TRUE        true
+#define TRUE                    true
 #endif
 
 /**
