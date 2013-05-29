37034c5ecf6ac44a7f65ec4e201c3ad551030495
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2013-05-29 10:26:01 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@5770 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- testhal/SPC563Mxx/SPI/mcuconf.h
+++ testhal/SPC563Mxx/SPI/mcuconf.h
@@ -92,4 +92,6 @@
 #define SPC5_SPI_DSPI2_DMA_PRIO             10
 #define SPC5_SPI_DSPI1_DMA_IRQ_PRIO         10
 #define SPC5_SPI_DSPI2_DMA_IRQ_PRIO         10
+#define SPC5_SPI_DSPI1_IRQ_PRIO             10
+#define SPC5_SPI_DSPI2_IRQ_PRIO             10
 #define SPC5_SPI_DMA_ERROR_HOOK(spip)       chSysHalt()
