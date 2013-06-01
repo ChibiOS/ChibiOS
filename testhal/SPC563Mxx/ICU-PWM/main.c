bea1f59f1407b52fd68727c665723105dbb8f715
pcirillo <pcirillo@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2013-06-01 14:36:29 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@5789 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- testhal/SPC563Mxx/ICU-PWM/main.c
+++ testhal/SPC563Mxx/ICU-PWM/main.c
@@ -1,21 +1,21 @@
 /*
- * Licensed under ST Liberty SW License Agreement V2, (the "License");
- * You may not use this file except in compliance with the License.
- * You may obtain a copy of the License at:
- *
- *        http://www.st.com/software_license_agreement_liberty_v2
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+    ChibiOS/RT - Copyright (C) 2006-2013 Giovanni Di Sirio
 
-/* Inclusion of the main header files of all the imported components in the
-   order specified in the application wizard. The file is generated
-   automatically.*/
-#include "components.h"
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include "ch.h"
+#include "hal.h"
 
 static void pwmpcb(PWMDriver *pwmp) {
 
