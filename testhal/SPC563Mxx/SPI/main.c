cb0364f42a9df0ca6bce07bdc51e0e1d99e9451f
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2013-06-03 08:58:07 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@5802 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- testhal/SPC563Mxx/SPI/main.c
+++ testhal/SPC563Mxx/SPI/main.c
@@ -26,7 +26,7 @@ static const SPIConfig hs_spicfg = {
   0,
   SPC5_MCR_PCSIS0,                                              /* MCR.     */
   SPC5_CTAR_CSSCK_DIV2 | SPC5_CTAR_ASC_DIV2 | SPC5_CTAR_FMSZ(8) |
-  SPC5_CTAR_PBR_PRE2 | SPC5_CTAR_BR_DIV2,                     /* CTAR0.   */
+  SPC5_CTAR_PBR_PRE2 | SPC5_CTAR_BR_DIV2,                       /* CTAR0.   */
   SPC5_PUSHR_CONT | SPC5_PUSHR_PCS(0)                           /* PUSHR.   */
 };
 
