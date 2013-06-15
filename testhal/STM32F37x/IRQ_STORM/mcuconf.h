e02105736dcf810de5855bc5a5fd40bf7727c852
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2013-06-15 16:41:36 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@5857 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- testhal/STM32F37x/IRQ_STORM/mcuconf.h
+++ testhal/STM32F37x/IRQ_STORM/mcuconf.h
@@ -105,16 +105,16 @@
 /*
  * GPT driver system settings.
  */
-#define STM32_GPT_USE_TIM2                  FALSE
-#define STM32_GPT_USE_TIM3                  FALSE
-#define STM32_GPT_USE_TIM4                  FALSE
-#define STM32_GPT_USE_TIM5                  FALSE
-#define STM32_GPT_USE_TIM6                  FALSE
-#define STM32_GPT_USE_TIM7                  FALSE
-#define STM32_GPT_USE_TIM12                 FALSE
-#define STM32_GPT_USE_TIM14                 FALSE
-#define STM32_GPT_TIM2_IRQ_PRIORITY         7
-#define STM32_GPT_TIM3_IRQ_PRIORITY         7
+#define STM32_GPT_USE_TIM2                  TRUE
+#define STM32_GPT_USE_TIM3                  TRUE
+#define STM32_GPT_USE_TIM4                  TRUE
+#define STM32_GPT_USE_TIM5                  TRUE
+#define STM32_GPT_USE_TIM6                  TRUE
+#define STM32_GPT_USE_TIM7                  TRUE
+#define STM32_GPT_USE_TIM12                 TRUE
+#define STM32_GPT_USE_TIM14                 TRUE
+#define STM32_GPT_TIM2_IRQ_PRIORITY         6
+#define STM32_GPT_TIM3_IRQ_PRIORITY         10
 #define STM32_GPT_TIM4_IRQ_PRIORITY         7
 #define STM32_GPT_TIM5_IRQ_PRIORITY         7
 #define STM32_GPT_TIM6_IRQ_PRIORITY         7
