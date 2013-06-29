0f13eed26d4c66d51715abcf059dfca3e3f4b722
utzig <utzig@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2013-06-29 13:59:41 +0000

    Add initial GPT driver for AVR
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@5906 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/platforms/AVR/platform.mk
+++ os/hal/platforms/AVR/platform.mk
@@ -4,7 +4,8 @@ PLATFORMSRC = ${CHIBIOS}/os/hal/platforms/AVR/hal_lld.c \
               ${CHIBIOS}/os/hal/platforms/AVR/serial_lld.c \
               ${CHIBIOS}/os/hal/platforms/AVR/adc_lld.c \
               ${CHIBIOS}/os/hal/platforms/AVR/i2c_lld.c \
-              ${CHIBIOS}/os/hal/platforms/AVR/spi_lld.c
+              ${CHIBIOS}/os/hal/platforms/AVR/spi_lld.c \
+              ${CHIBIOS}/os/hal/platforms/AVR/gpt_lld.c
 
 # Required include directories
 PLATFORMINC = ${CHIBIOS}/os/hal/platforms/AVR
