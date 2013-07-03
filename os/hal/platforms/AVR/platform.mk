dbc858cc9ccf11e388cc2f45f3d5be3761fb0e60
utzig <utzig@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2013-07-03 00:25:54 +0000

    Add initial PWM driver for AVR
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@5924 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/platforms/AVR/platform.mk
+++ os/hal/platforms/AVR/platform.mk
@@ -5,7 +5,8 @@ PLATFORMSRC = ${CHIBIOS}/os/hal/platforms/AVR/hal_lld.c \
               ${CHIBIOS}/os/hal/platforms/AVR/adc_lld.c \
               ${CHIBIOS}/os/hal/platforms/AVR/i2c_lld.c \
               ${CHIBIOS}/os/hal/platforms/AVR/spi_lld.c \
-              ${CHIBIOS}/os/hal/platforms/AVR/gpt_lld.c
+              ${CHIBIOS}/os/hal/platforms/AVR/gpt_lld.c \
+              ${CHIBIOS}/os/hal/platforms/AVR/pwm_lld.c
 
 # Required include directories
 PLATFORMINC = ${CHIBIOS}/os/hal/platforms/AVR
