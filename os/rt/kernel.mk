101156b2e64976b44b2a63af7156a98755f7bcdd
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2013-08-10 14:53:13 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/kernel_3_dev@6124 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/rt/kernel.mk
+++ os/rt/kernel.mk
@@ -1,25 +1,25 @@
 # List of all the ChibiOS/RT kernel files, there is no need to remove the files
 # from this list, you can disable parts of the kernel by editing chconf.h.
-KERNSRC = ${CHIBIOS}/os/kernel/src/chsys.c \
-          ${CHIBIOS}/os/kernel/src/chdebug.c \
-          ${CHIBIOS}/os/kernel/src/chlists.c \
-          ${CHIBIOS}/os/kernel/src/chvt.c \
-          ${CHIBIOS}/os/kernel/src/chtm.c \
-          ${CHIBIOS}/os/kernel/src/chstats.c \
-          ${CHIBIOS}/os/kernel/src/chschd.c \
-          ${CHIBIOS}/os/kernel/src/chthreads.c \
-          ${CHIBIOS}/os/kernel/src/chdynamic.c \
-          ${CHIBIOS}/os/kernel/src/chregistry.c \
-          ${CHIBIOS}/os/kernel/src/chsem.c \
-          ${CHIBIOS}/os/kernel/src/chmtx.c \
-          ${CHIBIOS}/os/kernel/src/chcond.c \
-          ${CHIBIOS}/os/kernel/src/chevents.c \
-          ${CHIBIOS}/os/kernel/src/chmsg.c \
-          ${CHIBIOS}/os/kernel/src/chmboxes.c \
-          ${CHIBIOS}/os/kernel/src/chqueues.c \
-          ${CHIBIOS}/os/kernel/src/chmemcore.c \
-          ${CHIBIOS}/os/kernel/src/chheap.c \
-          ${CHIBIOS}/os/kernel/src/chmempools.c
+KERNSRC = ${CHIBIOS}/os/rt/src/chsys.c \
+          ${CHIBIOS}/os/rt/src/chdebug.c \
+          ${CHIBIOS}/os/rt/src/chlists.c \
+          ${CHIBIOS}/os/rt/src/chvt.c \
+          ${CHIBIOS}/os/rt/src/chtm.c \
+          ${CHIBIOS}/os/rt/src/chstats.c \
+          ${CHIBIOS}/os/rt/src/chschd.c \
+          ${CHIBIOS}/os/rt/src/chthreads.c \
+          ${CHIBIOS}/os/rt/src/chdynamic.c \
+          ${CHIBIOS}/os/rt/src/chregistry.c \
+          ${CHIBIOS}/os/rt/src/chsem.c \
+          ${CHIBIOS}/os/rt/src/chmtx.c \
+          ${CHIBIOS}/os/rt/src/chcond.c \
+          ${CHIBIOS}/os/rt/src/chevents.c \
+          ${CHIBIOS}/os/rt/src/chmsg.c \
+          ${CHIBIOS}/os/rt/src/chmboxes.c \
+          ${CHIBIOS}/os/rt/src/chqueues.c \
+          ${CHIBIOS}/os/rt/src/chmemcore.c \
+          ${CHIBIOS}/os/rt/src/chheap.c \
+          ${CHIBIOS}/os/rt/src/chmempools.c
 
 # Required include directories
 KERNINC = ${CHIBIOS}/os/kernel/include
