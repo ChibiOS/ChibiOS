4eeddd9742524eb39716b7e4848b49c09fcb034e
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2013-08-19 13:58:56 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_2.6.x@6179 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- testhal/STM32F30x/IRQ_STORM/mcuconf.h
+++ testhal/STM32F30x/IRQ_STORM/mcuconf.h
@@ -110,7 +110,7 @@
 #define STM32_GPT_USE_TIM2                  TRUE
 #define STM32_GPT_USE_TIM3                  TRUE
 #define STM32_GPT_USE_TIM4                  TRUE
-#define STM32_GPT_USE_TIM8                  TRUE
+#define STM32_GPT_USE_TIM6                  TRUE
 #define STM32_GPT_USE_TIM7                  TRUE
 #define STM32_GPT_USE_TIM8                  TRUE
 #define STM32_GPT_TIM1_IRQ_PRIORITY         7
