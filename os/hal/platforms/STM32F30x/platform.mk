79ad42bc2b8e9013d0a02a9d758fa8af36322b27
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2013-08-19 14:28:03 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/kernel_3_dev@6182 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/platforms/STM32F30x/platform.mk
+++ os/hal/platforms/STM32F30x/platform.mk
@@ -6,7 +6,6 @@ PLATFORMSRC = ${CHIBIOS}/os/hal/platforms/common/ARMCMx/nvic.c \
               ${CHIBIOS}/os/hal/platforms/STM32F30x/ext_lld_isr.c \
               ${CHIBIOS}/os/hal/platforms/STM32/can_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/ext_lld.c \
-              ${CHIBIOS}/os/hal/platforms/STM32/st_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/GPIOv2/pal_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/I2Cv2/i2c_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/RTCv2/rtc_lld.c \
@@ -14,6 +13,7 @@ PLATFORMSRC = ${CHIBIOS}/os/hal/platforms/common/ARMCMx/nvic.c \
               ${CHIBIOS}/os/hal/platforms/STM32/TIMv1/gpt_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/TIMv1/icu_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/TIMv1/pwm_lld.c \
+              ${CHIBIOS}/os/hal/platforms/STM32/TIMv1/st_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/USARTv2/serial_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/USARTv2/uart_lld.c \
               ${CHIBIOS}/os/hal/platforms/STM32/USBv1/usb_lld.c
