1e80dceebf3203ff927fdee00a99924fa9600114
Giovanni Di Sirio <gdisirio@gmail.com>
2013-08-23 07:44:47 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_2.4.x@6202 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/hal/platforms/STM32F4xx/hal_lld.h
+++ os/hal/platforms/STM32F4xx/hal_lld.h
@@ -324,8 +324,8 @@
 #define STM32_I2C1_RX_DMA_MSK   (STM32_DMA_STREAM_ID_MSK(1, 0) |            \
                                  STM32_DMA_STREAM_ID_MSK(1, 5))
 #define STM32_I2C1_RX_DMA_CHN   0x00100001
-#define STM32_I2C1_TX_DMA_MSK   (STM32_DMA_STREAM_ID_MSK(1, 7)) |           \
-                                 (STM32_DMA_STREAM_ID_MSK(1, 6))
+#define STM32_I2C1_TX_DMA_MSK   (STM32_DMA_STREAM_ID_MSK(1, 7) |            \
+                                 STM32_DMA_STREAM_ID_MSK(1, 6))
 #define STM32_I2C1_TX_DMA_CHN   0x11000000
 
 #define STM32_HAS_I2C2          TRUE
