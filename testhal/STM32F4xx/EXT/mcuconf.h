de38efdc972e24ff7eb7a80ec40ad666e1f66bd1
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2013-08-24 08:50:25 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/kernel_3_dev@6216 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- testhal/STM32F4xx/EXT/mcuconf.h
+++ testhal/STM32F4xx/EXT/mcuconf.h
@@ -139,6 +139,7 @@
 #define STM32_I2C_USE_I2C1                  FALSE
 #define STM32_I2C_USE_I2C2                  FALSE
 #define STM32_I2C_USE_I2C3                  FALSE
+#define STM32_I2C_BUSY_TIMEOUT              50
 #define STM32_I2C_I2C1_RX_DMA_STREAM        STM32_DMA_STREAM_ID(1, 0)
 #define STM32_I2C_I2C1_TX_DMA_STREAM        STM32_DMA_STREAM_ID(1, 6)
 #define STM32_I2C_I2C2_RX_DMA_STREAM        STM32_DMA_STREAM_ID(1, 2)
@@ -151,9 +152,7 @@
 #define STM32_I2C_I2C1_DMA_PRIORITY         3
 #define STM32_I2C_I2C2_DMA_PRIORITY         3
 #define STM32_I2C_I2C3_DMA_PRIORITY         3
-#define STM32_I2C_I2C1_DMA_ERROR_HOOK()     osalSysHalt("DMA failure")
-#define STM32_I2C_I2C2_DMA_ERROR_HOOK()     osalSysHalt("DMA failure")
-#define STM32_I2C_I2C3_DMA_ERROR_HOOK()     osalSysHalt("DMA failure")
+#define STM32_I2C_DMA_ERROR_HOOK(i2cp)      chSysHalt("DMA failure")
 
 /*
  * ICU driver system settings.
