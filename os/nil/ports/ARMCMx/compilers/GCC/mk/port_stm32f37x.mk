5ba0abd185b6f880d925042179947fe0728f5af7
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2013-09-08 08:28:27 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/kernel_3_dev@6273 35acf78f-673a-0410-8e92-d51de3d6d3f4

---
+++ os/nil/ports/ARMCMx/compilers/GCC/mk/port_stm32f37x.mk
@@ -0,0 +1,15 @@
+# List of the ChibiOS/NIL Cortex-M4 STM32F37x port files.
+PORTSRC = $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/crt0.c \
+          $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/vectors.c \
+          ${CHIBIOS}/os/nil/ports/ARMCMx/nilcore.c \
+          ${CHIBIOS}/os/nil/ports/ARMCMx/nilcore_v7m.c
+          
+PORTASM = $(CHIBIOS)/os/nil/ports/ARMCMx/compilers/GCC/nilcoreasm_v7m.s
+
+PORTINC = ${CHIBIOS}/os/ext/CMSIS/include \
+          ${CHIBIOS}/os/ext/CMSIS/ST \
+          ${CHIBIOS}/os/common/ports/ARMCMx/devices/STM32F37x \
+          ${CHIBIOS}/os/nil/ports/ARMCMx \
+          ${CHIBIOS}/os/nil/ports/ARMCMx/compilers/GCC
+
+PORTLD  = ${CHIBIOS}/os/common/ports/ARMCMx/compilers/GCC/ld
