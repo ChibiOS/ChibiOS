655de2175fbf21f9a256bb783b3aac1ad08b7339
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2013-11-02 10:46:10 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/kernel_3_dev@6403 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/ports/STM32/OTGv1/usb_lld.c
+++ os/hal/ports/STM32/OTGv1/usb_lld.c
@@ -112,11 +112,15 @@ static const stm32_otg_params_t hsparams = {
 static void otg_core_reset(USBDriver *usbp) {
   stm32_otg_t *otgp = usbp->otg;
 
+  halPolledDelay(32);
+
   /* Core reset and delay of at least 3 PHY cycles.*/
   otgp->GRSTCTL = GRSTCTL_CSRST;
   while ((otgp->GRSTCTL & GRSTCTL_CSRST) != 0)
     ;
-  osalSysPolledDelayX(12);
+
+  halPolledDelay(12);
+
   /* Wait AHB idle condition.*/
   while ((otgp->GRSTCTL & GRSTCTL_AHBIDL) == 0)
     ;
