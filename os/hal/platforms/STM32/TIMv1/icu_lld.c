f7edaad7cc5a939dcd847c0816642670f28a31b3
Giovanni Di Sirio <gdisirio@gmail.com>
2013-12-27 10:00:29 +0000

    Fixed bug #448.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_2.6.x@6578 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/hal/platforms/STM32/TIMv1/icu_lld.c
+++ os/hal/platforms/STM32/TIMv1/icu_lld.c
@@ -405,7 +405,11 @@ void icu_lld_start(ICUDriver *icup) {
                        CORTEX_PRIORITY_MASK(STM32_ICU_TIM1_IRQ_PRIORITY));
       nvicEnableVector(STM32_TIM1_CC_NUMBER,
                        CORTEX_PRIORITY_MASK(STM32_ICU_TIM1_IRQ_PRIORITY));
+#if defined(STM32_TIM1CLK)
+      icup->clock = STM32_TIM1CLK;
+#else
       icup->clock = STM32_TIMCLK2;
+#endif
     }
 #endif
 #if STM32_ICU_USE_TIM2
@@ -452,7 +456,11 @@ void icu_lld_start(ICUDriver *icup) {
                        CORTEX_PRIORITY_MASK(STM32_ICU_TIM8_IRQ_PRIORITY));
       nvicEnableVector(STM32_TIM8_CC_NUMBER,
                        CORTEX_PRIORITY_MASK(STM32_ICU_TIM8_IRQ_PRIORITY));
+#if defined(STM32_TIM8CLK)
+      icup->clock = STM32_TIM8CLK;
+#else
       icup->clock = STM32_TIMCLK2;
+#endif
     }
 #endif
 #if STM32_ICU_USE_TIM9
