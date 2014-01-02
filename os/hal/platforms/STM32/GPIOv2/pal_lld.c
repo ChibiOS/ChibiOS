6c66ccf668fc9707c66d89e8afba4da4fa4b5728
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2014-01-02 13:26:11 +0000

    Added support for STM32F030xx/050xx/060xx devices.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_2.6.x@6594 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/platforms/STM32/GPIOv2/pal_lld.c
+++ os/hal/platforms/STM32/GPIOv2/pal_lld.c
@@ -36,21 +36,29 @@
                          RCC_AHBENR_GPIOCEN | RCC_AHBENR_GPIODEN |          \
                          RCC_AHBENR_GPIOEEN | RCC_AHBENR_GPIOHEN)
 #define AHB_LPEN_MASK   AHB_EN_MASK
-#elif defined(STM32F0XX)
+
+#elif defined(STM32F030) || defined(STM32F0XX_MD)
 #define AHB_EN_MASK     (RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOBEN |          \
                          RCC_AHBENR_GPIOCEN | RCC_AHBENR_GPIODEN |          \
                          RCC_AHBENR_GPIOFEN)
+
+#elif defined(STM32F0XX_LD)
+#define AHB_EN_MASK     (RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOBEN |          \
+                         RCC_AHBENR_GPIOCEN | RCC_AHBENR_GPIOFEN)
+
 #elif defined(STM32F2XX)
 #define AHB1_EN_MASK    (RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOBEN |        \
                          RCC_AHB1ENR_GPIOCEN | RCC_AHB1ENR_GPIODEN |        \
                          RCC_AHB1ENR_GPIOEEN | RCC_AHB1ENR_GPIOFEN |        \
                          RCC_AHB1ENR_GPIOGEN | RCC_AHB1ENR_GPIOHEN |        \
                          RCC_AHB1ENR_GPIOIEN)
+
 #define AHB1_LPEN_MASK  AHB1_EN_MASK
 #elif defined(STM32F30X) || defined(STM32F37X)
 #define AHB_EN_MASK     (RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOBEN |          \
                          RCC_AHBENR_GPIOCEN | RCC_AHBENR_GPIODEN |          \
                          RCC_AHBENR_GPIOEEN | RCC_AHBENR_GPIOFEN)
+
 #elif defined(STM32F4XX)
 #define AHB1_EN_MASK    (RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOBEN |        \
                          RCC_AHB1ENR_GPIOCEN | RCC_AHB1ENR_GPIODEN |        \
@@ -58,6 +66,7 @@
                          RCC_AHB1ENR_GPIOGEN | RCC_AHB1ENR_GPIOHEN |        \
                          RCC_AHB1ENR_GPIOIEN)
 #define AHB1_LPEN_MASK  AHB1_EN_MASK
+
 #else
 #error "missing or unsupported platform for GPIOv2 PAL driver"
 #endif
