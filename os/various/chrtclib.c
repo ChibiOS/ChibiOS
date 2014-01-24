068581db4d79cbb5876b5bf599549474c5c4ad66
Giovanni Di Sirio <gdisirio@gmail.com>
2014-01-24 13:41:36 +0000

    chcrtlib.c fix.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_2.6.x@6640 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/various/chrtclib.c
+++ os/various/chrtclib.c
@@ -36,8 +36,8 @@
 #if (defined(STM32F4XX) || defined(STM32F2XX) || defined(STM32L1XX) ||        \
      defined(STM32F30X) || defined(STM32F37X) ||                              \
      defined(STM32F1XX) || defined(STM32F10X_MD) || defined(STM32F10X_LD) ||  \
-     defined(STM32F10X_HD) || defined(STM32F0XX) || defined(LPC122X) ||       \
-     defined(__DOXYGEN__))
+     defined(STM32F10X_HD) || defined(STM32F10X_CL) || defined(STM32F0XX) ||  \
+     defined(LPC122X) || defined(__DOXYGEN__))
 #if STM32_RTC_IS_CALENDAR
 /**
  * @brief   Converts from STM32 BCD to canonicalized time format.
