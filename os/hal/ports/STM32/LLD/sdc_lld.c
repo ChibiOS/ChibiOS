58fd763110ba6c1c45d137e5be7321a12dfc9063
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2014-02-08 15:50:16 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/kernel_3_dev@6673 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/ports/STM32/LLD/sdc_lld.c
+++ os/hal/ports/STM32/LLD/sdc_lld.c
@@ -380,6 +380,9 @@ void sdc_lld_start_clk(SDCDriver *sdcp) {
   SDIO->CLKCR  = STM32_SDIO_DIV_LS;
   SDIO->POWER |= SDIO_POWER_PWRCTRL_0 | SDIO_POWER_PWRCTRL_1;
   SDIO->CLKCR |= SDIO_CLKCR_CLKEN;
+
+  /* Clock activation delay.*/
+  osalThreadSleep(MS2ST(STM32_SDC_CLOCK_ACTIVATION_DELAY));
 }
 
 /**
