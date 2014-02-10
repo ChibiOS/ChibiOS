7b3457b64feb6109153ca5d6bfe7fc87d09e9a3a
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2014-02-10 10:35:43 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/kernel_3_dev@6689 35acf78f-673a-0410-8e92-d51de3d6d3f4

---
+++ test/rt/test.mk
@@ -0,0 +1,7 @@
+# List of all the ChibiOS/RT test files.
+TESTSRC = ${CHIBIOS}/test/lib/ch_test.c \
+          ${CHIBIOS}/test/rt/test_sequence_000.c
+
+# Required include directories
+TESTINC = ${CHIBIOS}/test/lib \
+          ${CHIBIOS}/test/rt
