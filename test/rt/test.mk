1033792b73022cbf5a7ef741313ae18592306251
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2014-02-11 13:16:54 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/kernel_3_dev@6705 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- test/rt/test.mk
+++ test/rt/test.mk
@@ -1,16 +1,16 @@
 # List of all the ChibiOS/RT test files.
-TESTSRC = ${CHIBIOS}/test/test.c \
-          ${CHIBIOS}/test/testthd.c \
-          ${CHIBIOS}/test/testsem.c \
-          ${CHIBIOS}/test/testmtx.c \
-          ${CHIBIOS}/test/testmsg.c \
-          ${CHIBIOS}/test/testmbox.c \
-          ${CHIBIOS}/test/testevt.c \
-          ${CHIBIOS}/test/testheap.c \
-          ${CHIBIOS}/test/testpools.c \
-          ${CHIBIOS}/test/testdyn.c \
-          ${CHIBIOS}/test/testqueues.c \
-          ${CHIBIOS}/test/testbmk.c
+TESTSRC = ${CHIBIOS}/test/rt/test.c \
+          ${CHIBIOS}/test/rt/testthd.c \
+          ${CHIBIOS}/test/rt/testsem.c \
+          ${CHIBIOS}/test/rt/testmtx.c \
+          ${CHIBIOS}/test/rt/testmsg.c \
+          ${CHIBIOS}/test/rt/testmbox.c \
+          ${CHIBIOS}/test/rt/testevt.c \
+          ${CHIBIOS}/test/rt/testheap.c \
+          ${CHIBIOS}/test/rt/testpools.c \
+          ${CHIBIOS}/test/rt/testdyn.c \
+          ${CHIBIOS}/test/rt/testqueues.c \
+          ${CHIBIOS}/test/rt/testbmk.c
 
 # Required include directories
-TESTINC = ${CHIBIOS}/test
+TESTINC = ${CHIBIOS}/test/rt
