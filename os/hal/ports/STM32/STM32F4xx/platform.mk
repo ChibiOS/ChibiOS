a6224d1964ef80a53500bde178d639c12dda98ee
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2014-03-05 10:30:58 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/kernel_3_dev@6754 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/ports/STM32/STM32F4xx/platform.mk
+++ os/hal/ports/STM32/STM32F4xx/platform.mk
@@ -8,6 +8,7 @@ PLATFORMSRC = ${CHIBIOS}/os/hal/ports/common/ARMCMx/nvic.c \
               ${CHIBIOS}/os/hal/ports/STM32/LLD/ext_lld.c \
               ${CHIBIOS}/os/hal/ports/STM32/LLD/mac_lld.c \
               ${CHIBIOS}/os/hal/ports/STM32/LLD/sdc_lld.c \
+              ${CHIBIOS}/os/hal/ports/STM32/LLD/DACv1/dac_lld.c \
               ${CHIBIOS}/os/hal/ports/STM32/LLD/GPIOv2/pal_lld.c \
  			  ${CHIBIOS}/os/hal/ports/STM32/LLD/I2Cv1/i2c_lld.c \
               ${CHIBIOS}/os/hal/ports/STM32/LLD/OTGv1/usb_lld.c \
@@ -25,6 +26,7 @@ PLATFORMSRC = ${CHIBIOS}/os/hal/ports/common/ARMCMx/nvic.c \
 PLATFORMINC = ${CHIBIOS}/os/hal/ports/common/ARMCMx \
               ${CHIBIOS}/os/hal/ports/STM32/STM32F4xx \
               ${CHIBIOS}/os/hal/ports/STM32/LLD \
+              ${CHIBIOS}/os/hal/ports/STM32/LLD/DACv1 \
               ${CHIBIOS}/os/hal/ports/STM32/LLD/GPIOv2 \
               ${CHIBIOS}/os/hal/ports/STM32/LLD/I2Cv1 \
               ${CHIBIOS}/os/hal/ports/STM32/LLD/OTGv1 \
