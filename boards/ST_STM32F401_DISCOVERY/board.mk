80a3847bf6c7bb14b779d0535e6cd75df295823f
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2014-04-21 09:54:05 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@6864 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- boards/ST_STM32F401_DISCOVERY/board.mk
+++ boards/ST_STM32F401_DISCOVERY/board.mk
@@ -1,5 +1,5 @@
 # List of all the board related files.
-BOARDSRC = boards/ST_STM32F401_DISCOVERY/board.c
+BOARDSRC = ${CHIBIOS}/boards/ST_STM32F401_DISCOVERY/board.c
 
 # Required include directories
-BOARDINC = boards/ST_STM32F401_DISCOVERY
+BOARDINC = ${CHIBIOS}/boards/ST_STM32F401_DISCOVERY
