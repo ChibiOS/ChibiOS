8366a154e515bf67a8d626d49a545ef7e1c5526d
Giovanni Di Sirio <gdisirio@gmail.com>
2014-04-28 12:03:53 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_2.6.x@6891 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/various/chprintf.c
+++ os/various/chprintf.c
@@ -1,29 +1,23 @@
 /*
-    ChibiOS/RT - Copyright (C) 2006,2007,2008,2009,2010,
-                 2011,2012 Giovanni Di Sirio.
+    ChibiOS/RT - Copyright (C) 2006-2013 Giovanni Di Sirio
 
-    This file is part of ChibiOS/RT.
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
 
-    ChibiOS/RT is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 3 of the License, or
-    (at your option) any later version.
+        http://www.apache.org/licenses/LICENSE-2.0
 
-    ChibiOS/RT is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-                                      ---
-
-    A special exception to the GPL can be applied should you wish to distribute
-    a combined work that includes ChibiOS/RT, without being obliged to provide
-    the source code for any proprietary components. See the file exception.txt
-    for full details of how and when the exception can be applied.
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 */
+
+/*
+   Concepts and parts of this file have been contributed by Fabio Utzig,
+   chvprintf() added by Brent Roman.
+ */
 
 /**
  * @file    chprintf.c
@@ -33,24 +27,37 @@
  * @{
  */
 
-#include <stdarg.h>
-
 #include "ch.h"
+#include "chprintf.h"
+#include "memstreams.h"
 
 #define MAX_FILLER 11
+#define FLOAT_PRECISION 100000
 
-static char *ltoa(char *p, long num, unsigned radix) {
+static char *long_to_string_with_divisor(char *p,
+                                         long num,
+                                         unsigned radix,
+                                         long divisor) {
   int i;
   char *q;
+  long l, ll;
+
+  l = num;
+  if (divisor == 0) {
+    ll = num;
+  } else {
+    ll = divisor;
+  }
 
   q = p + MAX_FILLER;
   do {
-    i = (int)(num % radix);
+    i = (int)(l % radix);
     i += '0';
     if (i > '9')
       i += 'A' - '0' - 10;
     *--q = i;
-  } while ((num /= radix) != 0);
+    l /= radix;
+  } while ((ll /= radix) != 0);
 
   i = (int)(p + MAX_FILLER - q);
   do
@@ -60,10 +67,28 @@ static char *ltoa(char *p, long num, unsigned radix) {
   return p;
 }
 
+static char *ltoa(char *p, long num, unsigned radix) {
+
+  return long_to_string_with_divisor(p, num, radix, 0);
+}
+
+#if CHPRINTF_USE_FLOAT
+static char *ftoa(char *p, double num) {
+  long l;
+  unsigned long precision = FLOAT_PRECISION;
+
+  l = (long)num;
+  p = long_to_string_with_divisor(p, l, 10, 0);
+  *p++ = '.';
+  l = (long)((num - l) * precision);
+  return long_to_string_with_divisor(p, l, 10, precision / 10);
+}
+#endif
+
 /**
  * @brief   System formatted output function.
- * @details This function implements a minimal @p printf() like functionality
- *          with output on a @p BaseChannel.
+ * @details This function implements a minimal @p vprintf()-like functionality
+ *          with output on a @p BaseSequentialStream.
  *          The general parameters format is: %[-][width|*][.precision|*][l|L]p.
  *          The following parameter types (p) are supported:
  *          - <b>x</b> hexadecimal integer.
@@ -77,29 +102,31 @@ static char *ltoa(char *p, long num, unsigned radix) {
  *          - <b>c</b> character.
  *          - <b>s</b> string.
  *          .
- * @note    Floating point types are not implemented, this function is meant
- *          as a system utility and not a full implementation.
  *
- * @param[in] chp       pointer to a @p BaseChannel implementing object
+ * @param[in] chp       pointer to a @p BaseSequentialStream implementing object
  * @param[in] fmt       formatting string
+ * @param[in] ap        list of parameters
+ *
+ * @api
  */
-void chprintf(BaseChannel *chp, const char *fmt, ...) {
-  va_list ap;
-  char tmpbuf[MAX_FILLER + 1];
+void chvprintf(BaseSequentialStream *chp, const char *fmt, va_list ap) {
   char *p, *s, c, filler;
   int i, precision, width;
   bool_t is_long, left_align;
   long l;
+#if CHPRINTF_USE_FLOAT
+  float f;
+  char tmpbuf[2*MAX_FILLER + 1];
+#else
+  char tmpbuf[MAX_FILLER + 1];
+#endif
 
-  va_start(ap, fmt);
   while (TRUE) {
     c = *fmt++;
-    if (c == 0) {
-      va_end(ap);
+    if (c == 0)
       return;
-    }
     if (c != '%') {
-      chIOPut(chp, (uint8_t)c);
+      chSequentialStreamPut(chp, (uint8_t)c);
       continue;
     }
     p = tmpbuf;
@@ -110,7 +137,7 @@ void chprintf(BaseChannel *chp, const char *fmt, ...) {
       left_align = TRUE;
     }
     filler = ' ';
-    if (*fmt == '.') {
+    if ((*fmt == '.') || (*fmt == '0')) {
       fmt++;
       filler = '0';
     }
@@ -165,6 +192,8 @@ void chprintf(BaseChannel *chp, const char *fmt, ...) {
       break;
     case 'D':
     case 'd':
+    case 'I':
+    case 'i':
       if (is_long)
         l = va_arg(ap, long);
       else
@@ -175,6 +204,16 @@ void chprintf(BaseChannel *chp, const char *fmt, ...) {
       }
       p = ltoa(p, l, 10);
       break;
+#if CHPRINTF_USE_FLOAT
+    case 'f':
+      f = (float) va_arg(ap, double);
+      if (f < 0) {
+        *p++ = '-';
+        f = -f;
+      }
+      p = ftoa(p, f);
+      break;
+#endif
     case 'X':
     case 'x':
       c = 16;
@@ -204,21 +243,65 @@ unsigned_common:
       width = -width;
     if (width < 0) {
       if (*s == '-' && filler == '0') {
-        chIOPut(chp, (uint8_t)*s++);
+        chSequentialStreamPut(chp, (uint8_t)*s++);
         i--;
       }
-      do
-        chIOPut(chp, (uint8_t)filler);
-      while (++width != 0);
+      do {
+        chSequentialStreamPut(chp, (uint8_t)filler);
+      } while (++width != 0);
     }
     while (--i >= 0)
-      chIOPut(chp, (uint8_t)*s++);
+      chSequentialStreamPut(chp, (uint8_t)*s++);
 
     while (width) {
-      chIOPut(chp, (uint8_t)filler);
+      chSequentialStreamPut(chp, (uint8_t)filler);
       width--;
     }
   }
+}
+
+/**
+ * @brief   System formatted output function.
+ * @details This function implements a minimal @p vprintf()-like functionality
+ *          with output on a @p BaseSequentialStream.
+ *          The general parameters format is: %[-][width|*][.precision|*][l|L]p.
+ *          The following parameter types (p) are supported:
+ *          - <b>x</b> hexadecimal integer.
+ *          - <b>X</b> hexadecimal long.
+ *          - <b>o</b> octal integer.
+ *          - <b>O</b> octal long.
+ *          - <b>d</b> decimal signed integer.
+ *          - <b>D</b> decimal signed long.
+ *          - <b>u</b> decimal unsigned integer.
+ *          - <b>U</b> decimal unsigned long.
+ *          - <b>c</b> character.
+ *          - <b>s</b> string.
+ *          .
+ *
+ * @param[in] str       pointer to a buffer
+ * @param[in] size      maximum size of the buffer
+ * @param[in] fmt       formatting string
+ * @return              The size of the generated string.
+ *
+ * @api
+ */
+int chsnprintf(char *str, size_t size, const char *fmt, ...) {
+  va_list ap;
+  MemoryStream ms;
+  BaseSequentialStream *chp;
+
+  /* Memory stream object to be used as a string writer.*/
+  msObjectInit(&ms, (uint8_t *)str, size, 0);
+
+  /* Performing the print operation using the common code.*/
+  chp = (BaseSequentialStream *)&ms;
+  va_start(ap, fmt);
+  chvprintf(chp, fmt, ap);
+  va_end(ap);
+
+  /* Final zero and size return.*/
+  chSequentialStreamPut(chp, 0);
+  return ms.eos - 1;
 }
 
 /** @} */
