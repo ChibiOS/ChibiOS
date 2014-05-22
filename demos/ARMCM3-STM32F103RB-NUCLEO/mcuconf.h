fb25233df632d5a8436fd9a65a2962b93fbdbf3a
Giovanni Di Sirio <gdisirio@gmail.com>
2014-05-22 08:47:58 +0000

    Nucleo demo modified to use the default clock settings on the board, tested.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_2.6.x@6943 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- demos/ARMCM3-STM32F103RB-NUCLEO/mcuconf.h
+++ demos/ARMCM3-STM32F103RB-NUCLEO/mcuconf.h
@@ -36,20 +36,20 @@
 #define STM32_NO_INIT                       FALSE
 #define STM32_HSI_ENABLED                   TRUE
 #define STM32_LSI_ENABLED                   FALSE
-#define STM32_HSE_ENABLED                   TRUE
+#define STM32_HSE_ENABLED                   FALSE
 #define STM32_LSE_ENABLED                   FALSE
 #define STM32_SW                            STM32_SW_PLL
-#define STM32_PLLSRC                        STM32_PLLSRC_HSE
+#define STM32_PLLSRC                        STM32_PLLSRC_HSI
 #define STM32_PLLXTPRE                      STM32_PLLXTPRE_DIV1
-#define STM32_PLLMUL_VALUE                  9
+#define STM32_PLLMUL_VALUE                  12
 #define STM32_HPRE                          STM32_HPRE_DIV1
 #define STM32_PPRE1                         STM32_PPRE1_DIV2
 #define STM32_PPRE2                         STM32_PPRE2_DIV2
 #define STM32_ADCPRE                        STM32_ADCPRE_DIV4
 #define STM32_USB_CLOCK_REQUIRED            TRUE
-#define STM32_USBPRE                        STM32_USBPRE_DIV1P5
+#define STM32_USBPRE                        STM32_USBPRE_DIV1
 #define STM32_MCOSEL                        STM32_MCOSEL_NOCLOCK
-#define STM32_RTCSEL                        STM32_RTCSEL_HSEDIV
+#define STM32_RTCSEL                        STM32_RTCSEL_NOCLOCK
 #define STM32_PVD_ENABLE                    FALSE
 #define STM32_PLS                           STM32_PLS_LEV0
 
