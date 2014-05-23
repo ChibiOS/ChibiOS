09128771832130e9b5a6e860c9057a5e82d3dc63
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2014-05-23 09:12:09 +0000

    Added support for NUCLEO-F030R8 board.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_2.6.x@6952 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- testhal/STM32F0xx/ADC/mcuconf.h
+++ testhal/STM32F0xx/ADC/mcuconf.h
@@ -80,17 +80,19 @@
 #define STM32_GPT_USE_TIM1                  FALSE
 #define STM32_GPT_USE_TIM2                  FALSE
 #define STM32_GPT_USE_TIM3                  FALSE
+#define STM32_GPT_USE_TIM14                 FALSE
 #define STM32_GPT_TIM1_IRQ_PRIORITY         2
 #define STM32_GPT_TIM2_IRQ_PRIORITY         2
 #define STM32_GPT_TIM3_IRQ_PRIORITY         2
+#define STM32_GPT_TIM14_IRQ_PRIORITY        2
 
 /*
  * I2C driver system settings.
  */
 #define STM32_I2C_USE_I2C1                  FALSE
 #define STM32_I2C_USE_I2C2                  FALSE
-#define STM32_I2C_I2C1_IRQ_PRIORITY         10
-#define STM32_I2C_I2C2_IRQ_PRIORITY         10
+#define STM32_I2C_I2C1_IRQ_PRIORITY         3
+#define STM32_I2C_I2C2_IRQ_PRIORITY         3
 #define STM32_I2C_I2C1_DMA_PRIORITY         1
 #define STM32_I2C_I2C2_DMA_PRIORITY         1
 #define STM32_I2C_DMA_ERROR_HOOK(i2cp)      chSysHalt()
