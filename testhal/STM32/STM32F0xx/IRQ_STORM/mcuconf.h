8bdd0b926946c83dc7ec41ab6372baf4e158d382
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2014-05-23 13:27:26 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@6957 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- testhal/STM32/STM32F0xx/IRQ_STORM/mcuconf.h
+++ testhal/STM32/STM32F0xx/IRQ_STORM/mcuconf.h
@@ -80,9 +80,11 @@
 #define STM32_GPT_USE_TIM1                  TRUE
 #define STM32_GPT_USE_TIM2                  FALSE
 #define STM32_GPT_USE_TIM3                  TRUE
+#define STM32_GPT_USE_TIM14                 FALSE
 #define STM32_GPT_TIM1_IRQ_PRIORITY         1
 #define STM32_GPT_TIM2_IRQ_PRIORITY         2
 #define STM32_GPT_TIM3_IRQ_PRIORITY         3
+#define STM32_GPT_TIM14_IRQ_PRIORITY        2
 
 /*
  * I2C driver system settings.
