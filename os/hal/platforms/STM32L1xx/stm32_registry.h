da44cde636d7ee0e0f11064034b3c1a91321f49b
Giovanni Di Sirio <gdisirio@gmail.com>
2014-06-01 09:51:46 +0000

    Fixed bug #509.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_2.6.x@6976 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/hal/platforms/STM32L1xx/stm32_registry.h
+++ os/hal/platforms/STM32L1xx/stm32_registry.h
@@ -74,9 +74,9 @@
 
 /* I2C attributes.*/
 #define STM32_HAS_I2C1                      TRUE
-#define STM32_I2C1_RX_DMA_MSK               STM32_DMA_STREAM_ID_MSK(1, 7)
+#define STM32_I2C1_RX_DMA_MSK               STM32_DMA_STREAM_ID_MSK(1, 3)
 #define STM32_I2C1_RX_DMA_CHN               0x00000000
-#define STM32_I2C1_TX_DMA_MSK               STM32_DMA_STREAM_ID_MSK(1, 6)
+#define STM32_I2C1_TX_DMA_MSK               STM32_DMA_STREAM_ID_MSK(1, 2)
 #define STM32_I2C1_TX_DMA_CHN               0x00000000
 
 #define STM32_HAS_I2C2                      TRUE
@@ -89,7 +89,11 @@
 
 /* RTC attributes.*/
 #define STM32_HAS_RTC                       TRUE
+#if defined(STM32L1XX_MDP)
+#define STM32_RTC_HAS_SUBSECONDS            TRUE
+#else
 #define STM32_RTC_HAS_SUBSECONDS            FALSE
+#endif
 #define STM32_RTC_IS_CALENDAR               TRUE
 
 /* SDIO attributes.*/
