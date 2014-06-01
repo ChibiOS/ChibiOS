f96ae1b6bc4ce647eae83e867f9cddec58da4fbd
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2014-06-01 09:47:38 +0000

    Fixed bug #508.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_2.6.x@6974 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/platforms/STM32L1xx/stm32_registry.h
+++ os/hal/platforms/STM32L1xx/stm32_registry.h
@@ -74,9 +74,9 @@
 
 /* I2C attributes.*/
 #define STM32_HAS_I2C1                      TRUE
-#define STM32_I2C1_RX_DMA_MSK               STM32_DMA_STREAM_ID_MSK(1, 3)
+#define STM32_I2C1_RX_DMA_MSK               STM32_DMA_STREAM_ID_MSK(1, 7)
 #define STM32_I2C1_RX_DMA_CHN               0x00000000
-#define STM32_I2C1_TX_DMA_MSK               STM32_DMA_STREAM_ID_MSK(1, 2)
+#define STM32_I2C1_TX_DMA_MSK               STM32_DMA_STREAM_ID_MSK(1, 6)
 #define STM32_I2C1_TX_DMA_CHN               0x00000000
 
 #define STM32_HAS_I2C2                      TRUE
