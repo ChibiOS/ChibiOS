766a2388d46f49e07ec0280fa4f3d254367850d1
utzig <utzig@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2014-06-29 23:06:17 +0000

    AVR HAL ported from RT-2 (serial/pal/hal tested)
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@6999 35acf78f-673a-0410-8e92-d51de3d6d3f4

---
+++ os/hal/ports/AVR/hal_lld.c
@@ -0,0 +1,103 @@
+/*
+    ChibiOS/RT - Copyright (C) 2006-2014 Giovanni Di Sirio
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+/**
+ * @file    AVR/hal_lld.c
+ * @brief   AVR HAL subsystem low level driver code.
+ *
+ * @addtogroup HAL
+ * @{
+ */
+
+#include "hal.h"
+
+/*===========================================================================*/
+/* Driver exported variables.                                                */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver local variables and types.                                         */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver local functions.                                                   */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver interrupt handlers.                                                */
+/*===========================================================================*/
+
+/**
+ * @brief   Timer0 interrupt handler.
+ */
+OSAL_IRQ_HANDLER(AVR_TIMER_VECT) {
+
+  OSAL_IRQ_PROLOGUE();
+
+  osalSysLockFromISR();
+  osalOsTimerHandlerI();
+  osalSysUnlockFromISR();
+
+  OSAL_IRQ_EPILOGUE();
+}
+
+
+/*===========================================================================*/
+/* Driver exported functions.                                                */
+/*===========================================================================*/
+
+/**
+ * @brief   Low level HAL driver initialization.
+ *
+ * @notapi
+ */
+void hal_lld_init(void) {
+
+  /*
+   * Timer 0 setup.
+   */
+#if defined(TCCR0B) /* Timer has multiple output comparators                       */
+  TCCR0A  = (1 << WGM01) | (0 << WGM00) |                /* CTC mode.        */
+            (0 << COM0A1) | (0 << COM0A0) |              /* OC0A disabled.   */
+            (0 << COM0B1) | (0 << COM0B0);               /* OC0B disabled.   */
+  TCCR0B  = (0 << WGM02) | AVR_TIMER_PRESCALER_BITS;     /* CTC mode.        */
+  OCR0A   = AVR_TIMER_COUNTER - 1;
+  TCNT0   = 0;                                           /* Reset counter.   */
+  TIFR0   = (1 << OCF0A);                                /* Reset pending.   */
+  TIMSK0  = (1 << OCIE0A);                               /* IRQ on compare.  */
+
+#elif defined(TCCR0A) /* AT90CAN doesn't have TCCR0B and slightly different TCCR0A */
+  TCCR0A  = (1 << WGM01) | (0 << WGM00) |                /* CTC mode.        */
+            (0 << COM0A1) | (0 << COM0A0);               /* OC0A disabled.   */
+  OCR0A   = AVR_TIMER_COUNTER - 1;
+  TCNT0   = 0;                                           /* Reset counter.   */
+  TIFR0   = (1 << OCF0A);                                /* Reset pending.   */
+  TIMSK0  = (1 << OCIE0A);                               /* IRQ on compare.  */
+
+#elif defined(TCCR0) /* Timer has single output comparator                   */
+  TCCR0   = (1 << WGM01) | (0 << WGM00) |                /* CTC mode.        */
+            (0 << COM01) | (0 << COM00) |                /* OC0A disabled.   */
+            AVR_TIMER_PRESCALER_BITS;
+  OCR0    = AVR_TIMER_COUNTER - 1;
+  TCNT0   = 0;                                           /* Reset counter.   */
+  TIFR    = (1 << OCF0);                                 /* Reset pending.   */
+  TIMSK   = (1 << OCIE0);                                /* IRQ on compare.  */
+#else
+  #error "Neither TCCR0A nor TCCR0 registers are defined"
+#endif
+}
+
+/** @} */
---
+++ os/hal/ports/AVR/hal_lld.h
@@ -0,0 +1,117 @@
+/*
+    ChibiOS/RT - Copyright (C) 2006-2014 Giovanni Di Sirio
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+/**
+ * @file    AVR/hal_lld.h
+ * @brief   AVR HAL subsystem low level driver header.
+ *
+ * @addtogroup HAL
+ * @{
+ */
+
+#ifndef _HAL_LLD_H_
+#define _HAL_LLD_H_
+
+/*===========================================================================*/
+/* Driver constants.                                                         */
+/*===========================================================================*/
+
+/**
+ * @brief   Defines the support for realtime counters in the HAL.
+ */
+#define HAL_IMPLEMENTS_COUNTERS FALSE
+
+/**
+ * @brief   Platform name.
+ */
+#define PLATFORM_NAME   "AVR"
+
+/**
+ * @brief  Timer maximum value 
+ */
+#define AVR_TIMER_COUNTER_MAX 255
+
+/*===========================================================================*/
+/* Driver pre-compile time settings.                                         */
+/*===========================================================================*/
+
+/* Work out what the timer interrupt is called on this MCU */
+#ifdef TIMER0_COMPA_vect
+  #define AVR_TIMER_VECT TIMER0_COMPA_vect
+#elif defined(TIMER_COMPA_vect)
+  #define AVR_TIMER_VECT TIMER_COMPA_vect
+#elif defined(TIMER0_COMP_vect)
+  #define AVR_TIMER_VECT TIMER0_COMP_vect
+#else
+  #error "Cannot find interrupt vector name for timer"
+#endif
+
+/*===========================================================================*/
+/* Derived constants and error checks.                                       */
+/*===========================================================================*/
+
+/* Find the most suitable prescaler setting for the desired CH_CFG_ST_FREQUENCY */
+#if ((F_CPU / CH_CFG_ST_FREQUENCY) <= AVR_TIMER_COUNTER_MAX)
+  #define AVR_TIMER_PRESCALER 1
+  #define AVR_TIMER_PRESCALER_BITS (0 << CS02)  | (0 << CS01)  | (1 << CS00); /* CLK      */
+#elif ((F_CPU / CH_CFG_ST_FREQUENCY / 8) <= AVR_TIMER_COUNTER_MAX)
+  #define AVR_TIMER_PRESCALER 8
+  #define AVR_TIMER_PRESCALER_BITS (0 << CS02)  | (1 << CS01)  | (0 << CS00); /* CLK/8    */
+#elif ((F_CPU / CH_CFG_ST_FREQUENCY / 64) <= AVR_TIMER_COUNTER_MAX)
+  #define AVR_TIMER_PRESCALER 64
+  #define AVR_TIMER_PRESCALER_BITS (0 << CS02)  | (1 << CS01)  | (1 << CS00); /* CLK/64   */
+#elif ((F_CPU / CH_CFG_ST_FREQUENCY / 256) <= AVR_TIMER_COUNTER_MAX)
+  #define AVR_TIMER_PRESCALER 256
+  #define AVR_TIMER_PRESCALER_BITS (1 << CS02)  | (0 << CS01)  | (0 << CS00); /* CLK/256  */
+#elif ((F_CPU / CH_CFG_ST_FREQUENCY / 1024) <= AVR_TIMER_COUNTER_MAX)
+  #define AVR_TIMER_PRESCALER 1024
+  #define AVR_TIMER_PRESCALER_BITS (1 << CS02)  | (0 << CS01)  | (1 << CS00); /* CLK/1024 */
+#else
+  #error "Frequency too low for timer, please set CH_CFG_ST_FREQUENCY to a higher value"
+#endif
+
+#define AVR_TIMER_COUNTER (F_CPU / CH_CFG_ST_FREQUENCY / AVR_TIMER_PRESCALER)
+
+/* Test if CH_CFG_ST_FREQUENCY can be matched exactly using this timer */
+#define F_CPU_ (AVR_TIMER_COUNTER * AVR_TIMER_PRESCALER * CH_CFG_ST_FREQUENCY)
+#if (F_CPU_ != F_CPU)
+  #warning "CH_CFG_ST_FREQUENCY cannot be generated exactly using timer"
+#endif
+#undef F_CPU_
+
+/*===========================================================================*/
+/* Driver data structures and types.                                         */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver macros.                                                            */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* External declarations.                                                    */
+/*===========================================================================*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+  void hal_lld_init(void);
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _HAL_LLD_H_ */
+
+/** @} */
