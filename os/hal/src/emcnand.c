f5063b9072a799ef856733580a1e680c86e8269b
barthess <barthess@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2014-08-05 08:17:43 +0000

    STM32 FSMC NAND. Fixed license notes.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@7125 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/src/emcnand.c
+++ os/hal/src/emcnand.c
@@ -1,10 +1,10 @@
 /*
-    ChibiOS/RT - Copyright (C) 2006,2007,2008,2009,2010,
-                 2011,2012,2013 Giovanni Di Sirio.
+    ChibiOS/HAL - Copyright (C) 2006,2007,2008,2009,2010,
+                  2011,2012,2013,2014 Giovanni Di Sirio.
 
-    This file is part of ChibiOS/RT.
+    This file is part of ChibiOS/HAL
 
-    ChibiOS/RT is free software; you can redistribute it and/or modify
+    ChibiOS/HAL is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 3 of the License, or
     (at your option) any later version.
--- testhal/STM32/STM32F4xx/FSMC_NAND/dma_storm_adc.c
+++ testhal/STM32/STM32F4xx/FSMC_NAND/dma_storm_adc.c
@@ -1,5 +1,5 @@
 /*
-    ChibiOS/RT - Copyright (C) 2006-2013 Giovanni Di Sirio
+    ChibiOS/RT - Copyright (C) 2006-2014 Giovanni Di Sirio
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -13,6 +13,10 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 */
+/*
+   Concepts and parts of this file have been contributed by Uladzimir Pylinsky
+   aka barthess.
+ */
 
 #include "ch.h"
 #include "hal.h"
