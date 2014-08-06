c6d9449791b4a8f7b6f772515951ca485b4a6024
utzig <utzig@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2014-08-06 01:05:05 +0000

    [KINETIS] Add K20x family port makefile
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@7136 35acf78f-673a-0410-8e92-d51de3d6d3f4

---
+++ os/rt/ports/ARMCMx/compilers/GCC/mk/port_k20x.mk
@@ -0,0 +1,15 @@
+# List of the ChibiOS/RT Cortex-M4 K20x port files.
+PORTSRC = $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/crt0.c \
+          $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/vectors.c \
+          ${CHIBIOS}/os/rt/ports/ARMCMx/chcore.c \
+          ${CHIBIOS}/os/rt/ports/ARMCMx/chcore_v7m.c
+
+PORTASM = $(CHIBIOS)/os/rt/ports/ARMCMx/compilers/GCC/chcoreasm_v7m.s
+
+PORTINC = ${CHIBIOS}/os/ext/CMSIS/include \
+          ${CHIBIOS}/os/ext/CMSIS/KINETIS \
+          ${CHIBIOS}/os/common/ports/ARMCMx/devices/K20x \
+          ${CHIBIOS}/os/rt/ports/ARMCMx \
+          ${CHIBIOS}/os/rt/ports/ARMCMx/compilers/GCC
+
+PORTLD  = ${CHIBIOS}/os/common/ports/ARMCMx/compilers/GCC/ld
