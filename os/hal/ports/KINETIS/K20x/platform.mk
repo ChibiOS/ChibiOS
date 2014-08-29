7668607e63bde8848abf43040291ff0a3ef5691d
utzig <utzig@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2014-08-29 19:09:38 +0000

    [KINETIS] Initial I2C driver for K2x
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@7206 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/ports/KINETIS/K20x/platform.mk
+++ os/hal/ports/KINETIS/K20x/platform.mk
@@ -4,6 +4,7 @@ PLATFORMSRC = ${CHIBIOS}/os/hal/ports/common/ARMCMx/nvic.c \
               ${CHIBIOS}/os/hal/ports/KINETIS/K20x/pal_lld.c \
               ${CHIBIOS}/os/hal/ports/KINETIS/K20x/serial_lld.c \
               ${CHIBIOS}/os/hal/ports/KINETIS/K20x/spi_lld.c \
+              ${CHIBIOS}/os/hal/ports/KINETIS/K20x/i2c_lld.c \
               ${CHIBIOS}/os/hal/ports/KINETIS/K20x/st_lld.c
 
 # Required include directories
