616d5ef60b358c692a4e0b2b99b96b34c7e5418f
barthess <barthess@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2014-09-10 18:37:21 +0000

    [CPP wrappers] Syscalls cleanup.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@7257 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/various/cpp_wrappers/syscalls_cpp.cpp
+++ os/various/cpp_wrappers/syscalls_cpp.cpp
@@ -5,74 +5,37 @@
 
 #include "syscalls_cpp.hpp"
 
-/**
- *
- */
 #ifdef __cplusplus
 extern "C" {
 #endif
+
 void _exit(int status){
    (void) status;
    osalSysHalt("Unrealized");
    while(TRUE){}
 }
-#ifdef __cplusplus
-}
-#endif
 
-/**
- *
- */
-#ifdef __cplusplus
-extern "C" {
-#endif
 pid_t _getpid(void){
    return 1;
 }
-#ifdef __cplusplus
-}
-#endif
 
-/**
- *
- */
 #undef errno
 extern int errno;
-#ifdef __cplusplus
-extern "C" {
-#endif
 int _kill(int pid, int sig) {
   (void)pid;
   (void)sig;
   errno = EINVAL;
   return -1;
 }
-#ifdef __cplusplus
-}
-#endif
 
-/**
- *
- */
-#ifdef __cplusplus
-extern "C" {
-#endif
 void _open_r(void){
   return;
 }
-#ifdef __cplusplus
-}
-#endif
 
-/**
- *
- */
-#ifdef __cplusplus
-extern "C" {
-#endif
-  void __cxa_pure_virtual() {
-    osalSysHalt("Pure virtual function call.");
-  }
+void __cxa_pure_virtual() {
+  osalSysHalt("Pure virtual function call.");
+}
+
 #ifdef __cplusplus
 }
 #endif
