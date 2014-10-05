849c7af120b4dd015a590f95b900d794e319eab7
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2014-10-05 09:05:07 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@7363 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- demos/STM32/RT-STM32F103-OLIMEX_STM32_P103/chconf.h
+++ demos/STM32/RT-STM32F103-OLIMEX_STM32_P103/chconf.h
@@ -46,7 +46,7 @@
  * @details Frequency of the system timer that drives the system ticks. This
  *          setting also defines the system tick time unit.
  */
-#define CH_CFG_ST_FREQUENCY                 10000
+#define CH_CFG_ST_FREQUENCY                 2000
 
 /**
  * @brief   Time delta constant for the tick-less mode.
