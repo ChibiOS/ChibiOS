c054fd6a0937c1540336e50abcc0806e2a9f8e82
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2014-12-13 09:15:37 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@7574 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- doc/rt/rsync_web.sh
+++ doc/rt/rsync_web.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -avP -e ssh --delete --exclude=.* ./html/ gdisirio,chibios@web.sourceforge.net:/home/groups/c/ch/chibios/htdocs/html
+rsync -avP -e ssh --delete --exclude=.* ./html/ gdisirio,chibios@web.sourceforge.net:/home/groups/c/ch/chibios/htdocs/docs3/rt
