c7c04a2dfd295cc7cc2ad3f3bddeea378bb5009a
gdisirio <gdisirio@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2015-01-11 10:09:25 +0000

    maple support and demo
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@7618 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/boards/MAPLEMINI_STM32_F103/board.h
+++ os/hal/boards/MAPLEMINI_STM32_F103/board.h
@@ -1,5 +1,5 @@
 /*
-    ChibiOS/RT - Copyright (C) 2006-2014 Giovanni Di Sirio
+    ChibiOS/RT - Copyright (C) 2006-2015 Giovanni Di Sirio
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -39,9 +39,64 @@
 #define STM32F10X_MD
 
 /*
- * IO pins assignments.
+ * IO pins assignments
+ *
+ * numbering is sorted by onboard/connectors, as from the schematics in
+ * https://github.com/leaflabs/maplemini
  */
-/* Missing.*/
+
+/* on-board */
+
+#define GPIOB_LED               1
+#define GPIOB_USB_DISC          9
+#define GPIOD_OSC_IN            0
+#define GPIOD_OSC_OUT           1
+
+/* J1 connector */
+
+// pin 1:       AV+
+// pin 2:       AV-
+// pin 3:       VBAT
+#define GPIOC_TAMPER_RTC        13      // pin 4
+#define GPIOC_OSC32_IN          14      // pin 5
+#define GPIOC_OSD32_OUT         15      // pin 6
+// pin 7:       RESET
+#define GPIOA_USART2_CTS        0       // pin 8
+#define GPIOA_USART2_RTS        1       // pin 9
+#define GPIOA_USART2_TX         2       // pin 10
+#define GPIOA_USART2_RX         3       // pin 11
+#define GPIOA_SPI1_NSS          4       // pin 12
+#define GPIOA_SPI1_SCK          5       // pin 13
+#define GPIOA_SPI1_MISO         6       // pin 14
+#define GPIOA_SPI1_MOSI         7       // pin 15
+#define GPIOB_ADC12_IN8         0       // pin 16
+#define GPIOB_BOOT1             2       // pin 17
+#define GPIOB_I2C2_SCL          10      // pin 18
+#define GPIOB_I2C2_SDA          11      // pin 19
+// pin 20: VIN
+
+/* J2 connector */
+
+#define GPIOB_SPI2_NSS          12      // pin 1
+#define GPIOB_SPI2_SCK          13      // pin 2
+#define GPIOB_SPI2_MISO         14      // pin 3
+#define GPIOB_SPI2_MOSI         15      // pin 4
+#define GPIOA_USART1_CK         8       // pin 5
+#define GPIOA_USART1_TX         9       // pin 6
+#define GPIOA_USART1_RX         10      // pin 7
+#define GPIOA_USBDM             11      // pin 8
+#define GPIOA_USBDP             12      // pin 9
+#define GPIOA_JTMS              13      // pin 10
+#define GPIOA_JTCK              14      // pin 11
+#define GPIOA_JTDI              15      // pin 12
+#define GPIOB_JTDO              3       // pin 13
+#define GPIOB_JTRST             4       // pin 14
+#define GPIOB_I2C1_SMBA         5       // pin 15
+#define GPIOB_I2C1_SCL          6       // pin 16
+#define GPIOB_I2C1_SDA          7       // pin 17
+#define GPIOB_BOOT0_BUTTON      8       // pin 18
+// pin 19: GND
+// pin 20: VCC
 
 /*
  * I/O ports initial setup, this configuration is established soon after reset
@@ -82,10 +137,11 @@
 /*
  * Port B setup.
  * Everything input with pull-up except:
- * PB1  - Push Pull output  (LED).
+ * PB1    - Push Pull output  (LED).
+ * PB9    - Push Pull output  (USB switch).
  */
 #define VAL_GPIOBCRL            0x88888838      /*  PB7...PB0 */
-#define VAL_GPIOBCRH            0x88888888      /* PB15...PB8 */
+#define VAL_GPIOBCRH            0x88888838      /* PB15...PB8 */
 #define VAL_GPIOBODR            0xFFFFFFFF
 
 /*
@@ -117,12 +173,12 @@
 /*
  * USB bus activation macro, required by the USB driver.
  */
-#define usb_lld_connect_bus(usbp) palClearPad(GPIOC, GPIOC_USB_DISC)
+#define usb_lld_connect_bus(usbp) palClearPad(GPIOB, GPIOB_USB_DISC)
 
 /*
  * USB bus de-activation macro, required by the USB driver.
  */
-#define usb_lld_disconnect_bus(usbp) palSetPad(GPIOC, GPIOC_USB_DISC)
+#define usb_lld_disconnect_bus(usbp) palSetPad(GPIOB, GPIOB_USB_DISC)
 
 #if !defined(_FROM_ASM_)
 #ifdef __cplusplus
