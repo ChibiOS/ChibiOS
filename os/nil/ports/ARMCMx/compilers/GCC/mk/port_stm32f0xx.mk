6cc7199329fa18b0f2a2110c686b71c2d5ea556e
Giovanni Di Sirio <gdisirio@gmail.com>
2015-03-26 16:04:37 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@7815 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/nil/ports/ARMCMx/compilers/GCC/mk/port_stm32f0xx.mk
+++ os/nil/ports/ARMCMx/compilers/GCC/mk/port_stm32f0xx.mk
@@ -1,16 +1,15 @@
 # List of the ChibiOS/NIL Cortex-M0 STM32F0xx port files.
-PORTSRC = $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/crt0.c \
-          $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/vectors.c \
-          ${CHIBIOS}/os/nil/ports/ARMCMx/nilcore.c \
-          ${CHIBIOS}/os/nil/ports/ARMCMx/nilcore_v6m.c
+include $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/mk/startup_stm32f0xx.mk
+
+PORTSRC = $(STARTUPSRC) \
+          $(CHIBIOS)/os/nil/ports/ARMCMx/nilcore.c \
+          $(CHIBIOS)/os/nil/ports/ARMCMx/nilcore_v6m.c
           
-PORTASM = $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/crt0_v6m.s \
+PORTASM = $(STARTUPASM) \
           $(CHIBIOS)/os/nil/ports/ARMCMx/compilers/GCC/nilcoreasm_v6m.s
 
-PORTINC = ${CHIBIOS}/os/ext/CMSIS/include \
-          ${CHIBIOS}/os/ext/CMSIS/ST \
-          ${CHIBIOS}/os/common/ports/ARMCMx/devices/STM32F0xx \
-          ${CHIBIOS}/os/nil/ports/ARMCMx \
-          ${CHIBIOS}/os/nil/ports/ARMCMx/compilers/GCC
+PORTINC = $(STARTUPINC) \
+          $(CHIBIOS)/os/nil/ports/ARMCMx \
+          $(CHIBIOS)/os/nil/ports/ARMCMx/compilers/GCC
 
-PORTLD  = ${CHIBIOS}/os/common/ports/ARMCMx/compilers/GCC/ld
+PORTLD  = $(STARTUPLD)
--- os/nil/ports/ARMCMx/compilers/GCC/mk/port_stm32l1xx.mk
+++ os/nil/ports/ARMCMx/compilers/GCC/mk/port_stm32l1xx.mk
@@ -1,16 +1,15 @@
 # List of the ChibiOS/NIL Cortex-M4 STM32L1xx port files.
-PORTSRC = $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/crt0.c \
-          $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/vectors.c \
-          ${CHIBIOS}/os/nil/ports/ARMCMx/nilcore.c \
-          ${CHIBIOS}/os/nil/ports/ARMCMx/nilcore_v7m.c
+include $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/mk/startup_stm32l1xx.mk
+
+PORTSRC = $(STARTUPSRC) \
+          $(CHIBIOS)/os/nil/ports/ARMCMx/nilcore.c \
+          $(CHIBIOS)/os/nil/ports/ARMCMx/nilcore_v7m.c
           
-PORTASM = $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/crt0_v7m.s \
+PORTASM = $(STARTUPASM) \
           $(CHIBIOS)/os/nil/ports/ARMCMx/compilers/GCC/nilcoreasm_v7m.s
 
-PORTINC = ${CHIBIOS}/os/ext/CMSIS/include \
-          ${CHIBIOS}/os/ext/CMSIS/ST \
-          ${CHIBIOS}/os/common/ports/ARMCMx/devices/STM32L1xx \
-          ${CHIBIOS}/os/nil/ports/ARMCMx \
-          ${CHIBIOS}/os/nil/ports/ARMCMx/compilers/GCC
+PORTINC = $(STARTUPINC) \
+          $(CHIBIOS)/os/nil/ports/ARMCMx \
+          $(CHIBIOS)/os/nil/ports/ARMCMx/compilers/GCC
 
-PORTLD  = ${CHIBIOS}/os/common/ports/ARMCMx/compilers/GCC/ld
+PORTLD  = $(STARTUPLD)
