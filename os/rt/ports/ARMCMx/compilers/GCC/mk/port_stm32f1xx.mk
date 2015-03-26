2eb806050267e45254faf44bb6dfd73a5e0f1fe5
Giovanni Di Sirio <gdisirio@gmail.com>
2015-03-26 15:48:31 +0000

    Improved mk files for STM32 ports. Removed an unused file.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@7814 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/rt/ports/ARMCMx/compilers/GCC/mk/port_stm32f1xx.mk
+++ os/rt/ports/ARMCMx/compilers/GCC/mk/port_stm32f1xx.mk
@@ -1,16 +1,15 @@
 # List of the ChibiOS/RT Cortex-M3 STM32F1xx port files.
-PORTSRC = $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/crt0.c \
-          $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/vectors.c \
+include $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/mk/startup_stm32f1xx.mk
+
+PORTSRC = $(STARTUPSRC) \
           $(CHIBIOS)/os/rt/ports/ARMCMx/chcore.c \
           $(CHIBIOS)/os/rt/ports/ARMCMx/chcore_v7m.c
           
-PORTASM = $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/crt0_v7m.s \
+PORTASM = $(STARTUPASM) \
           $(CHIBIOS)/os/rt/ports/ARMCMx/compilers/GCC/chcoreasm_v7m.s
 
-PORTINC = $(CHIBIOS)/os/ext/CMSIS/include \
-          $(CHIBIOS)/os/ext/CMSIS/ST \
-          $(CHIBIOS)/os/common/ports/ARMCMx/devices/STM32F1xx \
+PORTINC = $(STARTUPINC) \
           $(CHIBIOS)/os/rt/ports/ARMCMx \
           $(CHIBIOS)/os/rt/ports/ARMCMx/compilers/GCC
 
-PORTLD  = $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/ld
+PORTLD  = $(STARTUPLD)
