a4cef8ba42643736d94e4e0ede3b217739339ee3
Giovanni Di Sirio <gdisirio@gmail.com>
2015-03-29 09:40:45 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@7829 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/rt/ports/ARM/compilers/GCC/mk/port_generic.mk
+++ os/rt/ports/ARM/compilers/GCC/mk/port_generic.mk
@@ -1,9 +1,7 @@
 # List of the ChibiOS/RT ARM generic port files.
 PORTSRC = ${CHIBIOS}/os/rt/ports/ARM/chcore.c
 
-PORTASM = $(CHIBIOS)/os/common/ports/ARM/compilers/GCC/vectors.s \
-          $(CHIBIOS)/os/common/ports/ARM/compilers/GCC/crt0.s \
-          $(CHIBIOS)/os/rt/ports/ARM/compilers/GCC/chcoreasm.s
+PORTASM = $(CHIBIOS)/os/rt/ports/ARM/compilers/GCC/chcoreasm.s
 
 PORTINC = ${CHIBIOS}/os/rt/ports/ARM \
           ${CHIBIOS}/os/rt/ports/ARM/compilers/GCC
