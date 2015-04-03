b360a11a9648e9e656b9b907814229f8e113f9e5
Giovanni Di Sirio <gdisirio@gmail.com>
2015-04-03 08:13:11 +0000

    Removed extra "Done" from makefile rules.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@7845 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/common/ports/ARM/compilers/GCC/rules.mk
+++ os/common/ports/ARM/compilers/GCC/rules.mk
@@ -267,8 +267,6 @@ else
 	@$(OD) $(ODFLAGS) $< > $@
 	@echo
 	@$(SZ) $<
-	@echo
-	@echo Done
 endif
 
 %.list: %.elf $(LDSCRIPT)
@@ -277,6 +275,7 @@ ifeq ($(USE_VERBOSE_COMPILE),yes)
 else
 	@echo Creating $@
 	@$(OD) -S $< > $@
+	@echo
 	@echo Done
 endif
 
