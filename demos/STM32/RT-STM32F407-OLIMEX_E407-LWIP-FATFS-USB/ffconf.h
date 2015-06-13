38e7c0e46d3851a908c6fc98fc898b413aeb62c8
Giovanni Di Sirio <gdisirio@gmail.com>
2015-06-13 13:06:31 +0000

    FatFS timeout fix.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@8030 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- demos/STM32/RT-STM32F407-OLIMEX_E407-LWIP-FATFS-USB/ffconf.h
+++ demos/STM32/RT-STM32F407-OLIMEX_E407-LWIP-FATFS-USB/ffconf.h
@@ -191,8 +191,8 @@
 /  with file lock control. This feature uses bss _FS_LOCK * 12 bytes. */
 
 
-#define _FS_REENTRANT   0       /* 0:Disable or 1:Enable */
-#define _FS_TIMEOUT     1000    /* Timeout period in unit of time tick */
+#define _FS_REENTRANT   0               /* 0:Disable or 1:Enable */
+#define _FS_TIMEOUT     MS2ST(1000)     /* Timeout period in unit of time tick */
 #define _SYNC_t         semaphore_t*    /* O/S dependent sync object type. e.g. HANDLE, OS_EVENT*, ID, SemaphoreHandle_t and etc.. */
 /* The _FS_REENTRANT option switches the re-entrancy (thread safe) of the FatFs module.
 /
