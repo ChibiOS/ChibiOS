6425481eb9383061a876f73b41d4a3bfda939e94
Fabio Utzig <utzig@utzig.org>
2015-07-26 14:17:40 +0000

    [AVR] Make tickless default for Mega demo
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@8110 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- demos/AVR/RT-ARDUINOMEGA/chconf.h
+++ demos/AVR/RT-ARDUINOMEGA/chconf.h
@@ -46,7 +46,7 @@
  * @details Frequency of the system timer that drives the system ticks. This
  *          setting also defines the system tick time unit.
  */
-#define CH_CFG_ST_FREQUENCY                 1000
+#define CH_CFG_ST_FREQUENCY                 15624
 
 /**
  * @brief   Time delta constant for the tick-less mode.
@@ -56,7 +56,7 @@
  *          The value one is not valid, timeouts are rounded up to
  *          this value.
  */
-#define CH_CFG_ST_TIMEDELTA                 0
+#define CH_CFG_ST_TIMEDELTA                 2
 
 /** @} */
 
@@ -78,7 +78,7 @@
  * @note    Disabling the round robin preemption makes the kernel more compact
  *          and generally faster.
  */
-#define CH_CFG_TIME_QUANTUM                 20
+#define CH_CFG_TIME_QUANTUM                 0
 
 /**
  * @brief   Managed RAM size.
