82ed9f3093c6eee305364314d891f73d64507ce0
utzig <utzig@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2015-08-12 00:36:29 +0000

    [KINETIS] Fix flash security block removal on RVCT
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@8206 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/ports/KINETIS/K20x/hal_lld.c
+++ os/hal/ports/KINETIS/K20x/hal_lld.c
@@ -36,7 +36,11 @@
 /* Driver local variables and types.                                         */
 /*===========================================================================*/
 
+#ifdef __CC_ARM
+__attribute__ ((section(".ARM.__at_0x400")))
+#else
 __attribute__ ((section(".cfmconfig")))
+#endif
 const uint8_t _cfm[0x10] = {
   0xFF,  /* NV_BACKKEY3: KEY=0xFF */
   0xFF,  /* NV_BACKKEY2: KEY=0xFF */
