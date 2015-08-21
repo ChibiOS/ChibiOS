3caa603df42e9215259d6d2256da1c3911e7d8ae
Giovanni Di Sirio <gdisirio@gmail.com>
2015-08-21 13:05:18 +0000

    Fixed bug #632.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@8229 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/include/hal.h
+++ os/hal/include/hal.h
@@ -101,7 +101,7 @@
 /**
  * @brief   HAL version string.
  */
-#define HAL_VERSION             "3.0.0"
+#define HAL_VERSION             "3.0.1"
 
 /**
  * @brief   HAL version major number.
@@ -116,7 +116,7 @@
 /**
  * @brief   HAL version patch number.
  */
-#define CH_HAL_PATCH            0
+#define CH_HAL_PATCH            1
 /** @} */
 
 /**
