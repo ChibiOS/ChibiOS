09b0d9645a7c5b103f53b10eadabc839cde88054
Giovanni Di Sirio <gdisirio@gmail.com>
2015-09-25 11:39:33 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@8327 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- demos/STM32/RT-STM32F746G-DISCOVERY-LWIP-FATFS-USB/chconf.h
+++ demos/STM32/RT-STM32F746G-DISCOVERY-LWIP-FATFS-USB/chconf.h
@@ -494,6 +494,8 @@
 /* Port-specific settings (override port settings defaulted in chcore.h).    */
 /*===========================================================================*/
 
+#define CORTEX_VTOR_INIT                    0x00200000U
+
 #endif  /* _CHCONF_H_ */
 
 /** @} */
