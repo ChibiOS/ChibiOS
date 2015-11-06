5b8eb07f31d08d9d69598b18d51b323ff1e41add
Rob Lippert <roblip@gmail.com>
2015-11-05 21:58:43 -0800

    hal/ports/avr: add USB Device-mode driver for USB capable AVRs
    This is a bare-bones driver with just enough support to use the
    existing HAL serial_usb driver as a CDC device on an AVR.
    
    Tested using the CDC driver on a PJRC TEENSY2++ using AT90USB1286 chip.
    
    Signed-off-by: Rob Lippert <roblip@gmail.com>

--- os/hal/ports/AVR/platform.mk
+++ os/hal/ports/AVR/platform.mk
@@ -8,7 +8,8 @@ PLATFORMSRC = ${CHIBIOS}/os/hal/ports/AVR/hal_lld.c \
               ${CHIBIOS}/os/hal/ports/AVR/gpt_lld.c \
               ${CHIBIOS}/os/hal/ports/AVR/pwm_lld.c \
               ${CHIBIOS}/os/hal/ports/AVR/icu_lld.c \
-              ${CHIBIOS}/os/hal/ports/AVR/st_lld.c
+              ${CHIBIOS}/os/hal/ports/AVR/st_lld.c \
+              ${CHIBIOS}/os/hal/ports/AVR/usb_lld.c
 
 # Required include directories
 PLATFORMINC = ${CHIBIOS}/os/hal/ports/AVR
