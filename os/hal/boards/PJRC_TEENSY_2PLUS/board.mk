59c0a558d91240283341397376aec4d82011030a
Rob Lippert <roblip@gmail.com>
2015-11-05 21:50:25 -0800

    hal/boards: add basic support for PJRC TEENSY2++ AVR board
    Signed-off-by: Rob Lippert <roblip@gmail.com>

---
+++ os/hal/boards/PJRC_TEENSY_2PLUS/board.mk
@@ -0,0 +1,5 @@
+# List of all the board related files.
+BOARDSRC = ${CHIBIOS}/os/hal/boards/PJRC_TEENSY_2PLUS/board.c
+
+# Required include directories
+BOARDINC = ${CHIBIOS}/os/hal/boards/PJRC_TEENSY_2PLUS
