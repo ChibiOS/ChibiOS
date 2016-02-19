6865ddcce5e27e124f2f7c114391191ce1f4bd27
Giovanni Di Sirio <gdisirio@gmail.com>
2016-02-19 11:19:42 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@8911 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/nil/src/ch.c
+++ os/nil/src/ch.c
@@ -942,7 +942,9 @@ void chSemResetI(semaphore_t *sp, cnt_t n) {
     tp++;
   }
 }
+#endif /* CH_CFG_USE_SEMAPHORES == TRUE */
 
+#if (CH_CFG_USE_EVENTS == TRUE) || defined(__DOXYGEN__)
 /**
  * @brief   Adds a set of event flags directly to the specified @p thread_t.
  *
@@ -958,9 +960,7 @@ void chEvtSignal(thread_t *tp, eventmask_t mask) {
   chSchRescheduleS();
   chSysUnlock();
 }
-#endif /* CH_CFG_USE_SEMAPHORES == TRUE */
 
-#if (CH_CFG_USE_EVENTS == TRUE) || defined(__DOXYGEN__)
 /**
  * @brief   Adds a set of event flags directly to the specified @p thread_t.
  * @post    This function does not reschedule so a call to a rescheduling
