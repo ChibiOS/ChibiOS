3237ad67f64a1f7a9679d155a058821c18953348
Rocco Marco Guglielmi <roccomarco.guglielmi@live.com>
2016-03-14 22:19:03 +0000

    Fixed wrong PLL Multiplier value
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@9107 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- demos/STM32/RT-STM32F334R8-NUCLEO/mcuconf.h
+++ demos/STM32/RT-STM32F334R8-NUCLEO/mcuconf.h
@@ -46,7 +46,7 @@
 #define STM32_SW                            STM32_SW_PLL
 #define STM32_PLLSRC                        STM32_PLLSRC_HSI
 #define STM32_PREDIV_VALUE                  1
-#define STM32_PLLMUL_VALUE                  12
+#define STM32_PLLMUL_VALUE                  9
 #define STM32_HPRE                          STM32_HPRE_DIV1
 #define STM32_PPRE1                         STM32_PPRE1_DIV2
 #define STM32_PPRE2                         STM32_PPRE2_DIV2
