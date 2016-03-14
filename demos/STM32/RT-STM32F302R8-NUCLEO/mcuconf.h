ddc7f99509bfa45808d9b393bbcfe8f183773b64
Giovanni Di Sirio <gdisirio@gmail.com>
2016-03-14 09:54:24 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_16.1.x@9095 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- demos/STM32/RT-STM32F302R8-NUCLEO/mcuconf.h
+++ demos/STM32/RT-STM32F302R8-NUCLEO/mcuconf.h
@@ -77,6 +77,20 @@
 #define STM32_CAN_CAN1_IRQ_PRIORITY         11
 
 /*
+ * DAC driver system settings.
+ */
+#define STM32_DAC_DUAL_MODE                 FALSE
+#define STM32_DAC_USE_DAC1_CH1              TRUE
+#define STM32_DAC_USE_DAC1_CH2              TRUE
+#define STM32_DAC_USE_DAC2_CH1              TRUE
+#define STM32_DAC_DAC1_CH1_IRQ_PRIORITY     10
+#define STM32_DAC_DAC1_CH2_IRQ_PRIORITY     10
+#define STM32_DAC_DAC2_CH1_IRQ_PRIORITY     10
+#define STM32_DAC_DAC1_CH1_DMA_PRIORITY     2
+#define STM32_DAC_DAC1_CH2_DMA_PRIORITY     2
+#define STM32_DAC_DAC2_CH1_DMA_PRIORITY     2
+
+/*
  * EXT driver system settings.
  */
 #define STM32_EXT_EXTI0_IRQ_PRIORITY        6
