db27f15531cc75d13c04211793ba3c9c91c6aeab
Rocco Marco Guglielmi <roccomarco.guglielmi@live.com>
2016-03-15 15:49:58 +0000

    Updated license
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@9115 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/lib/peripherals/sensors/hal_compass.h
+++ os/hal/lib/peripherals/sensors/hal_compass.h
@@ -1,5 +1,5 @@
 /*
-    ChibiOS - Copyright (C) 2016 Rocco Marco Guglielmi
+    ChibiOS - Copyright (C) 2006..2015 Giovanni Di Sirio
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
