50b0e64179d55478206b7a3a6fc8e852d12d373d
Giovanni Di Sirio <gdisirio@gmail.com>
2016-04-02 09:43:49 +0000

    AVR HAL LLD files renamed.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@9219 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/ports/AVR/platform.mk
+++ os/hal/ports/AVR/platform.mk
@@ -1,14 +1,14 @@
 # List of all the AVR platform files.
 PLATFORMSRC = ${CHIBIOS}/os/hal/ports/AVR/hal_lld.c \
-              ${CHIBIOS}/os/hal/ports/AVR/pal_lld.c \
-              ${CHIBIOS}/os/hal/ports/AVR/serial_lld.c \
-              ${CHIBIOS}/os/hal/ports/AVR/adc_lld.c \
-              ${CHIBIOS}/os/hal/ports/AVR/i2c_lld.c \
-              ${CHIBIOS}/os/hal/ports/AVR/spi_lld.c \
-              ${CHIBIOS}/os/hal/ports/AVR/gpt_lld.c \
-              ${CHIBIOS}/os/hal/ports/AVR/pwm_lld.c \
-              ${CHIBIOS}/os/hal/ports/AVR/icu_lld.c \
-              ${CHIBIOS}/os/hal/ports/AVR/st_lld.c
+              ${CHIBIOS}/os/hal/ports/AVR/hal_pal_lld.c \
+              ${CHIBIOS}/os/hal/ports/AVR/hal_serial_lld.c \
+              ${CHIBIOS}/os/hal/ports/AVR/hal_adc_lld.c \
+              ${CHIBIOS}/os/hal/ports/AVR/hal_i2c_lld.c \
+              ${CHIBIOS}/os/hal/ports/AVR/hal_spi_lld.c \
+              ${CHIBIOS}/os/hal/ports/AVR/hal_gpt_lld.c \
+              ${CHIBIOS}/os/hal/ports/AVR/hal_pwm_lld.c \
+              ${CHIBIOS}/os/hal/ports/AVR/hal_icu_lld.c \
+              ${CHIBIOS}/os/hal/ports/AVR/hal_st_lld.c
 
 # Required include directories
 PLATFORMINC = ${CHIBIOS}/os/hal/ports/AVR
