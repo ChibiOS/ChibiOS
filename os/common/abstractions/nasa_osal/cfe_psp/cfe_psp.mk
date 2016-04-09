19678138f0aec601213e58f550935967fd179179
Giovanni Di Sirio <gdisirio@gmail.com>
2016-04-09 11:17:13 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@9259 35acf78f-673a-0410-8e92-d51de3d6d3f4

---
+++ os/common/abstractions/nasa_osal/cfe_psp/cfe_psp.mk
@@ -0,0 +1,4 @@
+# NASA CFE PSP files.
+CFEOSALSRC = $(CHIBIOS)/os/common/abstractions/nasa_cfe/cfe_psp/src/cfe_psp_support.c
+
+CFEOSALINC = $(CHIBIOS)/os/common/abstractions/nasa_cfe/cfe_psp/include
--- os/common/abstractions/nasa_osal/nasa_osal.mk
+++
@@ -1,4 +0,0 @@
-# NASAOSAL files.
-NASAOSALSRC = $(CHIBIOS)/os/common/abstractions/nasa_osal/src/osapi.c
-
-NASAOSALINC = $(CHIBIOS)/os/common/abstractions/nasa_osal/include
