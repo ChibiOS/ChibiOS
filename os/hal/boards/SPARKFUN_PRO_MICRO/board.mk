e555cd14ab74a2d258b43c86a016f01243c3cfce
Fabio Utzig <utzig@utzig.org>
2016-05-15 12:17:52 +0000

    [AVR] Add support for sparkfun pro micro board
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@9489 35acf78f-673a-0410-8e92-d51de3d6d3f4

---
+++ os/hal/boards/SPARKFUN_PRO_MICRO/board.mk
@@ -0,0 +1,5 @@
+# List of all the board related files.
+BOARDSRC = ${CHIBIOS}/os/hal/boards/SPARKFUN_PRO_MICRO/board.c
+
+# Required include directories
+BOARDINC = ${CHIBIOS}/os/hal/boards/SPARKFUN_PRO_MICRO
