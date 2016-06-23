01a1b6800b76f4d0965f8ca54367b27eb1a04444
Rocco Marco Guglielmi <roccomarco.guglielmi@live.com>
2016-06-23 09:11:11 +0000

    Added XMC1100 initial demo
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@9650 35acf78f-673a-0410-8e92-d51de3d6d3f4

---
+++ os/common/startup/ARMCMx/compilers/GCC/mk/startup_xmc1100.mk
@@ -0,0 +1,12 @@
+# List of the ChibiOS generic XMC1xxx startup and CMSIS files.
+STARTUPSRC = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/crt1.c \
+             $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/vectors.c
+          
+STARTUPASM = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/crt0_v6m.S
+
+STARTUPINC = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC \
+             $(CHIBIOS)/os/common/startup/ARMCMx/devices/XMC1100 \
+             $(CHIBIOS)/os/common/ext/CMSIS/include \
+             $(CHIBIOS)/os/common/ext/CMSIS/Infineon/XMC1100
+
+STARTUPLD  = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/ld
