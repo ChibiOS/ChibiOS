501dce6dc591f065acdf3e83b2de29e9bf7af4a4
Giovanni Di Sirio <gdisirio@gmail.com>
2016-09-29 14:58:53 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@9822 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/common/startup/ARMCMx/compilers/GCC/mk/startup_xmc1100.mk
+++ os/common/startup/ARMCMx/compilers/GCC/mk/startup_xmc1100.mk
@@ -1,6 +1,5 @@
 # List of the ChibiOS generic XMC1100 startup and CMSIS files.
-STARTUPSRC = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/crt1.c \
-             $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/vectors.c
+STARTUPSRC = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/crt1.c
           
 STARTUPASM = $(CHIBIOS)/os/common/startup/ARMCMx/devices/XMC1100/veneers_gcc.S \
              $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/crt0_v6m.S
