65b57a9e9e71532fcd811bbb4f2983fd5e83e0b6
Giovanni Di Sirio <gdisirio@gmail.com>
2016-12-18 14:16:23 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@9968 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- demos/STM32/RT-STM32F107-OLIMEX_P107-LWIP/main.c
+++ demos/STM32/RT-STM32F107-OLIMEX_P107-LWIP/main.c
@@ -1,5 +1,5 @@
 /*
-    ChibiOS - Copyright (C) 2006..2016 Giovanni Di Sirio
+    ChibiOSch_ - Copyright (C) 2006..2016 Giovanni Di Sirio
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -14,247 +14,27 @@
     limitations under the License.
 */
 
-#include <stdio.h>
-#include <string.h>
-
 #include "ch.h"
 #include "hal.h"
 #include "ch_test.h"
 
-#include "chprintf.h"
-#include "shell.h"
-
 #include "lwipthread.h"
+
 #include "web/web.h"
-
-#include "ff.h"
-
-#include "usbcfg.h"
-
-/*===========================================================================*/
-/* Card insertion monitor.                                                   */
-/*===========================================================================*/
-
-#define POLLING_INTERVAL                10
-#define POLLING_DELAY                   10
-
-/**
- * @brief   Card monitor timer.
- */
-static virtual_timer_t tmr;
-
-/**
- * @brief   Debounce counter.
- */
-static unsigned cnt;
-
-/**
- * @brief   Card event sources.
- */
-static event_source_t inserted_event, removed_event;
-
-/**
- * @brief   Insertion monitor timer callback function.
- *
- * @param[in] p         pointer to the @p BaseBlockDevice object
- *
- * @notapi
- */
-static void tmrfunc(void *p) {
-  BaseBlockDevice *bbdp = p;
-
-  chSysLockFromISR();
-  if (cnt > 0) {
-    if (blkIsInserted(bbdp)) {
-      if (--cnt == 0) {
-        chEvtBroadcastI(&inserted_event);
-      }
-    }
-    else
-      cnt = POLLING_INTERVAL;
-  }
-  else {
-    if (!blkIsInserted(bbdp)) {
-      cnt = POLLING_INTERVAL;
-      chEvtBroadcastI(&removed_event);
-    }
-  }
-  chVTSetI(&tmr, MS2ST(POLLING_DELAY), tmrfunc, bbdp);
-  chSysUnlockFromISR();
-}
-
-/**
- * @brief   Polling monitor start.
- *
- * @param[in] p         pointer to an object implementing @p BaseBlockDevice
- *
- * @notapi
- */
-static void tmr_init(void *p) {
-
-  chEvtObjectInit(&inserted_event);
-  chEvtObjectInit(&removed_event);
-  chSysLock();
-  cnt = POLLING_INTERVAL;
-  chVTSetI(&tmr, MS2ST(POLLING_DELAY), tmrfunc, p);
-  chSysUnlock();
-}
-
-/*===========================================================================*/
-/* FatFs related.                                                            */
-/*===========================================================================*/
-
-/**
- * @brief FS object.
- */
-static FATFS SDC_FS;
-
-/* FS mounted and ready.*/
-static bool fs_ready = FALSE;
-
-/* Generic large buffer.*/
-static uint8_t fbuff[1024];
-
-static FRESULT scan_files(BaseSequentialStream *chp, char *path) {
-  FRESULT res;
-  FILINFO fno;
-  DIR dir;
-  int i;
-  char *fn;
-
-#if _USE_LFN
-  fno.lfname = 0;
-  fno.lfsize = 0;
-#endif
-  res = f_opendir(&dir, path);
-  if (res == FR_OK) {
-    i = strlen(path);
-    for (;;) {
-      res = f_readdir(&dir, &fno);
-      if (res != FR_OK || fno.fname[0] == 0)
-        break;
-      if (fno.fname[0] == '.')
-        continue;
-      fn = fno.fname;
-      if (fno.fattrib & AM_DIR) {
-        path[i++] = '/';
-        strcpy(&path[i], fn);
-        res = scan_files(chp, path);
-        if (res != FR_OK)
-          break;
-        path[--i] = 0;
-      }
-      else {
-        chprintf(chp, "%s/%s\r\n", path, fn);
-      }
-    }
-  }
-  return res;
-}
-
-/*===========================================================================*/
-/* Command line related.                                                     */
-/*===========================================================================*/
-
-#define SHELL_WA_SIZE   THD_WORKING_AREA_SIZE(2048)
-
-static void cmd_tree(BaseSequentialStream *chp, int argc, char *argv[]) {
-  FRESULT err;
-  uint32_t clusters;
-  FATFS *fsp;
-
-  (void)argv;
-  if (argc > 0) {
-    chprintf(chp, "Usage: tree\r\n");
-    return;
-  }
-  if (!fs_ready) {
-    chprintf(chp, "File System not mounted\r\n");
-    return;
-  }
-  err = f_getfree("/", &clusters, &fsp);
-  if (err != FR_OK) {
-    chprintf(chp, "FS: f_getfree() failed\r\n");
-    return;
-  }
-  chprintf(chp,
-           "FS: %lu free clusters, %lu sectors per cluster, %lu bytes free\r\n",
-           clusters, (uint32_t)SDC_FS.csize,
-           clusters * (uint32_t)SDC_FS.csize * (uint32_t)MMCSD_BLOCK_SIZE);
-  fbuff[0] = 0;
-  scan_files(chp, (char *)fbuff);
-}
-
-static const ShellCommand commands[] = {
-  {"tree", cmd_tree},
-  {NULL, NULL}
-};
-
-static const ShellConfig shell_cfg1 = {
-  (BaseSequentialStream *)&SDU1,
-  commands
-};
-
-/*===========================================================================*/
-/* Main and generic code.                                                    */
-/*===========================================================================*/
-
-static thread_t *shelltp = NULL;
-
-/*
- * Card insertion event.
- */
-static void InsertHandler(eventid_t id) {
-  FRESULT err;
-
-  (void)id;
-  /*
-   * On insertion SDC initialization and FS mount.
-   */
-  if (sdcConnect(&SDCD1))
-    return;
-
-  err = f_mount(&SDC_FS, "/", 1);
-  if (err != FR_OK) {
-    sdcDisconnect(&SDCD1);
-    return;
-  }
-  fs_ready = TRUE;
-}
-
-/*
- * Card removal event.
- */
-static void RemoveHandler(eventid_t id) {
-
-  (void)id;
-  sdcDisconnect(&SDCD1);
-  fs_ready = FALSE;
-}
-
-/*
- * Shell exit event.
- */
-static void ShellHandler(eventid_t id) {
-
-  (void)id;
-  if (chThdTerminatedX(shelltp)) {
-    chThdWait(shelltp);                 /* Returning memory to heap.        */
-    shelltp = NULL;
-  }
-}
 
 /*
  * Green LED blinker thread, times are in milliseconds.
  */
-static THD_WORKING_AREA(waThread1, 128);
-static THD_FUNCTION(Thread1, arg) {
+static WORKING_AREA(waThread1, 128);
+static msg_t Thread1(void *arg) {
 
   (void)arg;
   chRegSetThreadName("blinker");
-  while (true) {
-    palTogglePad(GPIOC, GPIOC_LED);
-    chThdSleepMilliseconds(fs_ready ? 125 : 500);
+  while (TRUE) {
+    palClearPad(GPIOC, GPIOC_LED_STATUS1);
+    chThdSleepMilliseconds(500);
+    palSetPad(GPIOC, GPIOC_LED_STATUS1);
+    chThdSleepMilliseconds(500);
   }
 }
 
@@ -262,12 +42,6 @@ static THD_FUNCTION(Thread1, arg) {
  * Application entry point.
  */
 int main(void) {
-  static const evhandler_t evhndl[] = {
-    InsertHandler,
-    RemoveHandler,
-    ShellHandler
-  };
-  event_listener_t el0, el1, el2;
 
   /*
    * System initializations.
@@ -275,44 +49,15 @@ int main(void) {
    *   and performs the board-specific initializations.
    * - Kernel initialization, the main() function becomes a thread and the
    *   RTOS is active.
-   * - lwIP subsystem initialization using the default configuration.
    */
   halInit();
   chSysInit();
   lwipInit(NULL);
 
   /*
-   * Initializes a serial-over-USB CDC driver.
+   * Activates the serial driver 3 using the driver default configuration.
    */
-  sduObjectInit(&SDU1);
-  sduStart(&SDU1, &serusbcfg);
-
-  /*
-   * Activates the USB driver and then the USB bus pull-up on D+.
-   * Note, a delay is inserted in order to not have to disconnect the cable
-   * after a reset.
-   */
-  usbDisconnectBus(serusbcfg.usbp);
-  chThdSleepMilliseconds(1500);
-  usbStart(serusbcfg.usbp, &usbcfg);
-  usbConnectBus(serusbcfg.usbp);
-
-  /*
-   * Shell manager initialization.
-   */
-  shellInit();
-
-  /*
-   * Activates the serial driver 6 and SDC driver 1 using default
-   * configuration.
-   */
-  sdStart(&SD6, NULL);
-  sdcStart(&SDCD1, NULL);
-
-  /*
-   * Activates the card insertion monitor.
-   */
-  tmr_init(&SDCD1);
+  sdStart(&SD3, NULL);
 
   /*
    * Creates the blinker thread.
@@ -326,18 +71,12 @@ int main(void) {
                     http_server, NULL);
 
   /*
-   * Normal main() thread activity, handling SD card events and shell
-   * start/exit.
+   * Normal main() thread activity, in this demo it does nothing except
+   * sleeping in a loop and check the button state.
    */
-  chEvtRegister(&inserted_event, &el0, 0);
-  chEvtRegister(&removed_event, &el1, 1);
-  chEvtRegister(&shell_terminated, &el2, 2);
   while (true) {
-    if (!shelltp && (SDU1.config->usbp->state == USB_ACTIVE)) {
-      shelltp = chThdCreateFromHeap(NULL, SHELL_WA_SIZE,
-                                    "shell", NORMALPRIO + 1,
-                                    shellThread, (void *)&shell_cfg1);
-    }
-    chEvtDispatch(evhndl, chEvtWaitOneTimeout(ALL_EVENTS, MS2ST(500)));
+    if (palReadPad(GPIOC, GPIOC_SWITCH_TAMPER) == 0)
+      TestThread(&SD3);
+    chThdSleepMilliseconds(500);
   }
 }
