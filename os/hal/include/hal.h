6f7b44d03e4b4e01f96d97bfeceeb084d6fe1786
Giovanni Di Sirio <gdisirio@gmail.com>
2017-01-16 18:17:29 +0000

    Updated version number.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_16.1.x@10056 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/hal/include/hal.h
+++ os/hal/include/hal.h
@@ -103,7 +103,7 @@
 /**
  * @brief   HAL version string.
  */
-#define HAL_VERSION             "4.0.6"
+#define HAL_VERSION             "4.0.7"
 
 /**
  * @brief   HAL version major number.
@@ -118,7 +118,7 @@
 /**
  * @brief   HAL version patch number.
  */
-#define CH_HAL_PATCH            6
+#define CH_HAL_PATCH            7
 /** @} */
 
 /**
