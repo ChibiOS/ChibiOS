09fdf34a2516eefd6ddf362b4f83a31145242891
tfateba <tfateba@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2017-04-26 19:49:03 +0000

    Change the macro on the hearder file.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@10189 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/ports/AVR/hal_i2c_lld.h
+++ os/hal/ports/AVR/hal_i2c_lld.h
@@ -22,8 +22,8 @@
  * @{
  */
 
-#ifndef _I2C_LLD_H_
-#define _I2C_LLD_H_
+#ifndef I2C_LLD_H
+#define I2C_LLD_H
 
 #if HAL_USE_I2C || defined(__DOXYGEN__)
 
@@ -215,6 +215,6 @@ extern "C" {
 
 #endif /* HAL_USE_I2C */
 
-#endif /* _I2C_LLD_H_ */
+#endif /* I2C_LLD_H */
 
 /** @} */
