d63dbb9e383589a44d0823fd97feb6a5fa22e747
edolomb <none@example.com>
2017-08-10 12:10:49 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@10382 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/ports/SAMA/SAMA5D2x/platform.mk
+++ os/hal/ports/SAMA/SAMA5D2x/platform.mk
@@ -1,15 +1,15 @@
 # Required platform files.
-# TODO add $(CHIBIOS)/os/hal/ports/common/ARMCAx/aic.c
-PLATFORMSRC := $(CHIBIOS)/os/hal/ports/SAMA/SAMA5D2x/hal_lld.c \
-               $(CHIBIOS)/os/hal/ports/SAMA/SAMA5D2x/hal_st_lld.c 
-# TODO add $(CHIBIOS)/os/hal/ports/common/ARMCAx     
-# Required include directories.
-PLATFORMINC := $(CHIBIOS)/os/hal/ports/SAMA/SAMA5D2x
 
-               
+PLATFORMSRC := $(CHIBIOS)/os/hal/ports/SAMA/SAMA5D2x/hal_lld.c \
+               $(CHIBIOS)/os/hal/ports/SAMA/SAMA5D2x/hal_st_lld.c \
+               $(CHIBIOS)/os/hal/ports/common/ARMCAx/aic.c
+     
+# Required include directories.
+PLATFORMINC := $(CHIBIOS)/os/hal/ports/SAMA/SAMA5D2x \
+               $(CHIBIOS)/os/hal/ports/common/ARMCAx
 
 # TODO: Add smart build.
 
 # Drivers compatible with the platform.
-#include $(CHIBIOS)/os/hal/ports/ATMEL/LLD/DMAv1/driver.mk
-#include $(CHIBIOS)/os/hal/ports/ATMEL/LLD/SPIv1/driver.mk
+#include $(CHIBIOS)/os/hal/ports/SAMA/LLD/DMAv1/driver.mk
+#include $(CHIBIOS)/os/hal/ports/SAMA/LLD/SPIv1/driver.mk
