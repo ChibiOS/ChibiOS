ccdc3635ef37ce5a0f83dc0f1e7fd5f79d94821f
Theodore Ateba <tf.ateba@gmail.com>
2017-08-16 06:38:55 +0000

    Modify AVR XMEGA platform according the new architecture.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@10432 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/ports/AVR/MEGA/ATMEGAxx/platform.mk
+++ os/hal/ports/AVR/MEGA/ATMEGAxx/platform.mk
@@ -1,15 +1,15 @@
 # List of all the AVR platform files.
-PLATFORMSRC = ${CHIBIOS}/os/hal/ports/AVR/MEGA/hal_lld.c \
+PLATFORMSRC = ${CHIBIOS}/os/hal/ports/AVR/MEGA/ATMEGAxx/hal_lld.c \
 
 # Required include directories
-PLATFORMINC = ${CHIBIOS}/os/hal/ports/AVR/MEGA
+PLATFORMINC = ${CHIBIOS}/os/hal/ports/AVR/MEGA/ATMEGAxx
 
 # Drivers compatible with the platform.
-include $(CHIBIOS)/os/hal/ports/AVR/MEGA/ADCv1/driver.mk
-include $(CHIBIOS)/os/hal/ports/AVR/MEGA/EXTv1/driver.mk
-include $(CHIBIOS)/os/hal/ports/AVR/MEGA/GPIOv1/driver.mk
-include $(CHIBIOS)/os/hal/ports/AVR/MEGA/I2Cv1/driver.mk
-include $(CHIBIOS)/os/hal/ports/AVR/MEGA/SPIv1/driver.mk
-include $(CHIBIOS)/os/hal/ports/AVR/MEGA/TIMv1/driver.mk
-include $(CHIBIOS)/os/hal/ports/AVR/MEGA/USARTv1/driver.mk
-include $(CHIBIOS)/os/hal/ports/AVR/MEGA/USBv1/driver.mk
+include $(CHIBIOS)/os/hal/ports/AVR/MEGA/LLD/ADCv1/driver.mk
+include $(CHIBIOS)/os/hal/ports/AVR/MEGA/LLD/EXTv1/driver.mk
+include $(CHIBIOS)/os/hal/ports/AVR/MEGA/LLD/GPIOv1/driver.mk
+include $(CHIBIOS)/os/hal/ports/AVR/MEGA/LLD/I2Cv1/driver.mk
+include $(CHIBIOS)/os/hal/ports/AVR/MEGA/LLD/SPIv1/driver.mk
+include $(CHIBIOS)/os/hal/ports/AVR/MEGA/LLD/TIMv1/driver.mk
+include $(CHIBIOS)/os/hal/ports/AVR/MEGA/LLD/USARTv1/driver.mk
+include $(CHIBIOS)/os/hal/ports/AVR/MEGA/LLD/USBv1/driver.mk
