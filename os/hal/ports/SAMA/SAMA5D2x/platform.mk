84807bcb98e315038b0bc10a62d1518ad36879be
edolomb <edolomb@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2017-08-24 16:10:13 +0000

    Included DMAv1 Driver
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@10474 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/ports/SAMA/SAMA5D2x/platform.mk
+++ os/hal/ports/SAMA/SAMA5D2x/platform.mk
@@ -10,5 +10,5 @@ PLATFORMINC := $(CHIBIOS)/os/hal/ports/SAMA/SAMA5D2x
 # TODO: Add smart build.
 
 # Drivers compatible with the platform.
-#include $(CHIBIOS)/os/hal/ports/SAMA/LLD/DMAv1/driver.mk
+include $(CHIBIOS)/os/hal/ports/SAMA/LLD/DMAv1/driver.mk
 #include $(CHIBIOS)/os/hal/ports/SAMA/LLD/SPIv1/driver.mk
