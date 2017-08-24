6904b7392c60cb91b70ad9acc5c279438d47b41e
Theodore Ateba <tf.ateba@gmail.com>
2017-08-24 23:01:41 +0000

    Add tabulation.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@10478 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/ports/AVR/MEGA/LLD/EXTv1/hal_ext_lld.c
+++ os/hal/ports/AVR/MEGA/LLD/EXTv1/hal_ext_lld.c
@@ -264,13 +264,13 @@ void ext_lld_channel_enable(EXTDriver *extp, expchannel_t channel) {
 #endif
 #if AVR_EXT_USE_INT2 || defined(__DOXYGEN__)
   if (channel == INT2) {
-    EIMSK |= 1 << INT2;
-  ext_lld_set_intx_edges(channel, extp->config->channels[channel].mode);
+    EIMSK |= (1 << INT2);
+    ext_lld_set_intx_edges(channel, extp->config->channels[channel].mode);
   }
 #endif
 #if AVR_EXT_USE_INT3 || defined(__DOXYGEN__)
   if (channel == INT3) {
-    EIMSK |= 1 << INT3;
+    EIMSK |= (1 << INT3);
     ext_lld_set_intx_edges(channel, extp->config->channels[channel].mode);
   }
 #endif
