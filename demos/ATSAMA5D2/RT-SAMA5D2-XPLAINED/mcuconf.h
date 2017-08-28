6452f6f997d2fe9401d6eb5c463d0744055068e5
edolomb <none@example.com>
2017-08-28 13:59:02 +0000

    Added SPI driver system settings
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@10502 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- demos/ATSAMA5D2/RT-SAMA5D2-XPLAINED/mcuconf.h
+++ demos/ATSAMA5D2/RT-SAMA5D2-XPLAINED/mcuconf.h
@@ -23,9 +23,9 @@
  * HAL driver system settings.
  */
 #define SAMA_NO_INIT                        FALSE
-#define SAMA_MOSCRC_ENABLED                 FALSE
-#define SAMA_MOSCXT_ENABLED                 TRUE
-#define SAMA_MOSC_SEL                       SAMA_MOSC_MOSCXT
+#define SAMA_MOSCRC_ENABLED                 TRUE
+#define SAMA_MOSCXT_ENABLED                 FALSE
+#define SAMA_MOSC_SEL                       SAMA_MOSC_MOSCRC
 #define SAMA_OSC_SEL                        SAMA_OSC_OSCXT
 #define SAMA_MCK_SEL                        SAMA_MCK_PLLA_CLK
 #define SAMA_MCK_PRES_VALUE                 1
@@ -33,5 +33,14 @@
 #define SAMA_PLLA_MUL_VALUE                 83
 #define SAMA_PLLADIV2_EN                    TRUE
 #define SAMA_H64MX_H32MX_RATIO              2
+
+/*
+ * SPI driver system settings.
+ */
+#define SAMA_SPI_USE_SPI0                   FALSE
+#define SAMA_SPI_USE_SPI1                   FALSE
+#define SAMA_SPI_SPI0_IRQ_PRIORITY          10
+#define SAMA_SPI_SPI1_IRQ_PRIORITY          10
+#define SAMA_SPI_DMA_ERROR_HOOK(spip)       osalSysHalt("DMA failure")
 
 #endif /* MCUCONF_H */
