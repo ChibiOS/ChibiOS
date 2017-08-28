d3034301c3b3880db2dfc4c33bd8da98fb5d0138
edolomb <none@example.com>
2017-08-28 13:19:50 +0000

    Included SPIv1 Driver
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@10498 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/ports/SAMA/SAMA5D2x/platform.mk
+++ os/hal/ports/SAMA/SAMA5D2x/platform.mk
@@ -11,4 +11,4 @@ PLATFORMINC := $(CHIBIOS)/os/hal/ports/SAMA/SAMA5D2x
 
 # Drivers compatible with the platform.
 include $(CHIBIOS)/os/hal/ports/SAMA/LLD/DMAv1/driver.mk
-#include $(CHIBIOS)/os/hal/ports/SAMA/LLD/SPIv1/driver.mk
+include $(CHIBIOS)/os/hal/ports/SAMA/LLD/SPIv1/driver.mk
