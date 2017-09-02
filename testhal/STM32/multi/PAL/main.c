f63698059e6f5ae4b7a050af57ad2fe5cb0e50b8
Giovanni Di Sirio <gdisirio@gmail.com>
2017-09-02 12:46:59 +0000

    Fixed some comments.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@10530 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- testhal/STM32/multi/PAL/main.c
+++ testhal/STM32/multi/PAL/main.c
@@ -62,7 +62,7 @@ int main(void) {
   chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
 #endif
 
-  /* Enabling callback on both edges of the button line.*/
+  /* Enabling events on both edges of the button line.*/
   palEnableLineEvent(PORTAB_LINE_BUTTON, PAL_EVENT_MODE_BOTH_EDGES,
                      NULL, NULL);
 
@@ -131,7 +131,7 @@ int main(void) {
   chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
 #endif
 
-  /* Enabling callback on both edges of the button line.*/
+  /* Enabling events on both edges of the button line.*/
   palEnableLineEvent(PORTAB_LINE_BUTTON, PAL_EVENT_MODE_BOTH_EDGES,
                      button_cb, NULL);
 
