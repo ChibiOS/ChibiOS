72549fa9c56a405903e7c9cc1e6afc0005b1a208
isiora <isiora@35acf78f-673a-0410-8e92-d51de3d6d3f4>
2017-09-22 10:20:20 +0000

    Entry to Boot_Handler, boot code in startup section.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@10673 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/common/startup/ARM/compilers/GCC/mk/startup_sama5d2.mk
+++ os/common/startup/ARM/compilers/GCC/mk/startup_sama5d2.mk
@@ -1,7 +1,8 @@
 # List of the ChibiOS generic SAMA5D2 file.
 STARTUPSRC = $(CHIBIOS)/os/common/startup/ARM/compilers/GCC/crt1.c
 
-STARTUPASM = $(CHIBIOS)/os/common/startup/ARM/compilers/GCC/vectors.S \
+STARTUPASM = $(CHIBIOS)/os/common/startup/ARM/devices/SAMA5D2/boot.S \
+             $(CHIBIOS)/os/common/startup/ARM/compilers/GCC/vectors.S \
              $(CHIBIOS)/os/common/startup/ARM/compilers/GCC/crt0.S
 
 STARTUPINC = ${CHIBIOS}/os/common/startup/ARM/devices/SAMA5D2
