3b07c94012e480114312ede2e4e1793f937f272c
isiora <none@example.com>
2017-11-22 10:04:23 +0000

    Added mmu.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@11059 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/common/startup/ARMCAx-TZ/compilers/GCC/mk/startup_sama5d2.mk
+++ os/common/startup/ARMCAx-TZ/compilers/GCC/mk/startup_sama5d2.mk
@@ -1,16 +1,12 @@
 # List of the ChibiOS generic SAMA5D2 file.
-STARTUPSRC = $(CHIBIOS)/os/common/startup/ARMCAx-TZ/compilers/GCC/crt1.c
+STARTUPSRC = $(CHIBIOS)/os/common/startup/ARMCAx-TZ/compilers/GCC/crt1.c \
+			 $(CHIBIOS)/os/common/startup/ARMCAx-TZ/devices/SAMA5D2/mmu.c
 
 STARTUPASM = $(CHIBIOS)/os/common/startup/ARMCAx-TZ/devices/SAMA5D2/boot.S \
              $(CHIBIOS)/os/common/startup/ARMCAx-TZ/compilers/GCC/vectors.S \
              $(CHIBIOS)/os/common/startup/ARMCAx-TZ/compilers/GCC/crt0.S
 
 STARTUPINC = ${CHIBIOS}/os/common/startup/ARMCAx-TZ/devices/SAMA5D2 \
-             $(CHIBIOS)/os/common/ext/ARM/CMSIS/Core_A/Include 
+             $(CHIBIOS)/os/common/ext/ARM/CMSIS/Core_A/Include
 
 STARTUPLD  = ${CHIBIOS}/os/common/startup/ARMCAx-TZ/compilers/GCC/ld
-
-# Shared variables
-ALLXASMSRC += $(STARTUPASM)
-ALLCSRC    += $(STARTUPSRC)
-ALLINC     += $(STARTUPINC)
