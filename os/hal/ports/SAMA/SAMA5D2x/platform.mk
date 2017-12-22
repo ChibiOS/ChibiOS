67b0d562b7c6f81dd9cabc3221f1aea9809f57ac
edolomb <none@example.com>
2017-12-22 17:39:26 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@11168 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/ports/SAMA/SAMA5D2x/platform.mk
+++ os/hal/ports/SAMA/SAMA5D2x/platform.mk
@@ -1,11 +1,11 @@
 # Required platform files.
 
-PLATFORMSRC := $(CHIBIOS)/os/hal/ports/SAMA/SAMA5D2x/hal_lld.c     \
-               $(CHIBIOS)/os/hal/ports/SAMA/SAMA5D2x/hal_st_lld.c  \
-               $(CHIBIOS)/os/hal/ports/SAMA/SAMA5D2x/aic.c         \
-               $(CHIBIOS)/os/hal/ports/SAMA/SAMA5D2x/sama_matrix.c \
-               $(CHIBIOS)/os/hal/ports/SAMA/SAMA5D2x/sama_cache.c  \
-               $(CHIBIOS)/os/hal/ports/SAMA/SAMA5D2x/hal_tc_lld.c	\
+PLATFORMSRC := $(CHIBIOS)/os/hal/ports/SAMA/SAMA5D2x/hal_lld.c        \
+               $(CHIBIOS)/os/hal/ports/SAMA/SAMA5D2x/hal_st_lld.c     \
+               $(CHIBIOS)/os/hal/ports/SAMA/SAMA5D2x/aic.c            \
+               $(CHIBIOS)/os/hal/ports/SAMA/SAMA5D2x/sama_matrix.c    \
+               $(CHIBIOS)/os/hal/ports/SAMA/SAMA5D2x/sama_cache.c     \
+               $(CHIBIOS)/os/hal/ports/SAMA/SAMA5D2x/hal_tc_lld.c     \
                $(CHIBIOS)/os/hal/ports/SAMA/SAMA5D2x/hal_crypto_lld.c
                
 # Required include directories.
@@ -26,6 +26,7 @@ endif
 
 # Drivers compatible with the platform.
 include $(CHIBIOS)/os/hal/ports/SAMA/LLD/DMAv1/driver.mk
+include $(CHIBIOS)/os/hal/ports/SAMA/LLD/MACv1/driver.mk
 include $(CHIBIOS)/os/hal/ports/SAMA/LLD/PIOv1/driver.mk
 include $(CHIBIOS)/os/hal/ports/SAMA/LLD/SPIv1/driver.mk
 include $(CHIBIOS)/os/hal/ports/SAMA/LLD/RTCv1/driver.mk
