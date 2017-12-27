163ed286a24255c1c660bb20e943e621272d6d2a
Giovanni Di Sirio <gdisirio@gmail.com>
2017-12-27 09:01:37 +0000

    Added DMAMUX helper. small doc fixes.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@11199 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/ports/STM32/STM32H7xx/stm32_isr.h
+++ os/hal/ports/STM32/STM32H7xx/stm32_isr.h
@@ -15,10 +15,10 @@
 */
 
 /**
- * @file    STM32F7xx/stm32_isr.h
- * @brief   STM32F7xx ISR handler header.
+ * @file    STM32H7xx/stm32_isr.h
+ * @brief   STM32H7xx ISR handler header.
  *
- * @addtogroup STM32F7xx_ISR
+ * @addtogroup STM32H7xx_ISR
  * @{
  */
 
