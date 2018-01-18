16c0ec2cfb97a78550ab9e7a304a74bc8ece4df1
isiora <none@example.com>
2018-01-18 09:33:21 +0000

    Added SMC related sources.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@11314 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/common/startup/ARM/compilers/GCC/mk/startup_sama5d2.mk
+++ os/common/startup/ARM/compilers/GCC/mk/startup_sama5d2.mk
@@ -1,5 +1,6 @@
 # List of the ChibiOS generic SAMA5D2 file.
-STARTUPSRC = $(CHIBIOS)/os/common/startup/ARM/compilers/GCC/crt1.c
+STARTUPSRC = $(CHIBIOS)/os/common/startup/ARM/compilers/GCC/crt1.c \
+			 $(CHIBIOS)/os/common/startup/ARMCAx-TZ/devices/SAMA5D2/mmu.c
 
 STARTUPASM = $(CHIBIOS)/os/common/startup/ARM/devices/SAMA5D2/boot.S \
              $(CHIBIOS)/os/common/startup/ARM/compilers/GCC/vectors.S \
@@ -7,7 +8,8 @@ STARTUPASM = $(CHIBIOS)/os/common/startup/ARM/devices/SAMA5D2/boot.S \
 
 STARTUPINC = $(CHIBIOS)/os/common/portability/GCC \
              ${CHIBIOS}/os/common/startup/ARM/devices/SAMA5D2 \
-             $(CHIBIOS)/os/common/ext/ARM/CMSIS/Core_A/Include 
+             $(CHIBIOS)/os/common/ext/ARM/CMSIS/Core_A/Include \
+             $(CHIBIOS)/os/common/portability/GCC 
 
 STARTUPLD  = ${CHIBIOS}/os/common/startup/ARM/compilers/GCC/ld
 
