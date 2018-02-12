74346e204e4c09f741317f88af8b49a64d37c249
Theodore Ateba <tf.ateba@gmail.com>
2018-02-12 20:18:46 +0000

    AVR: Add USART and SERIAL low level driver to the platform description file.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@11494 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/ports/AVR/XMEGA/ATXMEGAxxxA4U/platform.mk
+++ os/hal/ports/AVR/XMEGA/ATXMEGAxxxA4U/platform.mk
@@ -7,6 +7,7 @@ PLATFORMINC = ${CHIBIOS}/os/hal/ports/AVR/XMEGA/ATXMEGAxxxA4U
 # Drivers compatible with the platform.
 include $(CHIBIOS)/os/hal/ports/AVR/XMEGA/LLD/GPIOv1/driver.mk
 include $(CHIBIOS)/os/hal/ports/AVR/XMEGA/LLD/TIMv1/driver.mk
+include $(CHIBIOS)/os/hal/ports/AVR/XMEGA/LLD/USARTv1/driver.mk
 
 # Shared variables
 ALLCSRC += $(PLATFORMSRC)
