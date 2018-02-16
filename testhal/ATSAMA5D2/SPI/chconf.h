38739d7a8b9d1a7ebe523cf52dae3181db11caa2
edolomb <none@example.com>
2018-02-16 17:09:45 +0000

    Changed CH_DBG_THREADS_PROFILING at FALSE
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@11504 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- testhal/ATSAMA5D2/SPI/chconf.h
+++ testhal/ATSAMA5D2/SPI/chconf.h
@@ -467,7 +467,7 @@
  * @note    This debug option is not currently compatible with the
  *          tickless mode.
  */
-#define CH_DBG_THREADS_PROFILING            TRUE
+#define CH_DBG_THREADS_PROFILING            FALSE
 
 /** @} */
 
