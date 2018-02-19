89821914a580bc083de6d0e2958c0d0d351819a8
Theodore Ateba <tf.ateba@gmail.com>
2018-02-19 20:49:06 +0000

    AVR: Add watchdog to the platform driver file.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@11512 35acf78f-673a-0410-8e92-d51de3d6d3f4

--- os/hal/ports/AVR/XMEGA/ATXMEGAxxxA4U/platform.mk
+++ os/hal/ports/AVR/XMEGA/ATXMEGAxxxA4U/platform.mk
@@ -8,6 +8,7 @@ PLATFORMINC = ${CHIBIOS}/os/hal/ports/AVR/XMEGA/ATXMEGAxxxA4U
 include $(CHIBIOS)/os/hal/ports/AVR/XMEGA/LLD/GPIOv1/driver.mk
 include $(CHIBIOS)/os/hal/ports/AVR/XMEGA/LLD/TIMv1/driver.mk
 include $(CHIBIOS)/os/hal/ports/AVR/XMEGA/LLD/USARTv1/driver.mk
+include $(CHIBIOS)/os/hal/ports/AVR/XMEGA/LLD/WDGv1/driver.mk
 
 # Shared variables
 ALLCSRC += $(PLATFORMSRC)
