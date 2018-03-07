991afc2af953fc91cb4def631e1522bd297dfd60
isiora <none@example.com>
2018-03-07 11:48:25 +0000

    New TSSI interface.
    git-svn-id: https://svn.code.sf.net/p/chibios/svn2/trunk@11610 110e8d01-0319-4d1e-a829-52ad28d1bb01

--- os/common/ports/ARMCAx-TZ/compilers/GCC/mk/port_generic.mk
+++ os/common/ports/ARMCAx-TZ/compilers/GCC/mk/port_generic.mk
@@ -1,6 +1,6 @@
 # List of the ChibiOS/RT ARMCAx-TZ generic port files.
 PORTSRC = ${CHIBIOS}/os/common/ports/ARMCAx-TZ/chcore.c \
-          ${CHIBIOS}/os/common/ports/ARMCAx-TZ/chsmc.c
+          ${CHIBIOS}/os/common/ports/ARMCAx-TZ/chtssi.c
 
 PORTASM = $(CHIBIOS)/os/common/ports/ARMCAx-TZ/compilers/GCC/chcoreasm.S \
           $(CHIBIOS)/os/common/ports/ARMCAx-TZ/compilers/GCC/monitor.S
