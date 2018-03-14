ac45d390638e7a54c9e6ae8b47b436fb4677780f
Giovanni Di Sirio <gdisirio@gmail.com>
2018-03-14 12:31:59 +0000

    Added tool for chconf.h automatic realignment.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@11742 110e8d01-0319-4d1e-a829-52ad28d1bb01

---
+++ tools/updater/update_chconf_rt.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ $# -eq 0 ]
+then
+  find ../.. -name "chconf.h" -exec bash update_chconf.sh "{}" \;
+elif [ $# -eq 1 ]
+then
+  if egrep -q "_CHIBIOS_RT_CONF_" $1
+  then
+    echo Processing: $1
+    cat $1 | egrep -e "\#define\s+[a-zA-Z0-9_]*\s+[a-zA-Z0-9_]" | cut --bytes=9- - | sed 's/  */=/g' > ./values.txt
+    if ! fmpp -q -C chconf_rt.fmpp
+    then
+      echo
+      echo "aborted"
+      exit 1
+    fi
+    cp ./chconf.h $1
+    rm ./chconf.h ./values.txt
+  fi
+else
+  echo "illegal number of arguments"
+fi
