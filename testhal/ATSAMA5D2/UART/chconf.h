1c764b208a856d81eb10aaa9edf2ae04d2245ea9
edolomb <none@example.com>
2018-03-19 12:23:55 +0000

    Supported tick-less mode
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@11822 110e8d01-0319-4d1e-a829-52ad28d1bb01

--- testhal/ATSAMA5D2/UART/chconf.h
+++ testhal/ATSAMA5D2/UART/chconf.h
@@ -49,7 +49,8 @@
  * @details Frequency of the system timer that drives the system ticks. This
  *          setting also defines the system tick time unit.
  */
-#define CH_CFG_ST_FREQUENCY                 10000
+//#define CH_CFG_ST_FREQUENCY                 1000          /* periodic tick.*/
+#define CH_CFG_ST_FREQUENCY                 (84000000 / 32) /* tick-less.*/
 
 /**
  * @brief   Time intervals data size.
@@ -71,7 +72,7 @@
  *          The value one is not valid, timeouts are rounded up to
  *          this value.
  */
-#define CH_CFG_ST_TIMEDELTA                 0
+#define CH_CFG_ST_TIMEDELTA                 2
 
 /** @} */
 
