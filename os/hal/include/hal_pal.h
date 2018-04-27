6471bff88b8423cc785999ed852e236b0ee56532
Giovanni Di Sirio <gdisirio@gmail.com>
2018-04-27 07:49:11 +0000

    Fixed bug #940.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@11956 110e8d01-0319-4d1e-a829-52ad28d1bb01

--- os/hal/include/hal_pal.h
+++ os/hal/include/hal_pal.h
@@ -958,9 +958,9 @@ typedef struct {
 #ifdef __cplusplus
 extern "C" {
 #endif
-  ioportmask_t palReadBus(IOBus *bus);
-  void palWriteBus(IOBus *bus, ioportmask_t bits);
-  void palSetBusMode(IOBus *bus, iomode_t mode);
+  ioportmask_t palReadBus(const IOBus *bus);
+  void palWriteBus(const IOBus *bus, ioportmask_t bits);
+  void palSetBusMode(const IOBus *bus, iomode_t mode);
 #if (PAL_USE_CALLBACKS == TRUE) || defined(__DOXYGEN__)
   void palSetPadCallbackI(ioportid_t port, iopadid_t pad,
                           palcallback_t cb, void *arg);
--- readme.txt
+++ readme.txt
@@ -115,6 +115,8 @@
 - EX:  Updated LIS302DL to 1.1.0 (backported to 18.2.1).
 - EX:  Updated LPS25H to 1.1.0 (backported to 18.2.1).
 - EX:  Updated LSM303DLHC to 1.1.0 (backported to 18.2.1).
+- HAL: Fixed IOBus PAL functions missing the const qualifier (bug #940)
+       (backported to 18.2.1 and 17.6.5).
 - HAL: Fixed STM32 USBv1 driver does not reset data toggling bits on endpoint
        initialization (bug #939)(backported to 18.2.1 and 17.6.5).
 - HAL: Fixed incorrect behavior of USB driver on SET CONFIGURATION (bug #938)
