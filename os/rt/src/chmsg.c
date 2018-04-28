fc1588e68475c356a215a49850874e9142010111
Giovanni Di Sirio <gdisirio@gmail.com>
2018-04-28 14:29:57 +0000

    Minor documentation improvement.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@11969 110e8d01-0319-4d1e-a829-52ad28d1bb01

--- os/rt/src/chmsg.c
+++ os/rt/src/chmsg.c
@@ -111,8 +111,10 @@ msg_t chMsgSend(thread_t *tp, msg_t msg) {
  * @note    If the message is a pointer then you can assume that the data
  *          pointed by the message is stable until you invoke @p chMsgRelease()
  *          because the sending thread is suspended until then.
+ * @note    The reference counter of the sender thread is not increased, the
+ *          returned pointer is a temporary reference.
  *
- * @return              A reference to the thread carrying the message.
+ * @return              A pointer to the thread carrying the message.
  *
  * @api
  */
