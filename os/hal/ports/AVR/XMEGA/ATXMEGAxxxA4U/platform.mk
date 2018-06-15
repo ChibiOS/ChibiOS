ffd1e3fe458978900235a6e9db8c16fe69fbd2cc
Theodore Ateba <tf.ateba@gmail.com>
2018-06-15 06:34:37 +0000

    AVR: ADD SPI driver to the AVR platform.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@12097 110e8d01-0319-4d1e-a829-52ad28d1bb01

--- os/hal/ports/AVR/XMEGA/ATXMEGAxxxA4U/platform.mk
+++ os/hal/ports/AVR/XMEGA/ATXMEGAxxxA4U/platform.mk
@@ -9,6 +9,7 @@ include $(CHIBIOS)/os/hal/ports/AVR/XMEGA/LLD/CRYPv1/driver.mk
 include $(CHIBIOS)/os/hal/ports/AVR/XMEGA/LLD/DACv1/driver.mk
 include $(CHIBIOS)/os/hal/ports/AVR/XMEGA/LLD/DMAv1/driver.mk
 include $(CHIBIOS)/os/hal/ports/AVR/XMEGA/LLD/GPIOv1/driver.mk
+include $(CHIBIOS)/os/hal/ports/AVR/XMEGA/LLD/SPIv1/driver.mk
 include $(CHIBIOS)/os/hal/ports/AVR/XMEGA/LLD/TIMv1/driver.mk
 include $(CHIBIOS)/os/hal/ports/AVR/XMEGA/LLD/USARTv1/driver.mk
 include $(CHIBIOS)/os/hal/ports/AVR/XMEGA/LLD/WDGv1/driver.mk
