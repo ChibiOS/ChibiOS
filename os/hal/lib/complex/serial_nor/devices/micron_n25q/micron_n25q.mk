2dd28213f179c0a6e0a1f889ee19f803df53c42f
Giovanni Di Sirio <gdisirio@gmail.com>
2018-10-03 13:19:22 +0000

    Some renaming for consistence.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@12319 110e8d01-0319-4d1e-a829-52ad28d1bb01

--- os/hal/lib/complex/serial_nor/devices/micron_n25q/micron_n25q.mk
+++ os/hal/lib/complex/serial_nor/devices/micron_n25q/micron_n25q.mk
@@ -1,8 +1,8 @@
 # List of all the Micron N25Q device files.
 M25QSRC := $(CHIBIOS)/os/hal/lib/peripherals/flash/hal_flash.c \
            $(CHIBIOS)/os/hal/lib/peripherals/flash/hal_jesd216_flash.c \
-           $(CHIBIOS)/os/hal/lib/complex/m25q/m25q.c \
-           $(CHIBIOS)/os/hal/lib/complex/m25q/devices/micron_n25q/m25q_flash.c
+           $(CHIBIOS)/os/hal/lib/complex/m25q/serial_nor.c \
+           $(CHIBIOS)/os/hal/lib/complex/m25q/devices/micron_n25q/micron_n25q.c
 
 # Required include directories
 M25QINC := $(CHIBIOS)/os/hal/lib/peripherals/flash \
