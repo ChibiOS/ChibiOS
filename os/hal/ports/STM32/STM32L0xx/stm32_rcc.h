351bce60dfd299600cbd6f7ba8035614bf5ea072
Giovanni Di Sirio <gdisirio@gmail.com>
2018-10-13 18:40:54 +0000

    Fixed bugs #980 and #981.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_18.2.x@12358 110e8d01-0319-4d1e-a829-52ad28d1bb01

--- os/hal/ports/STM32/STM32L0xx/stm32_rcc.h
+++ os/hal/ports/STM32/STM32L0xx/stm32_rcc.h
@@ -489,6 +489,29 @@
 #define rccResetTIM2() rccResetAPB1(RCC_APB1RSTR_TIM2RST)
 
 /**
+ * @brief   Enables the TIM3 peripheral clock.
+ *
+ * @param[in] lp        low power enable flag
+ *
+ * @api
+ */
+#define rccEnableTIM3(lp) rccEnableAPB1(RCC_APB1ENR_TIM3EN, lp)
+
+/**
+ * @brief   Disables the TIM3 peripheral clock.
+ *
+ * @api
+ */
+#define rccDisableTIM3() rccDisableAPB1(RCC_APB1ENR_TIM3EN)
+
+/**
+ * @brief   Resets the TIM3 peripheral.
+ *
+ * @api
+ */
+#define rccResetTIM3() rccResetAPB1(RCC_APB1RSTR_TIM3RST)
+
+/**
  * @brief   Enables the TIM6 peripheral clock.
  *
  * @param[in] lp        low power enable flag
