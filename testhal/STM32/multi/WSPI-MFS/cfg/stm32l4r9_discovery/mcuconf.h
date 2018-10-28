18a8116ad45784a710a7129930e9d0b892ef1df3
gdisirio <gdisirio@110e8d01-0319-4d1e-a829-52ad28d1bb01>
2018-10-28 17:00:24 +0000

    Updated PLLSAIx settings.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@12398 110e8d01-0319-4d1e-a829-52ad28d1bb01

--- testhal/STM32/multi/WSPI-MFS/cfg/stm32l4r9_discovery/mcuconf.h
+++ testhal/STM32/multi/WSPI-MFS/cfg/stm32l4r9_discovery/mcuconf.h
@@ -69,13 +69,13 @@
 #define STM32_MCOSEL                        STM32_MCOSEL_NOCLOCK
 #define STM32_MCOPRE                        STM32_MCOPRE_DIV1
 #define STM32_LSCOSEL                       STM32_LSCOSEL_NOCLOCK
-#define STM32_PLLSAI1M_VALUE                1
+#define STM32_PLLSAI1M_VALUE                4
 #define STM32_PLLSAI1N_VALUE                72
 #define STM32_PLLSAI1PDIV_VALUE             6
 #define STM32_PLLSAI1P_VALUE                7
 #define STM32_PLLSAI1Q_VALUE                6
 #define STM32_PLLSAI1R_VALUE                6
-#define STM32_PLLSAI2M_VALUE                1
+#define STM32_PLLSAI2M_VALUE                4
 #define STM32_PLLSAI2N_VALUE                72
 #define STM32_PLLSAI2PDIV_VALUE             6
 #define STM32_PLLSAI2P_VALUE                7
