c14f479548ba23f4316ca59243d1cd9be5aa9b2a
Giovanni Di Sirio <gdisirio@gmail.com>
2019-01-20 15:23:50 +0000

    Updated version numbers.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@12565 110e8d01-0319-4d1e-a829-52ad28d1bb01

--- os/nil/include/ch.h
+++ os/nil/include/ch.h
@@ -55,7 +55,7 @@
 /**
  * @brief   Kernel version string.
  */
-#define CH_KERNEL_VERSION       "3.1.0"
+#define CH_KERNEL_VERSION       "3.2.0"
 
 /**
  * @brief   Kernel version major number.
@@ -65,7 +65,7 @@
 /**
  * @brief   Kernel version minor number.
  */
-#define CH_KERNEL_MINOR         1
+#define CH_KERNEL_MINOR         2
 
 /**
  * @brief   Kernel version patch number.
