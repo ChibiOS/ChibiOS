4852a58556e65b76e428f85074d264d681f87f57
Rocco Marco Guglielmi <roccomarco.guglielmi@gmail.com>
2019-01-27 12:26:37 +0000

    Updated testes/STM32/STM32L4xx demos to be compliant with new project organization/makefile mechanisms.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_19.1.x@12613 110e8d01-0319-4d1e-a829-52ad28d1bb01

--- testex/STM32/STM32L4xx/SPI-L3GD20/cfg/chconf.h
+++ testex/STM32/STM32L4xx/SPI-L3GD20/cfg/chconf.h
@@ -491,7 +491,7 @@
  * @note    The default is @p FALSE.
  */
 #if !defined(CH_DBG_SYSTEM_STATE_CHECK)
-#define CH_DBG_SYSTEM_STATE_CHECK           TRUE
+#define CH_DBG_SYSTEM_STATE_CHECK           FALSE
 #endif
 
 /**
@@ -502,7 +502,7 @@
  * @note    The default is @p FALSE.
  */
 #if !defined(CH_DBG_ENABLE_CHECKS)
-#define CH_DBG_ENABLE_CHECKS                TRUE
+#define CH_DBG_ENABLE_CHECKS                FALSE
 #endif
 
 /**
@@ -514,7 +514,7 @@
  * @note    The default is @p FALSE.
  */
 #if !defined(CH_DBG_ENABLE_ASSERTS)
-#define CH_DBG_ENABLE_ASSERTS               TRUE
+#define CH_DBG_ENABLE_ASSERTS               FALSE
 #endif
 
 /**
