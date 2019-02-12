e6e06730c0643bfa7f6f56e4f22808addf592745
Giovanni Di Sirio <gdisirio@gmail.com>
2019-02-12 12:51:12 +0000

    Fixed more e200z demos.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@12659 110e8d01-0319-4d1e-a829-52ad28d1bb01

--- os/hal/ports/SPC5/SPC560Bxx/platform.mk
+++ os/hal/ports/SPC5/SPC560Bxx/platform.mk
@@ -1,18 +1,18 @@
 # List of all the SPC560Bxx platform files.
 PLATFORMSRC = ${CHIBIOS}/os/hal/ports/SPC5/SPC560Bxx/hal_lld.c \
-              ${CHIBIOS}/os/hal/ports/SPC5/SPC5xx/EDMA_v1/spc5_edma.c \
-              ${CHIBIOS}/os/hal/ports/SPC5/SPC5xx/SIUL_v1/hal_pal_lld.c \
-              ${CHIBIOS}/os/hal/ports/SPC5/SPC5xx/LINFlex_v1/hal_serial_lld.c \
-              ${CHIBIOS}/os/hal/ports/SPC5/SPC5xx/DSPI_v1/hal_spi_lld.c \
-              ${CHIBIOS}/os/hal/ports/SPC5/SPC5xx/STM_v1/hal_st_lld.c
+              ${CHIBIOS}/os/hal/ports/SPC5/LLD/EDMA_v1/spc5_edma.c \
+              ${CHIBIOS}/os/hal/ports/SPC5/LLD/SIUL_v1/hal_pal_lld.c \
+              ${CHIBIOS}/os/hal/ports/SPC5/LLD/LINFlex_v1/hal_serial_lld.c \
+              ${CHIBIOS}/os/hal/ports/SPC5/LLD/DSPI_v1/hal_spi_lld.c \
+              ${CHIBIOS}/os/hal/ports/SPC5/LLD/STM_v1/hal_st_lld.c
 
 # Required include directories
 PLATFORMINC = ${CHIBIOS}/os/hal/ports/SPC5/SPC560Bxx \
-              ${CHIBIOS}/os/hal/ports/SPC5/SPC5xx/EDMA_v1 \
-              ${CHIBIOS}/os/hal/ports/SPC5/SPC5xx/SIUL_v1 \
-              ${CHIBIOS}/os/hal/ports/SPC5/SPC5xx/LINFlex_v1 \
-              ${CHIBIOS}/os/hal/ports/SPC5/SPC5xx/DSPI_v1 \
-              ${CHIBIOS}/os/hal/ports/SPC5/SPC5xx/STM_v1
+              ${CHIBIOS}/os/hal/ports/SPC5/LLD/EDMA_v1 \
+              ${CHIBIOS}/os/hal/ports/SPC5/LLD/SIUL_v1 \
+              ${CHIBIOS}/os/hal/ports/SPC5/LLD/LINFlex_v1 \
+              ${CHIBIOS}/os/hal/ports/SPC5/LLD/DSPI_v1 \
+              ${CHIBIOS}/os/hal/ports/SPC5/LLD/STM_v1
 
 # Shared variables
 ALLCSRC += $(PLATFORMSRC)
