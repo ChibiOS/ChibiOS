5266dadbb5884382f2afd81e29dddadb80eff44a
Giovanni Di Sirio <gdisirio@gmail.com>
2019-06-30 07:38:21 +0000

    Fixed bug #1036.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_18.2.x@12857 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/hal/ports/STM32/STM32L4xx/stm32_rcc.h
+++ os/hal/ports/STM32/STM32L4xx/stm32_rcc.h
@@ -567,6 +567,29 @@
  * @api
  */
 #define rccResetI2C3() rccResetAPB1R1(RCC_APB1RSTR1_I2C3RST)
+
+/**
+ * @brief Enables the I2C4 peripheral clock.
+ *
+ * @param[in] lp low power enable flag
+ *
+ * @api
+ */
+#define rccEnableI2C4(lp) rccEnableAPB1R2(RCC_APB1ENR2_I2C4EN, lp)
+
+/**
+ * @brief Disables the I2C4 peripheral clock.
+ *
+ * @api
+ */
+#define rccDisableI2C4() rccDisableAPB1R1(RCC_APB1ENR2_I2C4EN)
+
+/**
+ * @brief Resets the I2C4 peripheral.
+ *
+ * @api
+ */
+#define rccResetI2C4() rccResetAPB1R1(RCC_APB1RSTR2_I2C4RST)
 /** @} */
 
 /**
