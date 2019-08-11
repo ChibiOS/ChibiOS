20742714c0099084e5d63a7c6d47020bde2e81e7
Giovanni Di Sirio <gdisirio@gmail.com>
2019-08-11 12:13:52 +0000

    Removed old OSAL implementations.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@12949 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/hal/osal/nil/osal.mk
+++
@@ -1,9 +0,0 @@
-# OSAL files.
-OSALSRC += ${CHIBIOS}/os/hal/osal/nil/osal.c
-
-# Required include directories
-OSALINC += ${CHIBIOS}/os/hal/osal/nil
-
-# Shared variables
-ALLCSRC += $(OSALSRC)
-ALLINC  += $(OSALINC)
