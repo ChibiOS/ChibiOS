535db1f13bdc2215211bcb9983f16b64137d60d6
Giovanni Di Sirio <gdisirio@gmail.com>
2019-09-29 14:00:36 +0000

    Small fix to the G0 mcuconf generator.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@13048 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- demos/STM32/RT-STM32G071RB-NUCLEO64/cfg/mcuconf.h
+++ demos/STM32/RT-STM32G071RB-NUCLEO64/cfg/mcuconf.h
@@ -15,7 +15,7 @@
 */
 
 /*
- * STM32L4xx drivers configuration.
+ * STM32G0xx drivers configuration.
  * The following settings override the default settings present in
  * the various device driver implementation headers.
  * Note that the settings for each driver only have effect if the whole
