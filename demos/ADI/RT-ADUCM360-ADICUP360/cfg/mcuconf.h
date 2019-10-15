10fe5a6e9e425e79adff7fb18558b3b0e50f96da
Rocco Marco Guglielmi <roccomarco.guglielmi@live.com>
2019-10-15 13:26:09 +0000

    Added a base demo for ADICUP360 relying on PAL driver and ST only (still incomplete)
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@13100 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

---
+++ demos/ADI/RT-ADUCM360-ADICUP360/cfg/mcuconf.h
@@ -0,0 +1,62 @@
+/*
+    ChibiOS - Copyright (C) 2006..2018 Giovanni Di Sirio
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef MCUCONF_H
+#define MCUCONF_H
+
+/*
+ * ADUCM36x drivers configuration.
+ * The following settings override the default settings present in
+ * the various device driver implementation headers.
+ * Note that the settings for each driver only have effect if the whole
+ * driver is enabled in halconf.h.
+ *
+ * IRQ priorities:
+ * 3...0       Lowest...Highest.
+ *
+ * DMA priorities:
+ * 0...3        Lowest...Highest.
+ */
+
+#define ADUCM36x_MCUCONF
+
+/*
+ * HAL driver system settings.
+ */
+#define ADUCM_NO_INIT                       FALSE
+#define ADUCM_XOSC_ENABLED                  FALSE
+#define ADUCM_CLKMUX                        ADUCM_CLKMUX_HFOSC
+#define ADUCM_HFOSC_PREDIV                  ADUCM_HFOSC_DIV1
+#define ADUCM_XOSC_PREDIV                   ADUCM_XOSC_DIV1
+#define ADUCM_CD_DIV                        ADUCM_CD_DIV1
+#define ADUCM_SPI0CD_DIV                    ADUCM_SPI0CD_DIV1
+#define ADUCM_SPI1CD_DIV                    ADUCM_SPI1CD_DIV1
+#define ADUCM_I2CCD_DIV                     ADUCM_I2CCD_DIV1
+#define ADUCM_UARTCD_DIV                    ADUCM_UARTCD_DIV1
+#define ADUCM_PWMCD_DIV                     ADUCM_PWMCD_DIV1
+
+/*
+ * IRQ system settings.
+ */
+#define ADUCM_IRQ_UART1_PRIORITY            3
+
+/*
+ * SERIAL driver system settings.
+ */
+#define STM32_SERIAL_USE_UART1              TRUE
+
+/** @} */
+#endif /* MCUCONF_H */
---
+++ demos/ADI/RT-ADUCM360-ADICUP360/readme.txt
@@ -0,0 +1,27 @@
+*****************************************************************************
+** ChibiOS/RT port for ARM-Cortex-M4 ADICUP360.                                      **
+*****************************************************************************
+
+** TARGET **
+
+The demo runs on an ADICUP360 board.
+
+** The Demo **
+
+The demo flashes the two on-board LEDs using a thread.
+
+** Build Procedure **
+
+The demo has been tested by using the free Codesourcery GCC-based toolchain
+and YAGARTO.
+Just modify the TRGT line in the makefile in order to use different GCC ports.
+
+** Notes **
+
+Some files used by the demo are not part of ChibiOS/RT but are copyright of
+Analog Devices and are licensed under a different license.
+Also note that not all the files present within the Analog Devices 
+Cross Core Embedded Studio are distributed with ChibiOS/RT, 
+you can find the whole library on the ADI web site:
+
+                             http://www.analog.com
