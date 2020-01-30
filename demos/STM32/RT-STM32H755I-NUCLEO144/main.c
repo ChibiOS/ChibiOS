5490bcb764bbdd1624da4a463f866cf49eb14865
Giovanni Di Sirio <gdisirio@gmail.com>
2020-01-30 14:49:06 +0000

    Updated board files.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@13313 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- demos/STM32/RT-STM32H755I-NUCLEO144/main.c
+++ demos/STM32/RT-STM32H755I-NUCLEO144/main.c
@@ -29,10 +29,18 @@ static THD_FUNCTION(Thread1, arg) {
   (void)arg;
   chRegSetThreadName("blinker");
   while (true) {
-    palSetLine(LINE_LED1);
-    chThdSleepMilliseconds(500);
-    palClearLine(LINE_LED1);
-    chThdSleepMilliseconds(500);
+    palSetLine(LINE_LD1);
+    chThdSleepMilliseconds(50);
+    palSetLine(LINE_LD2);
+    chThdSleepMilliseconds(50);
+    palSetLine(LINE_LD3);
+    chThdSleepMilliseconds(200);
+    palClearLine(LINE_LD1);
+    chThdSleepMilliseconds(50);
+    palClearLine(LINE_LD2);
+    chThdSleepMilliseconds(50);
+    palClearLine(LINE_LD3);
+    chThdSleepMilliseconds(200);
   }
 }
 
