76dff26f8965dc7deaa4feedf6cb5dbac7183d47
kalyan_sreenivas <r.kalyan.sreenivas@gmail.com>
2020-02-10 20:46:09 +0100

    Fixed syscalls.cpp

--- os/various/cpp_wrappers/syscalls_cpp.cpp
+++ os/various/cpp_wrappers/syscalls_cpp.cpp
@@ -9,18 +9,21 @@
 extern "C" {
 #endif
 
+__attribute__((used))
 void _exit(int status){
    (void) status;
    osalSysHalt("Unrealized");
    while(TRUE){}
 }
 
+__attribute__((used))
 pid_t _getpid(void){
    return 1;
 }
 
 #undef errno
 extern int errno;
+__attribute__((used))
 int _kill(int pid, int sig) {
   (void)pid;
   (void)sig;
@@ -28,6 +31,7 @@ int _kill(int pid, int sig) {
   return -1;
 }
 
+__attribute__((used))
 void _open_r(void){
   return;
 }
