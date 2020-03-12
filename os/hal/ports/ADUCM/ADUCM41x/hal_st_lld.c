71348f96f8c013d96260fae8208267b802dc1bfd
Rocco Marco Guglielmi <roccomarco.guglielmi@live.com>
2020-03-12 14:07:35 +0000

    Fixed issue with the Systick handler for the ADUC41x
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@13413 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/hal/ports/ADUCM/ADUCM41x/hal_st_lld.c
+++ os/hal/ports/ADUCM/ADUCM41x/hal_st_lld.c
@@ -104,7 +104,7 @@ void st_lld_init(void) {
                   SysTick_CTRL_TICKINT_Msk;
 
   /* IRQ enabled.*/
-  nvicSetSystemHandlerPriority(HANDLER_SYSTICK, ADUCM_ST_IRQ_PRIORITY);
+  nvicEnableVector(HANDLER_SYSTICK, ADUCM_ST_IRQ_PRIORITY);
 #endif /* OSAL_ST_MODE == OSAL_ST_MODE_PERIODIC */
 }
 
