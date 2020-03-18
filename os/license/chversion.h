20bc46d0c5cf9a6ae6d87cb4ddbf2a9a8dd696f8
Giovanni Di Sirio <gdisirio@gmail.com>
2020-03-18 07:29:08 +0000

    Version number updated, nickname Erchie.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_20.3.x@13446 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/license/chversion.h
+++ os/license/chversion.h
@@ -42,7 +42,7 @@
 /**
  * @brief   Stable release flag.
  */
-#define CH_VERSION_STABLE       0
+#define CH_VERSION_STABLE       1
 
 /**
  * @name    ChibiOS version identification
@@ -51,17 +51,17 @@
 /**
  * @brief   ChibiOS version string.
  */
-#define CH_VERSION              "0.0.0"
+#define CH_VERSION              "20.3.0"
 
 /**
  * @brief   ChibiOS version release year.
  */
-#define CH_VERSION_YEAR         0
+#define CH_VERSION_YEAR         20
 
 /**
  * @brief   ChibiOS version release month.
  */
-#define CH_VERSION_MONTH        0
+#define CH_VERSION_MONTH        3
 
 /**
  * @brief   ChibiOS version patch number.
@@ -71,7 +71,7 @@
 /**
  * @brief   ChibiOS version nickname.
  */
-#define CH_VERSION_NICKNAME     "Lasagna"
+#define CH_VERSION_NICKNAME     "Erchie"
 /** @} */
 
 /*===========================================================================*/
