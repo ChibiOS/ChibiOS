7e31873495aa9417d30f4fffcb4a9cc32c3c2d2e
Giovanni Di Sirio <gdisirio@gmail.com>
2020-04-26 13:55:32 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_19.1.x@13581 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/hal/ports/SAMA/LLD/SDMMCv1/ch_sdmmc_mmc.h
+++
@@ -1,8 +0,0 @@
-#ifndef CH_SDMMC_MMC_H_
-#define CH_SDMMC_MMC_H_
-
-extern uint8_t MmcInit(SdmmcDriver *driver);
-extern uint8_t MmcGetExtInformation(SdmmcDriver *driver);
-
-
-#endif /* CH_SDMMC_MMC_H_ */
