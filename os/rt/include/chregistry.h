ab88fc798a33372587f3ee51f9b9bd4f742174ce
Giovanni Di Sirio <gdisirio@gmail.com>
2020-06-07 18:38:55 +0000

    Doc and white space fixes.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_20.3.x@13695 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/rt/include/chregistry.h
+++ os/rt/include/chregistry.h
@@ -93,9 +93,9 @@ typedef struct {
  */
 #define REG_INSERT(tp) {                                                    \
   (tp)->newer = (thread_t *)&ch.rlist;                                      \
-  (tp)->older = ch.rlist.older;                                           \
+  (tp)->older = ch.rlist.older;                                             \
   (tp)->older->newer = (tp);                                                \
-  ch.rlist.older = (tp);                                                  \
+  ch.rlist.older = (tp);                                                    \
 }
 
 /*===========================================================================*/
