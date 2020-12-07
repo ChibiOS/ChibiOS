8529def1ba92d1213c75699486bb33379c7f28f7
Giovanni Di Sirio <gdisirio@gmail.com>
2020-12-07 09:00:12 +0000

    Copyright cleanup.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@13949 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/hal/ports/STM32/STM32WBxx/hal_lld.c
+++ os/hal/ports/STM32/STM32WBxx/hal_lld.c
@@ -1,5 +1,5 @@
 /*
-    ChibiOS - Copyright (C) 2006..2019 Ilya Kharin
+    ChibiOS - Copyright (C) 2006..2020 Giovanni Di Sirio
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -12,6 +12,9 @@
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
     limitations under the License.
+*/
+/*
+    Concepts and parts of this file have been contributed by Ilya Kharin.
 */
 
 /**
