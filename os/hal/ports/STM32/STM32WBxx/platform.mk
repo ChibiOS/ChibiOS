4b3dde8019f3b1708abac38e9dcc1aeae5f88efa
cinsights <none@example.com>
2020-12-22 12:42:04 +0000

    Change STM32WB to gpio2 in board cfg and platform.mk
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@13975 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/hal/ports/STM32/STM32WBxx/platform.mk
+++ os/hal/ports/STM32/STM32WBxx/platform.mk
@@ -30,7 +30,7 @@ include $(CHIBIOS)/os/hal/ports/STM32/LLD/ADCv3/driver.mk
 include $(CHIBIOS)/os/hal/ports/STM32/LLD/CRYPv1/driver.mk
 include $(CHIBIOS)/os/hal/ports/STM32/LLD/DMAv1/driver.mk
 include $(CHIBIOS)/os/hal/ports/STM32/LLD/EXTIv1/driver.mk
-include $(CHIBIOS)/os/hal/ports/STM32/LLD/GPIOv3/driver.mk
+include $(CHIBIOS)/os/hal/ports/STM32/LLD/GPIOv2/driver.mk
 include $(CHIBIOS)/os/hal/ports/STM32/LLD/I2Cv2/driver.mk
 include $(CHIBIOS)/os/hal/ports/STM32/LLD/QUADSPIv1/driver.mk
 include $(CHIBIOS)/os/hal/ports/STM32/LLD/RNGv1/driver.mk
