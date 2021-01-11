8e5cc9a203501543d2c199ac7c94abf8a0df1364
cinsights <none@example.com>
2021-01-11 12:14:32 +0000

    Add time conversion macros and functions for monotonic time stamps
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@14016 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/rt/include/chtime.h
+++ os/rt/include/chtime.h
@@ -274,6 +274,64 @@ typedef uint32_t time_conv_t;
   (time_msecs_t)((((time_conv_t)(interval) * (time_conv_t)1000000) +        \
                   (time_conv_t)CH_CFG_ST_FREQUENCY - (time_conv_t)1) /      \
                  (time_conv_t)CH_CFG_ST_FREQUENCY)
+
+/**
+ * @brief   Time stamp interval to seconds.
+ * @details Converts from time stamp ticks number to seconds.
+ * @note    The result is rounded down to the second boundary.
+ * @note    Use of this macro for large values is not secure because of
+ *          integer overflows. Make sure your value can be correctly
+ *          converted.
+ *
+ * @param[in] interval  time stamp in ticks
+ *
+ * @return              The number of seconds.
+ *
+ * @api
+ */
+#define TIME_TS2S(timestamp)                                                \
+  (time_secs_t)(((systimestamp_t)(timestamp) /                              \
+      (systimestamp_t)CH_CFG_ST_FREQUENCY))
+
+/**
+ * @brief   Time stamp interval to milliseconds.
+ * @details Converts from time stamp ticks number to milliseconds.
+ * @note    The result is rounded down to the millisecond boundary.
+ * @note    Use of this macro for large values is not secure because of
+ *          integer overflows. Make sure your value can be correctly
+ *          converted.
+ *
+ * @param[in] interval  time stamp in ticks
+ *
+ * @return              The number of milliseconds.
+ *
+ * @api
+ */
+#define TIME_TS2MS(interval)                                                \
+  (time_msecs_t)((((systimestamp_t)(interval) %                             \
+      (systimestamp_t)(CH_CFG_ST_FREQUENCY)) /                              \
+      (systimestamp_t)((systimestamp_t)CH_CFG_ST_FREQUENCY /                \
+        (systimestamp_t)1000)) % 1000)
+
+/**
+ * @brief   Time stamp interval to microseconds.
+ * @details Converts from time stamp ticks number to microseconds.
+ * @note    The result is rounded down to the microsecond boundary.
+ * @note    Use of this macro for large values is not secure because of
+ *          integer overflows. Make sure your value can be correctly
+ *          converted.
+ *
+ * @param[in] interval  time stamp in ticks
+ *
+ * @return              The number of microseconds.
+ *
+ * @api
+ */
+#define TIME_TS2US(interval)                                                \
+  (time_usecs_t)((((systimestamp_t)(interval) %                             \
+      (systimestamp_t)(CH_CFG_ST_FREQUENCY)) /                              \
+      (systimestamp_t)((systimestamp_t)CH_CFG_ST_FREQUENCY /                \
+      (systimestamp_t)1000000)) % 1000000)
 /** @} */
 
 /*===========================================================================*/
@@ -434,6 +492,75 @@ static inline time_usecs_t chTimeI2US(sysinterval_t interval) {
 }
 
 /**
+ * @brief   Time stamp interval to seconds.
+ * @details Converts from time stamp interval to seconds.
+ * @note    The result is rounded up to the next second boundary.
+ *
+ * @param[in] interval  interval in ticks
+ * @return              The number of seconds.
+ *
+ * @special
+ */
+static inline time_secs_t chTimeTS2S(systimestamp_t interval) {
+  systimestamp_t secs;
+
+  secs = ((interval +
+         (systimestamp_t)CH_CFG_ST_FREQUENCY - (systimestamp_t)1) /
+         (systimestamp_t)CH_CFG_ST_FREQUENCY);
+
+  chDbgAssert(secs < (systimestamp_t)((time_secs_t)-1),
+              "conversion overflow");
+
+  return (time_secs_t)secs;
+}
+
+/**
+ * @brief   Time stamp interval to milliseconds.
+ * @details Converts from time stamp interval to milliseconds.
+ * @note    The result is rounded up to the next millisecond boundary.
+ *
+ * @param[in] interval  interval in ticks
+ * @return              The number of milliseconds.
+ *
+ * @special
+ */
+static inline time_msecs_t chTimeTS2MS(systimestamp_t interval) {
+  systimestamp_t msecs;
+
+  msecs = ((interval * (systimestamp_t)1000) +
+           (systimestamp_t)CH_CFG_ST_FREQUENCY - (systimestamp_t)1) /
+          (systimestamp_t)CH_CFG_ST_FREQUENCY;
+
+  chDbgAssert(msecs < (systimestamp_t)((time_msecs_t)-1),
+              "conversion overflow");
+
+  return (time_msecs_t)msecs;
+}
+
+/**
+ * @brief   Time stamp interval to microseconds.
+ * @details Converts from time stamp interval to microseconds.
+ * @note    The result is rounded up to the next microsecond boundary.
+ *
+ * @param[in] interval  interval in ticks
+ * @return              The number of microseconds.
+ *
+ * @special
+ */
+static inline time_usecs_t chTimeTS2US(systimestamp_t interval) {
+  systimestamp_t usecs;
+
+  usecs = ((interval * (systimestamp_t)1000000) +
+           (systimestamp_t)CH_CFG_ST_FREQUENCY - (systimestamp_t)1) /
+          (systimestamp_t)CH_CFG_ST_FREQUENCY;
+
+  chDbgAssert(usecs < (systimestamp_t)((time_usecs_t)-1),
+              "conversion overflow");
+
+  return (time_usecs_t)usecs;
+}
+
+/**
  * @brief   Adds an interval to a system time returning a system time.
  *
  * @param[in] systime   base system time
@@ -488,6 +615,23 @@ static inline bool chTimeIsInRangeX(systime_t time,
 
   return (bool)((systime_t)((systime_t)time - (systime_t)start) <
                 (systime_t)((systime_t)end - (systime_t)start));
+}
+
+/**
+ * @brief   Time stamp millisecond count in current second.
+ * @details Gets millisecond count of current second.
+ * @note    The result is rounded up to the next millisecond boundary.
+ *
+ * @param[in] interval  interval in time stamp ticks
+ * @return              The count of milliseconds.
+ *
+ * @special
+ */
+static inline time_msecs_t chTSSecMS(systimestamp_t interval) {
+
+  return (time_msecs_t)(interval /
+         ((systimestamp_t)CH_CFG_ST_FREQUENCY / (systimestamp_t)1000)
+          % (time_msecs_t)1000);
 }
 
 /** @} */
