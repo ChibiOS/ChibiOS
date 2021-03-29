328bb9c1f5fd6a4be6b5368a778e452a09ef43fd
Giovanni Di Sirio <gdisirio@gmail.com>
2021-03-29 15:26:36 +0000

    License update.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_20.3.x@14106 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/oslib/include/chobjcaches.h
+++ os/oslib/include/chobjcaches.h
@@ -1,12 +1,12 @@
 /*
-    ChibiOS - Copyright (C) 2006..2019 Giovanni Di Sirio.
+    ChibiOS - Copyright (C) 2006,2007,2008,2009,2010,2011,2012,2013,2014,
+              2015,2016,2017,2018,2019,2020,2021 Giovanni Di Sirio.
 
     This file is part of ChibiOS.
 
     ChibiOS is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 3 of the License, or
-    (at your option) any later version.
+    the Free Software Foundation version 3 of the License.
 
     ChibiOS is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
