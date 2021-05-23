d6757a81eb051bd02c2c1ed1349b9ba34f641780
Giovanni Di Sirio <gdisirio@gmail.com>
2021-05-23 09:47:06 +0000

    Mass update.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@14420 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- testhal/STM32/multi/RTC/cfg/stm32g071_nucleo64/mcuconf.h
+++ testhal/STM32/multi/RTC/cfg/stm32g071_nucleo64/mcuconf.h
@@ -39,8 +39,11 @@
  * HAL driver system settings.
  */
 #define STM32_NO_INIT                       FALSE
+#define STM32_CLOCK_DYNAMIC                 FALSE
 #define STM32_VOS                           STM32_VOS_RANGE1
 #define STM32_PWR_CR2                       (STM32_PVDRT_LEV0 | STM32_PVDFT_LEV0 | STM32_PVDE_DISABLED)
+#define STM32_PWR_CR3                       (PWR_CR3_EIWUL)
+#define STM32_PWR_CR4                       (0U)
 #define STM32_HSIDIV_VALUE                  1
 #define STM32_HSI16_ENABLED                 TRUE
 #define STM32_HSE_ENABLED                   FALSE
