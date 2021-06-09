09ed00f52b75fa9ab33db6d8b0abf263b7e95dc4
Giovanni Di Sirio <gdisirio@gmail.com>
2021-06-09 09:46:21 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@14520 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

---
+++ test/core_benchmarks/corebmk_test.mk
@@ -0,0 +1,6 @@
+# List of all the core benchmarks test files.
+TESTSRC += ${CHIBIOS}/test/core_benchmarks/source/test/corebmk_test_root.c \
+           ${CHIBIOS}/test/core_benchmarks/source/test/corebmk_test_sequence_001.c
+
+# Required include directories
+TESTINC += ${CHIBIOS}/test/test/source/test
