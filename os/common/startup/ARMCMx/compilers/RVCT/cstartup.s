8eb2605294653292d8e6f4a5223c92d2d67528a0
Giovanni Di Sirio <gdisirio@gmail.com>
2021-07-08 08:08:30 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@14589 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/common/startup/ARMCMx/compilers/RVCT/cstartup.s
+++ os/common/startup/ARMCMx/compilers/RVCT/cstartup.s
@@ -84,7 +84,7 @@ Reset_Handler   PROC
                 isb
                 bl      __early_init
 
-                IF      {CPU} = "Cortex-M4.fp.sp"
+                IF      {TARGET_FPU_SOFTVFP} == {FALSE}
                 LDR     R0, =0xE000ED88           ; Enable CP10,CP11
                 LDR     R1, [R0]
                 ORR     R1, R1, #(0xF << 20)
