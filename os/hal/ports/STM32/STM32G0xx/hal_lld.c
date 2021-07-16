7474921bb26012f5be51d509af7a1e2fd896e8dd
Giovanni Di Sirio <gdisirio@gmail.com>
2021-07-16 04:22:42 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@14603 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/hal/ports/STM32/STM32G0xx/hal_lld.c
+++ os/hal/ports/STM32/STM32G0xx/hal_lld.c
@@ -643,7 +643,7 @@ void stm32_clock_init(void) {
   rccEnableAPBR2(RCC_APBENR2_SYSCFGEN, false);
 
 #if defined(HAL_USE_RTC) && defined(RCC_APBENR1_RTCAPBEN)
-  rccEnableAPBR1(RCC_APBENR1_RTCAPBEN, false);
+  rccEnableAPBR1(RCC_APBENR1_RTCAPBEN, true);
 #endif
 
   /* Static PWR configurations.*/
