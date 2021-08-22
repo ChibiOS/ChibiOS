db41e75526f7bac04aec30ce192b6192ca01c5a5
Theodore Ateba <tf.ateba@gmail.com>
2021-08-22 01:26:33 +0000

    Update ADC low level driver header file.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@14693 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/hal/ports/AVR/MEGA/LLD/ADCv1/hal_adc_lld.h
+++ os/hal/ports/AVR/MEGA/LLD/ADCv1/hal_adc_lld.h
@@ -1,5 +1,5 @@
 /*
-    ChibiOS - Copyright (C) 2006..2018 Giovanni Di Sirio
+    ChibiOS - Copyright (C) 2006..2021 Giovanni Di Sirio
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -63,113 +63,31 @@ typedef uint16_t adcsample_t;
 typedef uint16_t adc_channels_num_t;
 
 /**
- * @brief   Type of a structure representing an ADC driver.
+ * @brief   Type of an ADC error mask.
  */
-typedef struct ADCDriver ADCDriver;
-
-/**
- * @brief   ADC notification callback type.
- *
- * @param[in] adcp      pointer to the @p ADCDriver object triggering the
- *                      callback
- * @param[in] buffer    pointer to the most recent samples data
- * @param[in] n         number of buffer rows available starting from @p buffer
- */
-typedef void (*adccallback_t)(ADCDriver *adcp, adcsample_t *buffer, size_t n);
-
-/**
- * @brief   Conversion group configuration structure.
- * @details This implementation-dependent structure describes a conversion
- *          operation.
- * @note    Implementations may extend this structure to contain more,
- *          architecture dependent, fields.
- */
-typedef struct {
-  /**
-   * @brief Enables the circular buffer mode for the group.
-   */
-  bool_t                    circular;
-  /**
-   * @brief Number of the analog channels belonging to the conversion group.
-   */
-  adc_channels_num_t        num_channels;
-  /**
-   * @brief Callback function associated to the group or @p NULL.
-   */
-  adccallback_t             end_cb;
-  /* End of the mandatory fields.*/
-
-  uint8_t                   channelsMask;
-
-} ADCConversionGroup;
-
-/**
- * @brief   Driver configuration structure.
- * @note    Implementations may extend this structure to contain more,
- *          architecture dependent, fields.
- * @note    It could be empty on some architectures.
- */
-typedef struct {
-
-  uint8_t analog_reference;
-
-} ADCConfig;
-
-/**
- * @brief   Structure representing an ADC driver.
- * @note    Implementations may extend this structure to contain more,
- *          architecture dependent, fields.
- */
-struct ADCDriver {
-  /**
-   * @brief Driver state.
-   */
-  adcstate_t                state;
-  /**
-   * @brief Current configuration data.
-   */
-  const ADCConfig           *config;
-  /**
-   * @brief Current samples buffer pointer or @p NULL.
-   */
-  adcsample_t               *samples;
-  /**
-   * @brief Current samples buffer depth or @p 0.
-   */
-  size_t                    depth;
-  /**
-   * @brief Current conversion group pointer or @p NULL.
-   */
-  const ADCConversionGroup  *grpp;
-#if ADC_USE_WAIT || defined(__DOXYGEN__)
-  /**
-   * @brief Waiting thread.
-   */
-  thread_reference_t        thread;
-#endif /* ADC_USE_WAIT */
-#if ADC_USE_MUTUAL_EXCLUSION || defined(__DOXYGEN__)
-#if CH_CFG_USE_MUTEXES || defined(__DOXYGEN__)
-  /**
-   * @brief Mutex protecting the peripheral.
-   */
-  mutex_t                   mutex;
-#elif CH_CFG_USE_SEMAPHORES
-  Semaphore                 semaphore;
-#endif
-#endif /* ADC_USE_MUTUAL_EXCLUSION */
-#if defined(ADC_DRIVER_EXT_FIELDS)
-  ADC_DRIVER_EXT_FIELDS
-#endif
-  /* End of the mandatory fields.*/
-  /**
-   * @brief Current position in the buffer.
-   */
-  size_t currentBufferPosition;
-};
+typedef uint8_t adcerror_t;
 
 /*==========================================================================*/
 /* Driver macros.                                                           */
 /*==========================================================================*/
+
+/**
+ * @brief   Low level fields of the ADC driver structure.
+ */
+#define adc_lld_driver_fields                                               \
+  size_t                   currentBufferPosition
+
+/**
+ * @brief   Low level fields of the ADC configuration structure.
+ */
+#define adc_lld_config_fields                                               \
+  uint8_t analog_reference
+
+/**
+ * @brief   Low level fields of the ADC configuration structure.
+ */
+#define adc_lld_configuration_group_fields                                  \
+  uint8_t                   channelsMask
 
 /*==========================================================================*/
 /* External declarations.                                                   */
