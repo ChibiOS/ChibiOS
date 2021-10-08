54047c87d90e4742d799b0fdfb30eb1fd2b43bcb
Giovanni Di Sirio <gdisirio@gmail.com>
2021-10-08 15:02:52 +0000

    draft file
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@14886 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

---
+++ os/common/ports/ARMv7-M-ALT/chcoreapi.c
@@ -0,0 +1,66 @@
+/*
+    ChibiOS - Copyright (C) 2006,2007,2008,2009,2010,2011,2012,2013,2014,
+              2015,2016,2017,2018,2019,2020,2021 Giovanni Di Sirio.
+
+    This file is part of ChibiOS.
+
+    ChibiOS is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation version 3 of the License.
+
+    ChibiOS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * @file    ARMv7-M-ALT/chcoreapi.c
+ * @brief   ARMv7-M (alternate) specific API.
+ *
+ * @addtogroup ARMV7M_ALT_COREAPI
+ * @{
+ */
+
+#include "ch.h"
+
+/*===========================================================================*/
+/* Module local definitions.                                                 */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Module exported variables.                                                */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Module local types.                                                       */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Module local variables.                                                   */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Module local functions.                                                   */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Module interrupt handlers.                                                */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Module exported functions.                                                */
+/*===========================================================================*/
+
+#if (PORT_USE_SYSCALL == TRUE) || defined(__DOXYGEN__)
+thread_t *chThdCreateStaticUnprivileged(void *wsp, size_t size, tprio_t prio,
+                                        uint32_t u_pc, uint32_t u_psp) {
+
+  return NULL;
+}
+#endif /* PORT_USE_SYSCALL == TRUE */
+
+/** @} */
