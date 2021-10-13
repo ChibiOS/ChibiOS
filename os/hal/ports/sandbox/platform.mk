2070147fe68b6ec0a6c624653e2e49f857e82c08
Giovanni Di Sirio <gdisirio@gmail.com>
2021-10-13 14:47:22 +0000

    Experimental code, to be removed.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@14908 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

---
+++ os/hal/ports/sandbox/platform.mk
@@ -0,0 +1,28 @@
+# Required platform files.
+PLATFORMSRC := $(CHIBIOS)/os/hal/ports/sandbox/hal_lld.c
+
+# Required include directories.
+PLATFORMINC := $(CHIBIOS)/os/hal/ports/sandbox
+
+# Optional platform files.
+ifeq ($(USE_SMART_BUILD),yes)
+
+# Configuration files directory
+ifeq ($(HALCONFDIR),)
+  ifeq ($(CONFDIR),)
+    HALCONFDIR = .
+  else
+    HALCONFDIR := $(CONFDIR)
+  endif
+endif
+
+HALCONF := $(strip $(shell cat $(HALCONFDIR)/halconf.h | egrep -e "\#define"))
+
+else
+endif
+
+# Drivers compatible with the platform.
+
+# Shared variables
+ALLCSRC += $(PLATFORMSRC)
+ALLINC  += $(PLATFORMINC)
