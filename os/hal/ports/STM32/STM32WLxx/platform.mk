29760bc1ead5758af5c0f02aa82ac3cb75878a85
vrepetenko <none@example.com>
2021-11-10 11:25:06 +0000

    STM32WL: updated to new SPI driver version.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@15048 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/hal/ports/STM32/STM32WLxx/platform.mk
+++ os/hal/ports/STM32/STM32WLxx/platform.mk
@@ -35,7 +35,7 @@ include $(CHIBIOS)/os/hal/ports/STM32/LLD/I2Cv2/driver.mk
 include $(CHIBIOS)/os/hal/ports/STM32/LLD/RCCv1/driver.mk
 include $(CHIBIOS)/os/hal/ports/STM32/LLD/RNGv1/driver.mk
 include $(CHIBIOS)/os/hal/ports/STM32/LLD/RTCv3/driver.mk
-include $(CHIBIOS)/os/hal/ports/STM32/LLD/SPIv2/driver.mk
+include $(CHIBIOS)/os/hal/ports/STM32/LLD/SPIv2/driver_v2.mk
 include $(CHIBIOS)/os/hal/ports/STM32/LLD/SYSTICKv1/driver.mk
 include $(CHIBIOS)/os/hal/ports/STM32/LLD/TIMv1/driver.mk
 include $(CHIBIOS)/os/hal/ports/STM32/LLD/USARTv2/driver.mk
