8318d115c07f4b28656bd81770fbc9104290aaae
Giovanni Di Sirio <gdisirio@gmail.com>
2022-03-10 11:07:41 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@15477 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- demos/STM32/RT-STM32H723ZG-NUCLEO144/main.c
+++ demos/STM32/RT-STM32H723ZG-NUCLEO144/main.c
@@ -32,10 +32,14 @@ static THD_FUNCTION(Thread1, arg) {
     palSetLine(LINE_LED1);
     chThdSleepMilliseconds(50);
     palSetLine(LINE_LED2);
+    chThdSleepMilliseconds(50);
+    palSetLine(LINE_LED3);
     chThdSleepMilliseconds(200);
     palClearLine(LINE_LED1);
     chThdSleepMilliseconds(50);
     palClearLine(LINE_LED2);
+    chThdSleepMilliseconds(50);
+    palClearLine(LINE_LED3);
     chThdSleepMilliseconds(200);
   }
 }
