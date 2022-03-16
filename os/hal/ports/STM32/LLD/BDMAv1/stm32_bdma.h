88dd95e70fc6259c46a9940b64238b689191df2a
Giovanni Di Sirio <gdisirio@gmail.com>
2022-03-16 16:17:09 +0000

    Removed check for old device revision.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@15525 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/hal/ports/STM32/LLD/BDMAv1/stm32_bdma.h
+++ os/hal/ports/STM32/LLD/BDMAv1/stm32_bdma.h
@@ -121,10 +121,8 @@
                                              STM32_BDMA_CR_MSIZE_MASK)
 #define STM32_BDMA_CR_PL_MASK               BDMA_CCR_PL_Msk
 #define STM32_BDMA_CR_PL(n)                 ((n) << 12U)
-#if !defined(STM32_ENFORCE_H7_REV_XY)
 #define STM32_BDMA_CR_DBM                   BDMA_CCR_DBM
 #define STM32_BDMA_CR_CM                    BDMA_CCR_CT
-#endif
 /** @} */
 
 /**
@@ -432,6 +430,20 @@ typedef struct {
     ;                                                                       \
   bdmaStreamDisable(stp);                                                   \
 }
+
+/**
+ * @brief   BDMA stream current target.
+ * @note    This function can be invoked in both ISR or thread context.
+ * @pre     The stream must have been allocated using @p bdmaStreamAllocate().
+ * @post    After use the stream can be released using @p bdmaStreamRelease().
+ *
+ * @param[in] stp       pointer to an @p stm32_bdma_stream_t structure
+ * @return              Current memory target index.
+ *
+ * @special
+ */
+#define bdmaStreamGetCurrentTarget(stp)                                     \
+  (((stp)->channel->CCR >> BDMA_CCR_CT_Pos) & 1U)
 /** @} */
 
 /*===========================================================================*/
