027131e086ec7d8f5f6f4de941d3d0a3cc0d1ea4
Giovanni Di Sirio <gdisirio@gmail.com>
2022-12-20 14:22:45 +0000

    Version numbers, documentation fixes.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@15898 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/hal/include/hal_sio.h
+++ os/hal/include/hal_sio.h
@@ -347,7 +347,7 @@ struct hal_sio_driver {
  * @brief   Clears flags from the enabled events flags mask.
  *
  * @param[in] siop      pointer to the @p SIODriver object
- * @param[in] flags     enabled events mask to be cleared
+ * @param[in] mask      enabled events mask to be cleared
  *
  * @xclass
  */
@@ -527,7 +527,6 @@ struct hal_sio_driver {
  * @brief   Wakes up the TX-waiting thread.
  *
  * @param[in] siop      pointer to the @p SIODriver object
- * @param[in] msg       the wake up message
  *
  * @notapi
  */
@@ -541,7 +540,6 @@ struct hal_sio_driver {
  * @brief   Wakes up the TXend-waiting thread.
  *
  * @param[in] siop      pointer to the @p SIODriver object
- * @param[in] msg       the wake up message
  *
  * @notapi
  */
