7773ef599de84ad5e4476fc586b85b972d65c97f
Theodore Ateba <tf.ateba@gmail.com>
2022-12-24 22:07:47 +0000

    [AVR] Add CRC HAL file for CRC module.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@15909 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/hal/ports/AVR/XMEGA/LLD/CRCv1/hal_crc.h
+++ os/hal/ports/AVR/XMEGA/LLD/CRCv1/hal_crc.h
@@ -1,12 +1,9 @@
 /*
     ChibiOS - Copyright (C) 2006..2022 Theodore Ateba
-
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
     You may obtain a copy of the License at
-
         http://www.apache.org/licenses/LICENSE-2.0
-
     Unless required by applicable law or agreed to in writing, software
     distributed under the License is distributed on an "AS IS" BASIS,
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,39 +12,23 @@
 */
 
 /**
- * @file    CRCv1/xmega_crc.h
- * @brief   XMEGA CRC units common header.
- * @note    This file requires definitions from the ATMEL XMEGA header file.
+ * @file    hal_crc.h
+ * @brief   CRC Driver macros and structures.
  *
- * @addtogroup XMEGA_CRCv1
+ * @addtogroup CRC
  * @{
  */
 
-#ifndef XMEGA_CRC_H
-#define XMEGA_CRC_H
+#ifndef HAL_CRC_H
+#define HAL_CRC_H
+
+#include <stdint.h>
+#include <stddef.h>
+#if (HAL_USE_CRC == TRUE) || defined(__DOXYGEN__)
 
 /*===========================================================================*/
 /* Driver constants.                                                         */
 /*===========================================================================*/
-
-/**
- * @brief   CRC module base address.
- */
-#define CRC_BASE        0x00D0
-
-/**
- * @note    Discarded definition from Atmel ATxmega headers.
- *          The CRC driver uses its own definition in order to have an
- *          unified handling for all devices.
- */
-#undef CRC
-
-/**
- * @name    CRC Cyclic redondancy check definitions.
- * @{
- */
-#define CRC             ((xmega_crc_t *)CRC_BASE)
-/** @} */
 
 /*===========================================================================*/
 /* Driver pre-compile time settings.                                         */
@@ -61,60 +42,7 @@
 /* Driver data structures and types.                                         */
 /*===========================================================================*/
 
-/**
- * @brief  XMEGA CRC registers block.
- */
-typedef struct {
-
-  volatile union {
-    uint8_t reg;
-    struct {
-      uint8_t SOURCE    : 4;
-      uint8_t           : 1;
-      uint8_t CRC32     : 1;
-      uint8_t RESET     : 2;
-    } bit;
-  }  CTRL;
-  volatile union {
-    uint8_t reg;
-    struct {
-      uint8_t BUSY      : 1;
-      uint8_t ZERO      : 1;
-      uint8_t           : 6;
-    } bit;
-  }  STATUS;
-  uint8_t reserved1;
-  volatile union {
-    uint8_t reg;
-    struct {
-      uint8_t DATAIN    : 8;
-    } bit;
-  }  DATAIN;
-  volatile union {
-    uint8_t reg;
-    struct {
-      uint8_t CHECKSUM  : 8;
-    } bit;
-  }  CHECKSUM0;
-  volatile union {
-    uint8_t reg;
-    struct {
-      uint8_t CHECKSUM  : 8;
-    } bit;
-  }  CHECKSUM1;
-  volatile union {
-    uint8_t reg;
-    struct {
-      uint8_t CHECKSUM  : 8;
-    } bit;
-  }  CHECKSUM2;
-  volatile union {
-    uint8_t reg;
-    struct {
-      uint8_t CHECKSUM  : 8;
-    } bit;
-  }  CHECKSUM3;
-} xmega_crc_t;
+#include "hal_crc_lld.h"
 
 /*===========================================================================*/
 /* Driver macros.                                                            */
@@ -124,6 +52,31 @@ typedef struct {
 /* External declarations.                                                    */
 /*===========================================================================*/
 
-#endif /* XMEGA_CRC_H */
+#ifdef __cplusplus
+extern "C" {
+#endif
+  void crcInit(void);
+  void crcObjectInit(CRCDriver *crcp);
+
+  msg_t crcStart(CRCDriver *crcp, CRCConfig * config);
+  void  crcStop(CRCDriver *crcp);
+
+  void  crcReset(CRCDriver *crcp, crcreset_t rt);
+
+  /* @TODO: uint16_t crcComputeCRC16OnMemory(CRCDriver *crcp, uint8_t *src, size_t size); */
+  /* @TODO: uint32_t crcComputeCRC32OnMemory(CRCDriver *crcp, uint8_t *src, size_t size); */
+
+  /* @TODO: uint16_t crcComputeCRC16OnDMA(CRCDriver *crcp, uint8_t *src, size_t size); */
+  /* @TODO: uint32_t crcComputeCRC32OnDMA(CRCDriver *crcp, uint8_t *src, size_t size); */
+
+  uint16_t crcComputeCRC16OnData(CRCDriver *crcp, uint8_t *srcp, size_t size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* HAL_USE_CRC == TRUE */
+
+#endif /* HAL_CRC_H */
 
 /** @} */
