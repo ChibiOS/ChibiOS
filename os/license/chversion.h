5158fb4ed99bac0c86fde7b3084868d00be8a412
Giovanni Di Sirio <gdisirio@gmail.com>
2022-12-26 11:00:16 +0000

    Release version and name.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_21.11.x@15924 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/license/chversion.h
+++ os/license/chversion.h
@@ -51,7 +51,7 @@
 /**
  * @brief   ChibiOS version string.
  */
-#define CH_VERSION              "2021.11.2"
+#define CH_VERSION              "2021.11.3"
 
 /**
  * @brief   ChibiOS version release year.
@@ -66,12 +66,12 @@
 /**
  * @brief   ChibiOS version patch number.
  */
-#define CH_VERSION_PATCH        2
+#define CH_VERSION_PATCH        3
 
 /**
  * @brief   ChibiOS version nickname.
  */
-#define CH_VERSION_NICKNAME     "Positano"
+#define CH_VERSION_NICKNAME     "Montepertuso"
 /** @} */
 
 /*===========================================================================*/
