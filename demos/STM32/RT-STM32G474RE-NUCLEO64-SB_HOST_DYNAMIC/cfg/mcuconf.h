f0fbde5f4f3d72170f6de38bb46ae6fd781d3974
Giovanni Di Sirio <gdisirio@gmail.com>
2023-03-11 10:42:47 +0000

    Small regressions.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_21.11.x@16154 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- demos/STM32/RT-STM32G474RE-NUCLEO64-SB_HOST_DYNAMIC/cfg/mcuconf.h
+++ demos/STM32/RT-STM32G474RE-NUCLEO64-SB_HOST_DYNAMIC/cfg/mcuconf.h
@@ -304,12 +304,12 @@
 /*
  * SERIAL driver system settings.
  */
-#define STM32_SERIAL_USE_USART1             TRUE
+#define STM32_SERIAL_USE_USART1             FALSE
 #define STM32_SERIAL_USE_USART2             FALSE
 #define STM32_SERIAL_USE_USART3             FALSE
 #define STM32_SERIAL_USE_UART4              FALSE
 #define STM32_SERIAL_USE_UART5              FALSE
-#define STM32_SERIAL_USE_LPUART1            FALSE
+#define STM32_SERIAL_USE_LPUART1            TRUE
 
 /*
  * SIO driver system settings.
