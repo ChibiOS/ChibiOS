c43f940a507ceae9892394532afec72af13401e3
Giovanni Di Sirio <gdisirio@gmail.com>
2023-04-13 17:25:05 +0000

    Enhanced STM32F7xx MPU configuration in mcuconf.h.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_21.11.x@16217 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- demos/STM32/RT-STM32-LWIP-FATFS-USB-HTTPS/cfg/stm32f746_discovery/mcuconf.h
+++ demos/STM32/RT-STM32-LWIP-FATFS-USB-HTTPS/cfg/stm32f746_discovery/mcuconf.h
@@ -36,9 +36,21 @@
 #define STM32F756_MCUCONF
 
 /*
- * HAL driver system settings.
+ * General settings.
  */
 #define STM32_NO_INIT                       FALSE
+
+/*
+ * Memory attributes settings.
+ */
+#define STM32_NOCACHE_ENABLE                TRUE
+#define STM32_NOCACHE_MPU_REGION            MPU_REGION_6
+#define STM32_NOCACHE_RBAR                  0x2004C000U
+#define STM32_NOCACHE_RASR                  MPU_RASR_SIZE_16K
+
+/*
+ * HAL driver system settings.
+ */
 #define STM32_PVD_ENABLE                    FALSE
 #define STM32_PLS                           STM32_PLS_LEV0
 #define STM32_BKPRAM_ENABLE                 FALSE
@@ -94,7 +106,6 @@
 #define STM32_CECSEL                        STM32_CECSEL_LSE
 #define STM32_CK48MSEL                      STM32_CK48MSEL_PLL
 #define STM32_SDMMC1SEL                     STM32_SDMMC1SEL_PLL48CLK
-#define STM32_SRAM2_NOCACHE                 FALSE
 
 /*
  * IRQ system settings.
