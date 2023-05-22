b29706616c5537ce7497bdadec2bc82f07dbbfe5
Rocco Marco Guglielmi <roccomarco.guglielmi@live.com>
2023-05-22 13:00:50 +0000

    Added CMSIS headers and startup files for MAX32625
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@16256 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

---
+++ os/common/ext/ADI/MAX3262x/max32625.h
@@ -0,0 +1,50 @@
+#ifndef __MAX32625_H__
+#define __MAX32625_H__
+
+#define MXC_UART_REV            1
+
+/* To add here support for 626 and 620 */
+#if defined(MAX32625)
+#include "MAX32625.h"
+#else
+#include "MAX32625.h"
+#endif
+
+#endif /* __MAX3262x_H__ */
+#ifndef __MAX32625_H__
+#define __MAX32625_H__
+
+#include "MAX32625/adc_regs.h"
+#include "MAX32625/aes_regs.h"
+#include "MAX32625/clkman_regs.h"
+#include "MAX32625/crc_regs.h"
+#include "MAX32625/flc_regs.h"
+#include "MAX32625/gpio_regs.h"
+#include "MAX32625/i2cm_regs.h"
+#include "MAX32625/i2cs_regs.h"
+#include "MAX32625/icc_regs.h"
+#include "MAX32625/ioman_regs.h"
+#include "MAX32625/maa_regs.h"
+#include "MAX32625/max32625.h"
+//#include "MAX32625/mxc_device.h"
+#include "MAX32625/owm_regs.h"
+#include "MAX32625/pmu_regs.h"
+#include "MAX32625/prng_regs.h"
+#include "MAX32625/pt_regs.h"
+#include "MAX32625/pwrman_regs.h"
+#include "MAX32625/pwrseq_regs.h"
+#include "MAX32625/rtc_regs.h"
+#include "MAX32625/spim_regs.h"
+#include "MAX32625/spis_regs.h"
+#include "MAX32625/spix_regs.h"
+#include "MAX32625/sysman_regs.h"
+#include "MAX32625/system_max32625.h"
+#include "MAX32625/tmr_regs.h"
+#include "MAX32625/tpu_regs.h"
+#include "MAX32625/trim_regs.h"
+#include "MAX32625/uart_regs.h"
+#include "MAX32625/usb_regs.h"
+#include "MAX32625/wdt2_regs.h"
+#include "MAX32625/wdt_regs.h"
+
+#endif /* __MAX32625_H__ */
