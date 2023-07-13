af2453d8dd769f6de87ba22b3f988aa40160afe4
fxcoder <none@example.com>
2023-07-13 21:24:51 +0000

    Remove DAC_MCR for F7 series
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_21.11.x@16330 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/hal/ports/STM32/STM32F7xx/stm32_registry.h
+++ os/hal/ports/STM32/STM32F7xx/stm32_registry.h
@@ -38,9 +38,6 @@
 /* Common.                                                                   */
 /*===========================================================================*/
 
-/* DAC attributes.*/
-#define STM32_DAC_HAS_MCR                   TRUE
-
 /* RNG attributes.*/
 #define STM32_HAS_RNG1                      TRUE
 
