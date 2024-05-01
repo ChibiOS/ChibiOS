4f63f01e7029730d88544ccbf0e126f22782bfb8
Giovanni Di Sirio <gdisirio@gmail.com>
2024-05-01 14:55:35 +0000

    Moved new API work to /branches/rt8_dev, undone changes here.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@16505 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/rt/rt.mk
+++ os/rt/rt.mk
@@ -20,8 +20,7 @@ KERNSRC := $(CHIBIOS)/os/rt/src/chsys.c \
            $(CHIBIOS)/os/rt/src/chvt.c \
            $(CHIBIOS)/os/rt/src/chschd.c \
            $(CHIBIOS)/os/rt/src/chinstances.c \
-           $(CHIBIOS)/os/rt/src/chthreads.c \
-           $(CHIBIOS)/os/rt/src/chthreads2.c
+           $(CHIBIOS)/os/rt/src/chthreads.c
 ifneq ($(findstring CH_CFG_USE_TM TRUE,$(CHCONF)),)
 KERNSRC += $(CHIBIOS)/os/rt/src/chtm.c
 endif
