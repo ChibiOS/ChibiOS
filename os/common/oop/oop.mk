90954b514869ef673aa6f6f3808ac200062b6584
Giovanni Di Sirio <gdisirio@gmail.com>
2024-10-07 08:47:27 +0000

    Makefiles now can check for multiple inclusions of common subsystems.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@16615 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/common/oop/oop.mk
+++ os/common/oop/oop.mk
@@ -1,9 +1,14 @@
-# List of the ChibiOS common OOP modules.
-OOPSRC = ${CHIBIOS}/os/common/oop/src/oop_base_object.c \
-         ${CHIBIOS}/os/common/oop/src/oop_referenced_object.c
- 
-OOPINC = ${CHIBIOS}/os/common/oop/include
+# OOP subsystem build.
 
-# Shared variables
-ALLCSRC += $(OOPSRC)
-ALLINC  += $(OOPINC)
+# Required files.
+ifeq ($(OOPSRC),)
+  OOPSRC = ${CHIBIOS}/os/common/oop/src/oop_base_object.c \
+           ${CHIBIOS}/os/common/oop/src/oop_referenced_object.c
+  ALLCSRC += $(OOPSRC)
+endif
+
+# Required include directories
+ifeq ($(OOPINC),)
+  OOPINC = ${CHIBIOS}/os/common/oop/include
+  ALLINC += $(OOPINC)
+endif
--- os/hal/lib/complex/xsnor/devices/micron_n25q/hal_xsnor_micron_n25q.mk
+++ os/hal/lib/complex/xsnor/devices/micron_n25q/hal_xsnor_micron_n25q.mk
@@ -1,10 +1,13 @@
-# List of all the Micron N25Q device files.
-N25QSRC := $(CHIBIOS)/os/hal/lib/complex/xsnor/src/hal_xsnor_base.c \
-           $(CHIBIOS)/os/hal/lib/complex/xsnor/devices/micron_n25q/hal_xsnor_micron_n25q.c
+# XSNOR + N25Q subsystem build.
+
+# Dependencies.
+include $(CHIBIOS)/os/hal/lib/complex/xsnor/hal_xsnor.mk
+
+# Required files.
+N25QSRC := $(CHIBIOS)/os/hal/lib/complex/xsnor/devices/micron_n25q/hal_xsnor_micron_n25q.c
 
 # Required include directories
-N25QINC := $(CHIBIOS)/os/hal/lib/complex/xsnor/include \
-           $(CHIBIOS)/os/hal/lib/complex/xsnor/devices/micron_n25q
+N25QINC := $(CHIBIOS)/os/hal/lib/complex/xsnor/devices/micron_n25q
 
 # Shared variables
 ALLCSRC += $(N25QSRC)
