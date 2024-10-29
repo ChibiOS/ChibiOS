97a2bd5d969e44fd0935af4dd3744d00cf634fe1
Giovanni Di Sirio <gdisirio@gmail.com>
2024-10-29 12:45:13 +0000

    experimental
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@16690 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

---
+++ os/common/ports/ARMv8-M-ML-ALT/chcore.c
@@ -0,0 +1,112 @@
+/*
+    ChibiOS - Copyright (C) 2006,2007,2008,2009,2010,2011,2012,2013,2014,
+              2015,2016,2017,2018,2019,2020,2021 Giovanni Di Sirio.
+
+    This file is part of ChibiOS.
+
+    ChibiOS is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation version 3 of the License.
+
+    ChibiOS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * @file    ARMv8-M-ML-TZ/chcore.c
+ * @brief   ARMv8-M MainLine port code.
+ *
+ * @addtogroup ARMV8M_ML_TZ_CORE
+ * @{
+ */
+
+#include "ch.h"
+
+/*===========================================================================*/
+/* Module local definitions.                                                 */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Module exported variables.                                                */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Module local types.                                                       */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Module local variables.                                                   */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Module local functions.                                                   */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Module exported functions.                                                */
+/*===========================================================================*/
+
+/**
+ * @brief   Tail ISR context switch code.
+ *
+ * @return              The threads pointers encoded in a single 64 bits value.
+ */
+uint64_t __port_schedule_next(void) {
+
+  /* Note, not an error, we are outside the ISR already.*/
+  chSysLock();
+
+  if (likely(chSchIsPreemptionRequired())) {
+    thread_t *otp, *ntp;
+
+    otp = chThdGetSelfX();
+    ntp = chSchSelectFirst();
+
+    __trace_switch(ntp, otp);
+    __stats_ctxswc(ntp, otp);
+
+    return ((uint64_t)(uint32_t)otp << 32) | ((uint64_t)(uint32_t)ntp << 0);
+  }
+
+  chSysUnlock();
+
+  return (uint64_t)0;
+}
+
+/**
+ * @brief   Port-related initialization code.
+ *
+ * @param[in, out] oip  pointer to the @p os_instance_t structure
+ *
+ * @notapi
+ */
+void port_init(os_instance_t *oip) {
+
+  (void)oip;
+
+  /* Starting in a known IRQ configuration.*/
+  port_suspend();
+
+  /* Initializing priority grouping.*/
+  NVIC_SetPriorityGrouping(CORTEX_PRIGROUP_INIT);
+
+  /* DWT cycle counter enable.*/
+  CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+  DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
+
+  /* Initialization of the system vectors used by the port.*/
+  NVIC_SetPriority(SVCall_IRQn, CORTEX_PRIORITY_SVCALL);
+  NVIC_SetPriority(PendSV_IRQn, CORTEX_PRIORITY_PENDSV);
+
+#if PORT_USE_SYSCALL == TRUE
+  /* MPU is enabled.*/
+  mpuEnable(MPU_CTRL_PRIVDEFENA);
+#endif
+}
+
+/** @} */
