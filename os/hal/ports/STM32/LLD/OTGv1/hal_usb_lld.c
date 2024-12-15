dfdb730ecf5e536174e37dd9b5ff37e6bb5a59da
Giovanni Di Sirio <gdisirio@gmail.com>
2024-12-15 07:44:56 +0000

    Fixed partial patch, removed an extra check.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_21.11.x@16812 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/hal/ports/STM32/LLD/OTGv1/hal_usb_lld.c
+++ os/hal/ports/STM32/LLD/OTGv1/hal_usb_lld.c
@@ -311,30 +311,36 @@ static void otg_fifo_read_to_buffer(volatile uint32_t *fifop,
  * @notapi
  */
 static void otg_rxfifo_handler(USBDriver *usbp) {
-  uint32_t sts, cnt, ep;
+  uint32_t sts, ep;
+  size_t n, max;
 
   /* Popping the event word out of the RX FIFO.*/
   sts = usbp->otg->GRXSTSP;
 
   /* Event details.*/
-  cnt = (sts & GRXSTSP_BCNT_MASK) >> GRXSTSP_BCNT_OFF;
-  ep  = (sts & GRXSTSP_EPNUM_MASK) >> GRXSTSP_EPNUM_OFF;
+  n  = (size_t)((sts & GRXSTSP_BCNT_MASK) >> GRXSTSP_BCNT_OFF);
+  ep = (sts & GRXSTSP_EPNUM_MASK) >> GRXSTSP_EPNUM_OFF;
 
   switch (sts & GRXSTSP_PKTSTS_MASK) {
   case GRXSTSP_SETUP_DATA:
     otg_fifo_read_to_buffer(usbp->otg->FIFO[0], usbp->epc[ep]->setup_buf,
-                            cnt, 8);
+                            n, 8);
     break;
   case GRXSTSP_SETUP_COMP:
     break;
   case GRXSTSP_OUT_DATA:
+    max = usbp->epc[ep]->out_state->rxsize - usbp->epc[ep]->out_state->rxcnt;
     otg_fifo_read_to_buffer(usbp->otg->FIFO[0],
                             usbp->epc[ep]->out_state->rxbuf,
-                            cnt,
-                            usbp->epc[ep]->out_state->rxsize -
-                            usbp->epc[ep]->out_state->rxcnt);
-    usbp->epc[ep]->out_state->rxbuf += cnt;
-    usbp->epc[ep]->out_state->rxcnt += cnt;
+                            n, max);
+    if (n < max) {
+      usbp->epc[ep]->out_state->rxbuf += n;
+      usbp->epc[ep]->out_state->rxcnt += n;
+    }
+    else {
+      usbp->epc[ep]->out_state->rxbuf += max;
+      usbp->epc[ep]->out_state->rxcnt += max;
+    }
     break;
   case GRXSTSP_OUT_COMP:
     break;
@@ -624,14 +630,13 @@ irq_retry:
       otgp->GINTMSK &= ~GINTMSK_SOFM;
     }
     if (usbp->state == USB_SUSPENDED) {
-      /* If clocks are gated off, turn them back on (may be the case if
-         coming out of suspend mode).*/
-      if (otgp->PCGCCTL & (PCGCCTL_STPPCLK | PCGCCTL_GATEHCLK)) {
-        /* Set to zero to un-gate the USB core clocks.*/
-        otgp->PCGCCTL &= ~(PCGCCTL_STPPCLK | PCGCCTL_GATEHCLK);
-      }
+      /* Set to zero to un-gate the USB core clocks.*/
+      otgp->PCGCCTL &= ~(PCGCCTL_STPPCLK | PCGCCTL_GATEHCLK);
       _usb_wakeup(usbp);
     }
+
+    /* Re-enable endpoint irqs if they have been disabled by suspend before.*/
+    otg_enable_ep(usbp);
 
     _usb_isr_invoke_sof_cb(usbp);
   }
