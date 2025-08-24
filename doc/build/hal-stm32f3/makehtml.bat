9dd2b400e7ec5532ac598cc4de1077a1d695ab7b
Giovanni Di Sirio <gdisirio@gmail.com>
2025-08-24 09:03:41 +0000

    Removed unused builder.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@17136 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- doc/build/hal-stm32f3/makehtml.bat
+++
@@ -1,5 +0,0 @@
-del ..\..\manuals\html\hal_stm32f3xx\*.* /Q
-doxygen Doxyfile_html
-del ..\..\manuals\html\hal_stm32f3xx\*.md5
-del ..\..\manuals\html\hal_stm32f3xx\*.map
-pause
--- doc/build/hal-stm32f3/makehtml.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm ../../manuals/html/hal_stm32f3xx/*
-doxygen Doxyfile_html
-rm ../../manuals/html/hal_stm32f3xx/*.md5
-rm ../../manuals/html/hal_stm32f3xx/*.map
-
