ccf47c42bf62adc2096567fbfd9039976a882732
Giovanni Di Sirio <gdisirio@gmail.com>
2025-08-27 10:05:46 +0000

    PDF builders all working, removed some old worarounds.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_21.11.x@17155 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- doc/build/makepdf.sh
+++ doc/build/makepdf.sh
@@ -2,7 +2,7 @@
 export CHVERSION=21.11.4
 
 function makeclean() {
-  rm *.log ../../manuals/*.pdf ../../manuals/*.tar.gz 2> /dev/null
+  rm *.log 2> /dev/null
 }
 
 #function makechm() {
@@ -28,53 +28,53 @@ function makeclean() {
 function makepdf() {
   rm latex/* 2> /dev/null
   cp ../common/tex/chibios.sty ./latex
-  if ! doxygen Doxyfile_pdf 2> /dev/null
+  if ! doxygen Doxyfile_pdf #2> /dev/null
   then
     echo "Doxygen failed"
     exit
   fi
-  cp ../common/tex/Makefile ./latex
+#  cp ../common/tex/Makefile ./latex
   cd latex
   # Patches a Doxygen bug.
-  if grep "input{hierarchy}" < refman.tex > /dev/null
-  then
-    cp refman.tex refman2.tex
-    sed -e '0,/input{hierarchy}/s/Data Structure/Class Hierarchy/' < refman2.tex > refman.tex
-  fi
+#  if grep "input{hierarchy}" < refman.tex > /dev/null
+#  then
+#    cp refman.tex refman2.tex
+#    sed -e '0,/input{hierarchy}/s/Data Structure/Class Hierarchy/' < refman2.tex > refman.tex
+#  fi
   # PDF generation.
-  if ! make all 1> ../chibios_${CHVERSION}_${CHNAME}_pdf1.log 2> ../chibios_${CHVERSION}_${CHNAME}_pdf2.log
+  if ! make all 1> ../chibios_${CHVERSION}_${CHNAME}_rm_pdf1.log 2> ../chibios_${CHVERSION}_${CHNAME}_rm_pdf2.log
   then
     echo "Make failed"
     exit
   fi
   cd ..
-  cp latex/refman.pdf ../../manuals/chibios_${CHVERSION}_${CHNAME}.pdf
+  cp latex/refman.pdf ../../manuals/chibios_${CHVERSION}_${CHNAME}_rm.pdf
   rm latex/* 2> /dev/null
   echo "  - PDF generated"
 }
 
 function makearchive() {
 #  tar -pczf chibios_${CHVERSION}_${CHNAME}.tar.gz chibios_${CHVERSION}_${CHNAME}.chm chibios_${CHVERSION}_${CHNAME}.pdf
-  tar -pczf ../../manuals/chibios_${CHVERSION}_${CHNAME}.tar.gz ../../manuals/chibios_${CHVERSION}_${CHNAME}.pdf 2> /dev/null
+  tar -pczf ../../manuals/chibios_${CHVERSION}_${CHNAME}_rm.tar.gz ../../manuals/chibios_${CHVERSION}_${CHNAME}_rm.pdf 2> /dev/null
 }
 
-#export CHNAME=chibios_hal_rm
-#echo doc_${CHNAME}
-#cd doc_${CHNAME}
-#makeclean
+export CHNAME=hal
+echo ${CHNAME}
+cd ${CHNAME}
+makeclean
 #makechm
-#makepdf
-#makearchive
-#cd ..
+makepdf
+makearchive
+cd ..
 
-#export CHNAME=chibios_nil_rm
-#echo doc_${CHNAME}
-#cd doc_${CHNAME}
-#makeclean
+export CHNAME=nil
+echo ${CHNAME}
+cd ${CHNAME}
+makeclean
 #makechm
-#makepdf
-#makearchive
-#cd ..
+makepdf
+makearchive
+cd ..
 
 export CHNAME=rt
 echo ${CHNAME}
