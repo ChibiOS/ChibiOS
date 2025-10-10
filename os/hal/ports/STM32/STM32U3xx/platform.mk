f3b8280b02d8c5217923016211b2fff430e67a7e
Giovanni Di Sirio <gdisirio@gmail.com>
2025-10-10 12:36:51 +0000

    More U3 code.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@17237 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/hal/ports/STM32/STM32U3xx/platform.mk
+++ os/hal/ports/STM32/STM32U3xx/platform.mk
@@ -25,21 +25,20 @@ else
 endif
 
 # Drivers compatible with the platform.
-include $(CHIBIOS)/os/hal/ports/STM32/LLD/ADCv6/driver.mk
-include $(CHIBIOS)/os/hal/ports/STM32/LLD/DACv2/driver.mk
+#include $(CHIBIOS)/os/hal/ports/STM32/LLD/ADCv6/driver.mk
+#include $(CHIBIOS)/os/hal/ports/STM32/LLD/DACv2/driver.mk
 include $(CHIBIOS)/os/hal/ports/STM32/LLD/DMA3v1/driver.mk
 include $(CHIBIOS)/os/hal/ports/STM32/LLD/EXTIv1/driver.mk
-include $(CHIBIOS)/os/hal/ports/STM32/LLD/FDCANv1/driver.mk
+#include $(CHIBIOS)/os/hal/ports/STM32/LLD/FDCANv1/driver.mk
 include $(CHIBIOS)/os/hal/ports/STM32/LLD/GPIOv2/driver.mk
-include $(CHIBIOS)/os/hal/ports/STM32/LLD/I2Cv4/driver.mk
+#include $(CHIBIOS)/os/hal/ports/STM32/LLD/I2Cv4/driver.mk
 include $(CHIBIOS)/os/hal/ports/STM32/LLD/ICACHEv1/driver.mk
-include $(CHIBIOS)/os/hal/ports/STM32/LLD/MACv2/driver.mk
 include $(CHIBIOS)/os/hal/ports/STM32/LLD/RCCv1/driver.mk
-include $(CHIBIOS)/os/hal/ports/STM32/LLD/RTCv3/driver.mk
-include $(CHIBIOS)/os/hal/ports/STM32/LLD/SPIv4/driver.mk
-include $(CHIBIOS)/os/hal/ports/STM32/LLD/SDMMCv2/driver.mk
+#include $(CHIBIOS)/os/hal/ports/STM32/LLD/RTCv3/driver.mk
+#include $(CHIBIOS)/os/hal/ports/STM32/LLD/SPIv4/driver.mk
+#include $(CHIBIOS)/os/hal/ports/STM32/LLD/SDMMCv2/driver.mk
 include $(CHIBIOS)/os/hal/ports/STM32/LLD/SYSTICKv1/driver.mk
-include $(CHIBIOS)/os/hal/ports/STM32/LLD/RNGv1/driver.mk
+#include $(CHIBIOS)/os/hal/ports/STM32/LLD/RNGv1/driver.mk
 include $(CHIBIOS)/os/hal/ports/STM32/LLD/TIMv1/driver.mk
 include $(CHIBIOS)/os/hal/ports/STM32/LLD/USARTv3/driver.mk
 include $(CHIBIOS)/os/hal/ports/STM32/LLD/USBv2/driver.mk
