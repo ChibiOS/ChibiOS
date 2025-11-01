da227ac2610aacb49309058d7b4be41e33a68599
Giovanni Di Sirio <gdisirio@gmail.com>
2025-11-01 04:27:46 +0000

    Changed API signature.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@17280 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/various/cpp_wrappers/ch.hpp
+++ os/various/cpp_wrappers/ch.hpp
@@ -81,30 +81,24 @@ namespace chibios_rt {
      * @brief   System integrity check.
      * @details Performs an integrity check of the important ChibiOS/RT data
      *          structures.
-     * @note    The appropriate action in case of failure is to halt the system
-     *          before releasing the critical zone.
-     * @note    If the system is corrupted then one possible outcome of this
-     *          function is an exception caused by @p nullptr or corrupted
-     *          pointers in list elements. Exception vectors must be monitored
-     *          as well.
+     * @note    The reaction in case of failure is to invoke the
+     *          @p CH_CFG_INTEGRITY_HOOK which, by default, halts the system.
+     * @note    This functionality is available at any hardening level.
+     * @note    Pointers validation is enabled at hardening level 2 or greater,
+     *          at lower levels a corrupted pointer can cause an exception.
+     *          Exceptions should be monitored as well as possible outcomes.
      * @note    This function is not used internally, it is up to the
      *          application to define if and where to perform system
      *          checking.
-     * @note    Performing all tests at once can be a slow operation and can
-     *          degrade the system response time. It is suggested to execute
-     *          one test at time and release the critical zone in between tests.
      *
      * @param[in] testmask  Each bit in this mask is associated to a test to be
      *                      performed.
-     * @return              The test result.
-     * @retval false        The test succeeded.
-     * @retval true         Test failed.
      *
      * @iclass
      */
-    static bool integrityCheckI(unsigned int testmask) {
+    static void integrityCheckI(unsigned int testmask) {
 
-      return chSysIntegrityCheckI(testmask);
+      chSftIntegrityCheckI(testmask);
     }
 
     /**
