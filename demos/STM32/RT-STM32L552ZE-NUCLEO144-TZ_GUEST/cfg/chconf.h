7708d56b98ddf8cb1959dbf03eb32494cf7d1ec5
Giovanni Di Sirio <gdisirio@gmail.com>
2025-11-28 11:27:37 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@17380 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- demos/STM32/RT-STM32L552ZE-NUCLEO144-TZ_GUEST/cfg/chconf.h
+++ demos/STM32/RT-STM32L552ZE-NUCLEO144-TZ_GUEST/cfg/chconf.h
@@ -85,7 +85,7 @@
  *          setting also defines the system tick time unit.
  */
 #if !defined(CH_CFG_ST_FREQUENCY)
-#define CH_CFG_ST_FREQUENCY                 2000
+#define CH_CFG_ST_FREQUENCY                 1000
 #endif
 
 /**
