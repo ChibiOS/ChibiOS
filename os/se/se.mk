40bd2c6f375984de84d3468eb953d42fc7ff7dfe
Giovanni Di Sirio <gdisirio@gmail.com>
2025-11-28 20:49:37 +0000

    Remove legacy SE safety extensions
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@17387 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/se/se.mk
+++
@@ -1,9 +0,0 @@
-# List of all the ChibiOS/SE kernel file.
-SESRC := $(CHIBIOS)/os/se/src/seloops.c
-
-# Required include directories
-SEINC := $(CHIBIOS)/os/se/include
-
-# Shared variables
-ALLCSRC += $(SESRC)
-ALLINC  += $(SEINC)
