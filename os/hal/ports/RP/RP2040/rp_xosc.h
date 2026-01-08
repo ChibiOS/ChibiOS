0d87486648bdebb4457e896a348d0100255177e4
Eric Molitor <eric@molitor.org>
2026-01-08 13:13:07 +0000

    RP2040 add atomic register functions and initial XOSC port
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@17556 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

---
+++ os/hal/ports/RP/RP2040/rp_xosc.h
@@ -0,0 +1,79 @@
+/*
+    ChibiOS - Copyright (C) 2006..2025 Giovanni Di Sirio
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+/**
+ * @file    RP2040/rp_xosc.h
+ * @brief   RP2040 XOSC header.
+ *
+ * @addtogroup RP_XOSC
+ * @{
+ */
+
+#ifndef RP_XOSC_H
+#define RP_XOSC_H
+
+/*===========================================================================*/
+/* Driver constants.                                                         */
+/*===========================================================================*/
+
+/**
+ * @brief   XOSC startup delay multiplier.
+ * @note    Default value of 1 for 12MHz crystal.
+ */
+#if !defined(RP_XOSC_STARTUP_DELAY_MULTIPLIER)
+#define RP_XOSC_STARTUP_DELAY_MULTIPLIER    1U
+#endif
+
+/*===========================================================================*/
+/* Driver pre-compile time settings.                                         */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Derived constants and error checks.                                       */
+/*===========================================================================*/
+
+/**
+ * @brief   XOSC startup delay value.
+ * @note    Delay is in multiples of 256 crystal periods
+ *          For 12MHz, each count is ~21.3us. Default gives 1ms startup.
+ */
+#define RP_XOSC_STARTUP_DELAY               \
+    ((((RP_XOSCCLK / 1000U) + 128U) / 256U) * RP_XOSC_STARTUP_DELAY_MULTIPLIER)
+
+/*===========================================================================*/
+/* Driver data structures and types.                                         */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver macros.                                                            */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* External declarations.                                                    */
+/*===========================================================================*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+  void rp_xosc_init(void);
+  void rp_xosc_disable(void);
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* RP_XOSC_H */
+
+/** @} */
