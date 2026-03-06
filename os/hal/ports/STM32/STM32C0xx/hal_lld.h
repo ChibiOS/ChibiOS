f38f73771625438f83931f9099706c0be0f9bac0
Giovanni Di Sirio <gdisirio@gmail.com>
2026-03-06 18:08:45 +0000

    License mass update.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/branches/stable_21.11.x@17767 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/hal/ports/STM32/STM32C0xx/hal_lld.h
+++ os/hal/ports/STM32/STM32C0xx/hal_lld.h
@@ -1,5 +1,5 @@
 /*
-    ChibiOS - Copyright (C) 2006..2022 Giovanni Di Sirio
+    ChibiOS - Copyright (C) 2006-2026 Giovanni Di Sirio.
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -11,7 +11,7 @@
     distributed under the License is distributed on an "AS IS" BASIS,
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
-    limitations under the License.f
+    limitations under the License.
 */
 
 /**
