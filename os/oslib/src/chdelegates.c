cc18f5eba3c9ba38374930c5ca7d2c61990f273d
Giovanni Di Sirio <gdisirio@gmail.com>
2026-03-18 18:18:29 +0000

    Doc fixes, added a parameters check.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@17903 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/oslib/src/chdelegates.c
+++ os/oslib/src/chdelegates.c
@@ -159,12 +159,13 @@ msg_t chDelegateCallVeneer(thread_t *tp, delegate_veneer_t veneer, ...) {
   call_message_t cm;
   msg_t msg;
 
+  chDbgCheck((tp != NULL) && (veneer != NULL));
+
   va_start(args, veneer);
 
   /* Preparing the call message.*/
   cm.veneer = veneer;
   cm.argsp  = &args;
-  (void)cm; /* Suppresses a lint warning.*/
 
   /* Sending the message to the dispatcher thread, the return value is
      contained in the returned message.*/
@@ -207,7 +208,9 @@ void chDelegateDispatch(void) {
  *
  * @param[in] timeout   the number of ticks before the operation timeouts,
  *                      the following special values are allowed:
+ *                      - @a TIME_IMMEDIATE immediate timeout.
  *                      - @a TIME_INFINITE no timeout.
+ *                      .
  * @return              The function outcome.
  * @retval MSG_OK       if a function has been called.
  * @retval MSG_TIMEOUT  if a timeout occurred.
