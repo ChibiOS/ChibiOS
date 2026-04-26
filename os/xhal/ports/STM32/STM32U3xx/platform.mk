d9ec6f93040fbdeafd35a5f1b582422b213e2f48
Giovanni Di Sirio <gdisirio@gmail.com>
2026-04-26 13:11:03 +0000

    • Add STM32U0/U3 XHAL ports and multi-demo targets
      Import the STM32U0xx and STM32U3xx ports into XHAL and adapt their platform
      files, IRQ setup, and xmcuconf.h checks for XHAL.
    
      Add STM32U083RC-Nucleo64 and STM32U385RG-Nucleo64 targets to the XHAL STM32
      multi demo, including local configs, makefiles, SIO portability glue, and
      Eclipse CDT metadata.
    
      Add U0/U3 xmcuconf.h FTL templates and updater scripts. Validate both new demo
      targets and generated configuration update flow.
    
    
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@18141 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

---
+++ os/xhal/ports/STM32/STM32U3xx/platform.mk
@@ -0,0 +1,47 @@
+# Required platform files.
+PLATFORMSRC := $(CHIBIOS)/os/xhal/ports/common/ARMCMx/nvic.c \
+               $(CHIBIOS)/os/xhal/ports/STM32/STM32U3xx/stm32_isr.c \
+               $(CHIBIOS)/os/xhal/ports/STM32/STM32U3xx/hal_lld.c
+
+# Required include directories.
+PLATFORMINC := $(CHIBIOS)/os/xhal/ports/common/ARMCMx \
+               $(CHIBIOS)/os/xhal/ports/STM32/STM32U3xx \
+               $(CHIBIOS)/os/xhal/ports/STM32/LLD/SPI
+
+# Optional platform files.
+ifeq ($(USE_SMART_BUILD),yes)
+
+# Configuration files directory
+ifeq ($(HALCONFDIR),)
+  ifeq ($(CONFDIR),)
+    HALCONFDIR = .
+  else
+    HALCONFDIR := $(CONFDIR)
+  endif
+endif
+
+HALCONF := $(strip $(shell cat $(HALCONFDIR)/xhalconf.h | grep -E "\#define"))
+
+else
+endif
+
+# Drivers compatible with the platform.
+include $(CHIBIOS)/os/xhal/ports/STM32/LLD/DACv2/driver.mk
+include $(CHIBIOS)/os/xhal/ports/STM32/LLD/DMA3v1/driver.mk
+include $(CHIBIOS)/os/xhal/ports/STM32/LLD/EXTIv1/driver.mk
+include $(CHIBIOS)/os/xhal/ports/STM32/LLD/FDCANv1/driver.mk
+include $(CHIBIOS)/os/xhal/ports/STM32/LLD/GPIOv2/driver.mk
+include $(CHIBIOS)/os/xhal/ports/STM32/LLD/I2Cv4/driver.mk
+include $(CHIBIOS)/os/xhal/ports/STM32/LLD/ICACHEv1/driver.mk
+include $(CHIBIOS)/os/xhal/ports/STM32/LLD/RCCv1/driver.mk
+include $(CHIBIOS)/os/xhal/ports/STM32/LLD/SDMMCv2/driver.mk
+include $(CHIBIOS)/os/xhal/ports/STM32/LLD/SPIv2/driver.mk
+include $(CHIBIOS)/os/xhal/ports/STM32/LLD/SYSTICKv1/driver.mk
+include $(CHIBIOS)/os/xhal/ports/STM32/LLD/RNGv1/driver.mk
+include $(CHIBIOS)/os/xhal/ports/STM32/LLD/TIMv1/driver.mk
+include $(CHIBIOS)/os/xhal/ports/STM32/LLD/USARTv3/driver.mk
+include $(CHIBIOS)/os/xhal/ports/STM32/LLD/xWDGv1/driver.mk
+
+# Shared variables
+ALLCSRC += $(PLATFORMSRC)
+ALLINC  += $(PLATFORMINC)
