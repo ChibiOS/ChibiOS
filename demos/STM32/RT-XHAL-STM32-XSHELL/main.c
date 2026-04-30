f15d568378d45ba43bfe512e5f27f82b3afb5072
Giovanni Di Sirio <gdisirio@gmail.com>
2026-04-30 09:42:33 +0000

    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@18169 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

---
+++ demos/STM32/RT-XHAL-STM32-XSHELL/main.c
@@ -0,0 +1,181 @@
+/*
+    ChibiOS - Copyright (C) 2006-2026 Giovanni Di Sirio.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include "ch.h"
+#include "hal.h"
+
+#include "oop_chprintf.h"
+#include "xshell.h"
+
+#include "clock_cmds.h"
+#include "portab.h"
+
+static hal_buffered_sio_c bsio1;
+static uint8_t rxbuf[32];
+static uint8_t txbuf[512];
+
+/*===========================================================================*/
+/* Command line related.                                                     */
+/*===========================================================================*/
+
+#define SHELL_WA_SIZE       THD_STACK_SIZE(1024)
+
+static void cmd_halt(xshell_t *xshp, int argc, char *argv[], char *envp[]) {
+
+  (void)argv;
+  (void)envp;
+
+  if (argc != 1) {
+    xshellUsage(xshp, "halt");
+    return;
+  }
+
+  chprintf(xshp->stream, XSHELL_NEWLINE_STR "halted");
+  chThdSleepMilliseconds(10);
+  chSysHalt("shell halt");
+}
+
+/* Can be measured using dd if=/dev/xxxx of=/dev/null bs=512 count=10000.*/
+static void cmd_write(xshell_t *xshp, int argc, char *argv[], char *envp[]) {
+  static const uint8_t buf[] =
+      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+      "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef";
+
+  (void)argv;
+  (void)envp;
+
+  if (argc != 1) {
+    xshellUsage(xshp, "write");
+    return;
+  }
+
+  while (chnGetTimeout((asynchronous_channel_i *)xshp->stream,
+                       TIME_IMMEDIATE) == Q_TIMEOUT) {
+    chnWrite(xshp->stream, buf, sizeof buf - 1);
+  }
+  chprintf(xshp->stream, XSHELL_NEWLINE_STR "stopped" XSHELL_NEWLINE_STR);
+}
+
+static const xshell_command_t commands[] = {
+#if defined(HAL_LLD_USE_CLOCK_MANAGEMENT)
+  {"clock",     cmd_clock},
+#endif
+#if defined(CLK_POINT_NAMES)
+  {"clocks",    cmd_clocks},
+#endif
+  {"halt",      cmd_halt},
+  {"write",     cmd_write},
+  {NULL, NULL}
+};
+
+static const xshell_manager_config_t cfg1 = {
+  .thread_name      = "shell",
+  .banner           = XSHELL_DEFAULT_BANNER_STR,
+  .prompt           = XSHELL_DEFAULT_PROMPT_STR,
+  .commands         = commands,
+  .use_heap         = true,
+  .stack.size       = SHELL_WA_SIZE
+};
+
+/*===========================================================================*/
+/* Application code.                                                         */
+/*===========================================================================*/
+
+/*
+ * LED blinker thread, times are in milliseconds.
+ */
+static THD_STACK(thd1_stack, 256);
+static THD_FUNCTION(thd1_func, arg) {
+
+  (void)arg;
+
+  while (true) {
+    palSetLine(PORTAB_LINE_LED1);
+    chThdSleepMilliseconds(500);
+    palClearLine(PORTAB_LINE_LED1);
+    chThdSleepMilliseconds(500);
+  }
+}
+
+/*
+ * Application entry point.
+ */
+int main(void) {
+  xshell_manager_t sm1;
+  msg_t msg;
+
+  /*
+   * System initializations.
+   * - HAL initialization, this also initializes the configured device drivers
+   *   and performs the board-specific initializations.
+   * - Kernel initialization, the main() function becomes a thread and the
+   *   RTOS is active.
+   */
+  halInit();
+  chSysInit();
+
+  /* Board-dependent GPIO setup code.*/
+  portab_setup();
+
+  /*
+   * Spawning a blinker thread.
+   */
+  static thread_t thd1;
+  static const THD_DECL_STATIC(thd1_desc, "blinker", thd1_stack,
+                               NORMALPRIO + 10, thd1_func, NULL, NULL);
+  chThdSpawnRunning(&thd1, &thd1_desc);
+
+  /*
+   * Shell manager initialization.
+   */
+  xshellObjectInit(&sm1, &cfg1);
+
+  /*
+   * Starting a buffered SIO, it must behave exactly as a serial driver.
+   */
+  bsioObjectInit(&bsio1, &PORTAB_SIO1,
+                 rxbuf, sizeof rxbuf,
+                 txbuf, sizeof txbuf);
+  msg = drvStart(&bsio1, NULL);
+  osalDbgAssert(msg == HAL_RET_SUCCESS, "buffered SIO start failed");
+
+  /*
+   * Normal main() thread activity, spawning shells.
+   */
+  while (true) {
+    xshell_t *xshp = xshellSpawn(&sm1,
+                                 (shell_stream_i *)oopGetIf(&bsio1, chn),
+                                 NORMALPRIO + 1, NULL);
+    xshellWait(xshp);
+    chThdSleepMilliseconds(500);
+  }
+
+  return 0;
+}
---
+++ demos/STM32/RT-XHAL-STM32-XSHELL/source/clock_cmds.c
@@ -0,0 +1,92 @@
+/*
+    ChibiOS - Copyright (C) 2006-2026 Giovanni Di Sirio.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include <string.h>
+
+#include "ch.h"
+#include "hal.h"
+#include "xshell.h"
+
+#include "clock_cmds.h"
+#include "portab.h"
+
+#if defined(HAL_LLD_USE_CLOCK_MANAGEMENT)
+void cmd_clock(xshell_t *xshp, int argc, char *argv[], char *envp[]) {
+  bool result;
+  const halclkcfg_t *ccp;
+
+  (void)envp;
+
+  if (argc != 2) {
+    xshellUsage(xshp, "clock reset|default");
+    return;
+  }
+
+  if (strcmp(argv[1], "reset") == 0) {
+    chprintf(xshp->stream, XSHELL_NEWLINE_STR "Switching to post-reset clock settings: ");
+    ccp = &hal_clkcfg_reset;
+  }
+  else if (strcmp(argv[1], "default") == 0) {
+    chprintf(xshp->stream, XSHELL_NEWLINE_STR "Switching to default xmcuconf.h clock settings: ");
+    ccp = &hal_clkcfg_default;
+  }
+  else {
+    xshellUsage(xshp, "clock reset|default");
+    return;
+  }
+
+  /* Time for the serial TX buffer to flush.*/
+  chThdSleepMilliseconds(10);
+
+  /* Switching clocks.*/
+  result = halClockSwitchMode(ccp);
+
+  /* Reconfiguring the SIO because clock frequencies could have changed.*/
+  if (!result) {
+    portab_sio_update_clock();
+    if (drvSelectCfgX(&PORTAB_SIO1, 0U) == NULL) {
+      result = true;
+    }
+  }
+
+  /* Printing result.*/
+  if (result) {
+    chprintf(xshp->stream, "failed" XSHELL_NEWLINE_STR);
+  }
+  else {
+    chprintf(xshp->stream, "done" XSHELL_NEWLINE_STR);
+  }
+}
+#endif /* defined(HAL_LLD_USE_CLOCK_MANAGEMENT) */
+
+#if defined(CLK_POINT_NAMES)
+void cmd_clocks(xshell_t *xshp, int argc, char *argv[], char *envp[]) {
+  static const char *names[CLK_ARRAY_SIZE] = CLK_POINT_NAMES;
+  unsigned i;
+
+  (void)argv;
+  (void)envp;
+
+  if (argc != 1) {
+    xshellUsage(xshp, "clocks");
+    return;
+  }
+
+  for (i = 0U; i < CLK_ARRAY_SIZE; i++) {
+    chprintf(xshp->stream, "%10s: %10u" XSHELL_NEWLINE_STR, names[i], halClockGetPointX(i));
+  }
+}
+#endif /* defined(CLK_POINT_NAMES) */
